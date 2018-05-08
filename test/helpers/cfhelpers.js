4cb5c8dd1dd5a91c96d398417f881b3956d61cb7
Armani Ferrante <armaniferrante@gmail.com>
2018-05-08 16:59:42 -0400

    Rebase on develop and fix eslint

--- test/helpers/cfhelpers.js
+++ test/helpers/cfhelpers.js
@@ -28,80 +28,78 @@ async function deployMultisig(owners) {
 }
 
 function getCFDeployer(multisig, registry, provider) {
-    return {
-        deploy: async (contract, signer, cargs) => {
-            const bytecode = ethers.Contract.getDeployTransaction(
-                contract.binary,
-                contract.abi,
-                ...(cargs || []),
-                {
-                    owner: multisig.address,
-                    registry: registry.address,
-                    id: 0,
-                    deltaTimeout: 10,
-                    finalizesAt: 0,
-                    latestNonce: 0,
-                    wasDeclaredFinal: false,
-                    dependancy: {
-                        addr: '0x0',
-                        nonce: 0,
-                    }
-                },
-            ).data
-            const data = new ethers
-                .Interface(registry.abi)
-                .functions
-                .deployAsOwner(bytecode)
-                .data
-            await gnosisSafeUtils.executeTxData(
-                data,
-                registry.address,
-                multisig,
-                [signer],
-                gnosisSafeUtils.Operation.Call
-            )
-            const cfaddress = ethers.utils.solidityKeccak256(
-                ['bytes', 'address[]'],
-                [bytecode, [multisig.address]]
-            )
-            return {
-                cfaddress,
-                contract: new ethers.Contract(
-                    await registry.resolve(cfaddress),
-                    contract.abi,
-                    provider.getSigner(),
-                )
-            }
-        },
-        _callOrDelegateCall: async (callType, cfobject, signer, fnName, fnArgs) => {
-            const data = new ethers
-                .Interface(registry.abi)
-                .functions
-                [callType](
-                    registry.address,
-                    cfobject.cfaddress,
-                    new ethers
-                        .Interface(cfobject.contract.interface.abi)
-                        .functions
-                        [fnName](...fnArgs)
-                        .data
-                )
-                .data
-            await gnosisSafeUtils.executeTxData(
-                data,
-                registry.address,
-                multisig,
-                [signer],
-                gnosisSafeUtils.Operation.Delegatecall
-            )
-        },
-        call: async function (...args) {
-            await this._callOrDelegateCall('proxyCall', ...args)
-        },
-        delegatecall: async function (...args) {
-            await this._callOrDelegateCall('proxyDelegatecall', ...args)
-        }
-    }
+	return {
+		deploy: async (contract, signer, cargs) => {
+			const bytecode = ethers.Contract.getDeployTransaction(
+				contract.binary,
+				contract.abi,
+				...(cargs || []),
+				{
+					owner: multisig.address,
+					registry: registry.address,
+					id: 0,
+					deltaTimeout: 10,
+					finalizesAt: 0,
+					latestNonce: 0,
+					wasDeclaredFinal: false,
+					dependancy: {
+						addr: "0x0",
+						nonce: 0,
+					}
+				},
+			).data;
+			const data = new ethers
+				.Interface(registry.abi)
+				.functions
+				.deployAsOwner(bytecode)
+				.data;
+			await gnosisSafeUtils.executeTxData(
+				data,
+				registry.address,
+				multisig,
+				[signer],
+				gnosisSafeUtils.Operation.Call
+			);
+			const cfaddress = ethers.utils.solidityKeccak256(
+				["bytes", "address[]"],
+				[bytecode, [multisig.address]]
+			);
+			return {
+				cfaddress,
+				contract: new ethers.Contract(
+					await registry.resolve(cfaddress),
+					contract.abi,
+					provider.getSigner(),
+				)
+			};
+		},
+		_callOrDelegateCall: async (callType, cfobject, signer, fnName, fnArgs) => {
+			const data = new ethers
+				.Interface(registry.abi)
+				.functions[callType](
+					registry.address,
+					cfobject.cfaddress,
+					new ethers
+						.Interface(cfobject.contract.interface.abi)
+						.functions[fnName](...fnArgs)
+						.data
+				)
+				.data;
+			await gnosisSafeUtils.executeTxData(
+				data,
+				registry.address,
+				multisig,
+				[signer],
+				gnosisSafeUtils.Operation.Delegatecall
+			);
+		},
+		call: async function (...args) {
+			await this._callOrDelegateCall("proxyCall", ...args);
+		},
+		delegatecall: async function (...args) {
+			await this._callOrDelegateCall("proxyDelegatecall", ...args);
+		}
+	};
 }
 
 // TODO Get rid of use cases of this for the approach above
--- test/integration/unidirectional.js
+++ test/integration/unidirectional.js
@@ -1,118 +1,94 @@
-const ethers = require('ethers')
-const solc = require('solc')
+const ethers = require("ethers");
 
 const {
-    signMessage,
-    unusedAddr,
-    zeroAddress,
-    zeroBytes32,
-    evm_mine,
-    assertRejects,
-} = require('../helpers/utils.js')
+	signMessage,
+	unusedAddr,
+	assertRejects,
+} = require("../helpers/utils.js");
 
 const {
-    getCFDeployer,
-    deployMultisig,
-} = require('../helpers/cfhelpers.js')
+	getCFDeployer,
+	deployMultisig,
+} = require("../helpers/cfhelpers.js");
 
-const Registry = artifacts.require("Registry")
-const UnidirectionalETHBalance = artifacts.require("UnidirectionalETHBalance")
+const Registry = artifacts.require("Registry");
+const UnidirectionalETHBalance = artifacts.require("UnidirectionalETHBalance");
 
-contract('UnidirectionalETHBalance', (accounts) => {
+contract("UnidirectionalETHBalance", (accounts) => {
 
-    let registry, multisig, signer
+	let registry, signer;
 
-    const provider = new ethers.providers.Web3Provider(web3.currentProvider)
+	const provider = new ethers.providers.Web3Provider(web3.currentProvider);
 
-    let i = 0;
-    const defaultObjectStorage = (registry) => ({
-        owner: accounts[0],
-        registry: registry.address,
-        id: i++,
-        deltaTimeout: 10,
-        finalizesAt: 0,
-        latestNonce: 0,
-        wasDeclaredFinal: false,
-        dependancy: {
-            addr: '0x0',
-            nonce: 0,
-        }
-    })
+	beforeEach(async () => {
+		registry = await Registry.new(unusedAddr);
+		signer = ethers.Wallet.createRandom();
+		signer.provider = provider;
+	});
 
-    beforeEach(async () => {
-        registry = await Registry.new(unusedAddr)
-        signer = ethers.Wallet.createRandom()
-        signer.provider = provider
-    })
+	it("Can instantly send money to the recipient", async () => {
 
-    it("Can instantly send money to the recipient", async () => {   
+		const multisig = await deployMultisig([signer.address]);
 
-        const extraGas = {
-            gasLimit: 4712388,
-            gasPrice: await provider.getGasPrice(),
-        }
+		const deployer = getCFDeployer(multisig, registry, provider);
 
-        const multisig = await deployMultisig([signer.address])
+		const cargs = [signer.address, signer.address, ethers.utils.parseEther("1")];
 
-        const deployer = getCFDeployer(multisig, registry, provider)
+		const unidirectional = await deployer.deploy(UnidirectionalETHBalance, signer, cargs);
 
-        const cargs = [signer.address, signer.address, ethers.utils.parseEther('1')]
+		const sig = signMessage(
+			ethers.utils.solidityKeccak256(
+				["bytes1", "address", "uint256"],
+				["0x19", multisig.address, ethers.utils.parseEther("0.5")],
+			),
+			signer
+		);
 
-        const unidirectional = await deployer.deploy(UnidirectionalETHBalance, signer, cargs)
+		await unidirectional.contract.connect(await provider.getSigner()).setAmount(
+			{v: sig[0], r: sig[1], s: sig[2]},
+			{v: sig[0], r: sig[1], s: sig[2]},
+			ethers.utils.parseEther("0.5")
+		);
 
-        const sig = signMessage(
-            ethers.utils.solidityKeccak256(
-                ['bytes1', 'address', 'uint256'],
-                ['0x19', multisig.address, ethers.utils.parseEther('0.5')],
-            ),
-            signer
-        )
+		assert.equal(
+			(await unidirectional.contract.decidedAmount()).toString(),
+			(ethers.utils.parseEther("0.5")).toString(),
+		);
 
-        await unidirectional.contract.connect(await provider.getSigner()).setAmount(
-            {v: sig[0], r: sig[1], s: sig[2]},
-            {v: sig[0], r: sig[1], s: sig[2]},
-            ethers.utils.parseEther('0.5')
-        )
+		assert.equal(
+			(await unidirectional.contract.maxAmount()).toString(),
+			(ethers.utils.parseEther("1.0")).toString(),
+		);
 
-        assert.equal(
-            (await unidirectional.contract.decidedAmount()).toString(),
-            (ethers.utils.parseEther('0.5')).toString(),
-        )
+		const moneybags = await provider.getSigner(accounts[0]);
+		await moneybags.sendTransaction({
+			to: multisig.address,
+			value: ethers.utils.parseEther("2.5"),
+		});
 
-        assert.equal(
-            (await unidirectional.contract.maxAmount()).toString(),
-            (ethers.utils.parseEther('1.0')).toString(),
-        )
+		await deployer.delegatecall(unidirectional, signer, "claimAmount", [
+			registry.address, unidirectional.cfaddress
+		]);
 
-        const moneybags = await provider.getSigner(accounts[0])
-        await moneybags.sendTransaction({
-            to: multisig.address,
-            value: ethers.utils.parseEther('2.5'),
-        })
+		assert.equal(
+			(await provider.getBalance(multisig.address)).toString(),
+			(ethers.utils.parseEther("2.0")).toString()
+		);
 
-        await deployer.delegatecall(unidirectional, signer, 'claimAmount', [
-            registry.address, unidirectional.cfaddress
-        ])
-    
-        assert.equal(
-            (await provider.getBalance(multisig.address)).toString(),
-            (ethers.utils.parseEther('2.0')).toString()
-        )
+		assert.equal(
+			(await provider.getBalance(signer.address)).toString(),
+			(ethers.utils.parseEther("0.5")).toString()
+		);
 
-        assert.equal(
-            (await provider.getBalance(signer.address)).toString(),
-            (ethers.utils.parseEther('0.5')).toString()
-        )
+		await assertRejects(
+			unidirectional.contract.connect(await provider.getSigner()).setAmount(
+				{v: sig[0], r: sig[1], s: sig[2]},
+				{v: sig[0], r: sig[1], s: sig[2]},
+				ethers.utils.parseEther("0.5")
+			)
+		);
 
-        await assertRejects(
-            unidirectional.contract.connect(await provider.getSigner()).setAmount(
-                {v: sig[0], r: sig[1], s: sig[2]},
-                {v: sig[0], r: sig[1], s: sig[2]},
-                ethers.utils.parseEther('0.5')
-            )
-        )
-
-    })
+	});
 
 
-})
+});
