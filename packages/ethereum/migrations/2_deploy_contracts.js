4be9e9786eb2b4197113a663eb08fcd3febdb67f
snario <liam@lihorne.com>
2018-06-27 02:27:59 -0400

    Fix issue with gitignore being too aggressive

---
+++ packages/ethereum/migrations/2_deploy_contracts.js
@@ -0,0 +1,22 @@
+const AssetLib = artifacts.require("./AssetLib.sol");
+const MinimumViableMultisig = artifacts.require("./MinimumViableMultisig.sol");
+const ProxyFactory = artifacts.require("./ProxyFactory.sol");
+const Registry = artifacts.require("./Registry.sol");
+const RegistryAddressLib = artifacts.require("./RegistryAddressLib.sol");
+const RegistryCallLib = artifacts.require("./RegistryCallLib.sol");
+
+module.exports = (deployer) => {
+	deployer.then(async () => {
+		await deployer.deploy(MinimumViableMultisig);
+
+		await deployer.deploy(ProxyFactory);
+
+		await deployer.deploy(RegistryAddressLib);
+
+		await deployer.deploy(RegistryCallLib);
+
+		await deployer.deploy(Registry);
+
+		await deployer.deploy(AssetLib);
+	});
+};
