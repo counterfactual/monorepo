73181e1ab75dd4810b6707678e470cba130294c5
snario <liam@lihorne.com>
2018-07-24 08:09:20 -0400

    Improvements from feedback 1

--- packages/cf-core/contracts/test/TestCaller.sol
+++ packages/cf-core/contracts/test/TestCaller.sol
@@ -29,7 +29,7 @@ contract TestCaller {
     returns (bool)
   {
     bytes memory data = abi.encodePacked(selector, calldata);
-    return to.staticcallBool(data);
+    return to.staticcall_as_bool(data);
   }
 
 }
