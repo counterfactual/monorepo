1370e6500857eb3ac649c29cefba9191016db693
snario <liam@lihorne.com>
2018-07-27 12:19:20 -0400

    More docs...

--- packages/cf-core/contracts/lib/Transfer.sol
+++ packages/cf-core/contracts/lib/Transfer.sol
@@ -5,6 +5,12 @@ import "openzeppelin-solidity/contracts/AddressUtils.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
+/// @title Transfer - A library to encode a generic asset transfer data type
+/// @author Liam Horne - <liam@l4v.io>
+/// @notice This library defines `Transfer.Details` and `Transfer.Terms`, two structures
+/// which are used in state channel applications to represent a kind of "resolution" and
+/// a commitment to how much can be resolved respectively. A `Transfer.Details` object
+/// should be able to encode any arbitrary Ethereum-based asset transfer.
 library Transfer {
 
   enum Asset {
@@ -26,6 +32,9 @@ library Transfer {
     bytes data;
   }
 
+  /// @notice A delegate target for executing transfers of an arbitrary Transfer.Detail
+  /// @param details A `Transfer.Details` struct
+  /// TODO: Add support for an OTHER Asset type and do a (to, value, data) CALL
   function executeTransfer(Transfer.Details memory details) public {
     for (uint256 i = 0; i < details.to.length; i++) {
       address to = details.to[i];
@@ -39,6 +48,11 @@ library Transfer {
     }
   }
 
+  /// @notice Verifies whether or not a `Transfer.Details` meets the terms set by a
+  /// `Transfer.Terms` object based on the limit information of how much can be transferred
+  /// @param details A `Transfer.Details` struct
+  /// @param details A `Transfer.Terms` struct
+  /// @return A boolean indicating if the terms are met
   function meetsTerms(
     Transfer.Details memory details,
     Transfer.Terms terms
