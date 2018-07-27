e80c97dc2f190d53390acb72bb8ae2abc3b65f09
snario <liam@lihorne.com>
2018-07-27 14:17:28 -0400

    WIP

--- packages/cf-apps/contracts/test/loadContracts.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity 0.4.24;
-pragma experimental "ABIEncoderV2";
-
-import "@counterfactual/core/contracts/lib/StaticCall.sol";
-
--- packages/cf-core/contracts/test/loadContracts.sol
+++
@@ -1,4 +0,0 @@
-pragma solidity 0.4.24;
-
-import "@counterfactual/apps/contracts/CountingApp.sol";
-import "@counterfactual/apps/contracts/PaymentApp.sol";
