0bf0ba7d45d0fcfbf39504612768749be4962665
Mitchell Van Der Hoeff <mitchell.vanderhoeff@gmail.com>
2018-07-30 15:42:01 -0400

    Added docs for ConditionalTransfer (#106)
    * Added docs for ConditionalTransfer
    
    * Short fixes

--- packages/contracts/contracts/delegateTargets/ConditionalTransfer.sol
+++ packages/contracts/contracts/delegateTargets/ConditionalTransfer.sol
@@ -8,6 +8,10 @@ import "@counterfactual/contracts/contracts/NonceRegistry.sol";
 import "@counterfactual/contracts/contracts/StateChannel.sol";
 
 
+/// @title ConditionalTransfer - A conditional transfer contract
+/// @author Liam Horne - <liam@l4v.io>
+/// @author Mitchell Van Der Hoeff - <mitchell@l4v.io>
+/// @notice Supports a complex transfer of funds contingent on some condition.
 contract ConditionalTransfer is Conditional {
 
   using Transfer for Transfer.Details;
@@ -15,11 +19,17 @@ contract ConditionalTransfer is Conditional {
   Registry public _registry;
   NonceRegistry public _nonceRegistry;
 
+  /// @notice Contract constructor
+  /// @param registry The registry to use for resolution of Counterfactual addresses
+  /// @param nonceRegistry The nonce registry to check for finalized state channel nonces
   constructor(Registry registry, NonceRegistry nonceRegistry) {
     _registry = registry;
     _nonceRegistry = nonceRegistry;
   }
 
+  /// @notice Execute a fund transfer if simple condition is met
+  /// @param condition The condition that has to be met
+  /// @param details Details of the funds transfer to be executed
   function executeSimpleConditionalTransfer(
     Condition condition,
     Transfer.Details memory details
@@ -30,10 +40,15 @@ contract ConditionalTransfer is Conditional {
     details.executeTransfer();
   }
 
+  /// @notice Execute a fund transfer for a state channel in a finalized state
+  /// @param key The key in the nonce registry
+  /// @param expectedNonce The expected nonce in the nonce registry
+  /// @param channelCfAddress Counterfactual address of the state channel contract
+  /// @param terms The pre-agreed upon terms of the funds transfer
   function executeStateChannelConditionalTransfer(
     bytes32 key,
     uint256 expectedNonce,
-    bytes32 stateChannelId,
+    bytes32 channelCfAddress,
     Transfer.Terms terms
   )
     public
@@ -44,8 +59,8 @@ contract ConditionalTransfer is Conditional {
       "State Channel nonce is either not finalized or finalized at an incorrect nonce"
     );
 
-    address addr = _registry.resolver(stateChannelId);
-    StateChannel channel = StateChannel(addr);
+    address channelAddr = _registry.resolver(channelCfAddress);
+    StateChannel channel = StateChannel(channelAddr);
     Transfer.Details memory details = channel.getResolution();
 
     require(
