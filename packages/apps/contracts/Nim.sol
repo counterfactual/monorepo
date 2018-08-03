8cb8b62d2cb19472e8e6bc9556a59aa265b8bac8
Li Xuanji <xuanji@gmail.com>
2018-08-03 08:59:47 -0400

    Add contract and contract test for Nim (#5)
    * Add contract for Nim
    
    * add tests
    
    * lint
    
    * remove untested tag
    
    * rename to reduce

---
+++ packages/apps/contracts/Nim.sol
@@ -0,0 +1,96 @@
+pragma solidity 0.4.24;
+pragma experimental "ABIEncoderV2";
+
+import "@counterfactual/contracts/contracts/lib/Transfer.sol";
+
+
+/*
+Normal-form Nim
+https://en.wikipedia.org/wiki/Nim
+*/
+contract Nim {
+
+  struct Action {
+    uint256 pileIdx;
+    uint256 takeAmnt;
+  }
+
+  struct AppState {
+    address[2] players;
+    uint256 turnNum;
+    uint256[3] pileHeights;
+  }
+
+  function isStateFinal(AppState state)
+    public
+    pure
+    returns (bool)
+  {
+    return isWin(state);
+  }
+
+  function isWin(AppState state)
+    internal
+    pure
+    returns (bool)
+  {
+    return (
+      (state.pileHeights[0] == 0) &&
+      (state.pileHeights[1] == 0) &&
+      (state.pileHeights[2] == 0)
+    );
+  }
+
+  function turn(AppState state)
+    public
+    pure
+    returns (uint256)
+  {
+    return state.turnNum % 2;
+  }
+
+  function reduce(AppState state, Action action)
+    public
+    view
+    returns (bytes)
+  {
+    require(0 <= action.pileIdx);
+    require(action.pileIdx < 3);
+    require(state.pileHeights[action.pileIdx] >= action.takeAmnt);
+
+    AppState memory ret = state;
+
+    ret.pileHeights[action.pileIdx] -= action.takeAmnt;
+    ret.turnNum += 1;
+
+    return abi.encode(ret);
+  }
+
+  function resolver(AppState state, Transfer.Terms terms)
+    public
+    pure
+    returns (Transfer.Details)
+  {
+    require(isWin(state));
+    address loser = state.players[state.turnNum % 2];
+    address winner = state.players[1 - (state.turnNum % 2)];
+
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = terms.limit;
+    amounts[1] = 0;
+
+    address[] memory to = new address[](2);
+    to[0] = loser;
+    to[1] = winner;
+    bytes memory data; // = 0
+
+    return Transfer.Details(
+      terms.assetType,
+      terms.token,
+      to,
+      amounts,
+      data
+    );
+  }
+
+}
---
+++ packages/apps/test/nim.spec.ts
@@ -0,0 +1,112 @@
+import * as ethers from "ethers";
+
+import * as Utils from "@counterfactual/test-utils";
+
+const Nim = artifacts.require("Nim");
+
+const StaticCall = artifacts.require("StaticCall");
+
+const web3 = (global as any).web3;
+const { provider, unlockedAccount } = Utils.setupTestEnv(web3);
+
+contract("Nim", (accounts: string[]) => {
+  let game: ethers.Contract;
+
+  const stateEncoding =
+    "tuple(address[2] players, uint256 turnNum, uint256[3] pileHeights)";
+
+  beforeEach(async () => {
+    const contract = new ethers.Contract("", Nim.abi, unlockedAccount);
+    Nim.link("StaticCall", StaticCall.address);
+    game = await contract.deploy(Nim.binary);
+  });
+
+  describe("reducer", () => {
+    it("can take from a pile", async () => {
+      const preState = {
+        players: [Utils.zeroAddress, Utils.zeroAddress],
+        turnNum: 0,
+        pileHeights: [6, 5, 12]
+      };
+
+      const action = {
+        pileIdx: 0,
+        takeAmnt: 5
+      };
+
+      const ret = await game.functions.reduce(preState, action);
+
+      const postState = ethers.utils.defaultAbiCoder.decode(
+        [stateEncoding],
+        ret
+      )[0];
+
+      postState.pileHeights[0].should.be.bignumber.eq(1);
+      postState.pileHeights[1].should.be.bignumber.eq(5);
+      postState.pileHeights[2].should.be.bignumber.eq(12);
+      postState.turnNum.should.be.bignumber.eq(1);
+    });
+
+    it("can take to produce an empty pile", async () => {
+      const preState = {
+        players: [Utils.zeroAddress, Utils.zeroAddress],
+        turnNum: 0,
+        pileHeights: [6, 5, 12]
+      };
+
+      const action = {
+        pileIdx: 0,
+        takeAmnt: 6
+      };
+
+      const ret = await game.functions.reduce(preState, action);
+
+      const postState = ethers.utils.defaultAbiCoder.decode(
+        [stateEncoding],
+        ret
+      )[0];
+
+      postState.pileHeights[0].should.be.bignumber.eq(0);
+      postState.pileHeights[1].should.be.bignumber.eq(5);
+      postState.pileHeights[2].should.be.bignumber.eq(12);
+      postState.turnNum.should.be.bignumber.eq(1);
+    });
+
+    it("should fail for taking too much", async () => {
+      const preState = {
+        players: [Utils.zeroAddress, Utils.zeroAddress],
+        turnNum: 0,
+        pileHeights: [6, 5, 12]
+      };
+
+      const action = {
+        pileIdx: 0,
+        takeAmnt: 7
+      };
+
+      await Utils.assertRejects(game.functions.reduce(preState, action));
+    });
+  });
+
+  describe("isFinal", () => {
+    it("empty state is final", async () => {
+      const preState = {
+        players: [Utils.zeroAddress, Utils.zeroAddress],
+        turnNum: 49,
+        pileHeights: [0, 0, 0]
+      };
+      const ret = await game.functions.isStateFinal(preState);
+      ret.should.be.eq(true);
+    });
+
+    it("nonempty state is not final", async () => {
+      const preState = {
+        players: [Utils.zeroAddress, Utils.zeroAddress],
+        turnNum: 49,
+        pileHeights: [0, 1, 0]
+      };
+      const ret = await game.functions.isStateFinal(preState);
+      ret.should.be.eq(false);
+    });
+  });
+});
