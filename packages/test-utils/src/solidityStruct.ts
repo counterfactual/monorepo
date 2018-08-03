63ddc361c61572bb3b9c49e5136573d005e5c4a8
Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
2018-08-03 15:50:20 -0400

    Add docs

--- packages/test-utils/src/solidityStruct.ts
+++ packages/test-utils/src/solidityStruct.ts
@@ -1,8 +1,35 @@
 import * as ethers from "ethers";
 
+/**
+ * Convenience class to define a Solidity struct type to create instances of Solidity structs.
+ * See SolidityStruct class for instance methods.
+ * @example
+ *  const AppState = new SolidityStructType(`
+ *    uint256 someNumber;
+ *    address[] someAddresses;
+ *  `);
+ *  const appState = AppState.new({
+ *    someNumber: 3,
+ *    someAddresses: ["0xbb..", "0xaa.."]
+ *  });
+ *
+ */
 export class SolidityStructType {
+  /**
+   * Struct field definitions. Format: "[type] [name]" e.g. "uint256 someNumber"
+   */
   public readonly definitions: string[] = [];
 
+  /**
+   * Create a new Solidity struct type from definition.
+   * @param solidityDefinition The Solidity definition of the struct fields. `uint256` should be substituted for Enum types.
+   * @example
+   *   new SolidityStructType(`
+   *     uint256 someEnum;
+   *     address[] someAddresses;
+   *   `);
+   * @throws Error if Solidity definition in a bad format.
+   */
   constructor(solidityDefinition: string) {
     const lines = solidityDefinition.split(";");
     for (const line of lines) {
@@ -20,11 +47,19 @@ export class SolidityStructType {
     }
   }
 
+  /**
+   * Create a new SolidityStruct instance of this type.
+   * Ensure the values are the correct types, as they are not type checked.
+   * @param values The values to assign to the struct fields.
+   */
   public new(values: { [name: string]: any }): SolidityStruct {
     return new SolidityStruct(this, values);
   }
 }
 
+/**
+ * Solidity struct convenience class for use with Counterfactual framework.
+ */
 export class SolidityStruct {
   constructor(
     private structType: SolidityStructType,
@@ -35,11 +70,19 @@ export class SolidityStruct {
     return `tuple(${this.structType.definitions.join(", ")})`;
   }
 
+  /**
+   * Compute the keccak256 hash of the ABI encoded representation of this struct.
+   * @returns string 32-byte keccak256 hash
+   */
   public keccak256(): string {
     const encoded = this.encodeBytes();
     return ethers.utils.solidityKeccak256(["bytes"], [encoded]);
   }
 
+  /**
+   * ABI encode this struct.
+   * @returns string bytestring of the ABI encoding
+   */
   public encodeBytes(): string {
     return ethers.utils.defaultAbiCoder.encode(
       [this._encoding],
@@ -47,6 +90,9 @@ export class SolidityStruct {
     );
   }
 
+  /**
+   * The struct as a Javascript object for use in a contract call.
+   */
   public asObject(): { [k: string]: any } {
     const values: { [k: string]: any } = {};
     Object.keys(this.values).forEach(name => {
@@ -59,6 +105,11 @@ export class SolidityStruct {
     return values;
   }
 
+  /**
+   * Clone this struct and assign new values to the fields.
+   * @param newValues New values to assign
+   * @returns SolidityStruct New struct of same Solidity struct type with new values
+   */
   public clone(newValues: { [k: string]: any }): SolidityStruct {
     const values = this.asObject();
     return this.structType.new({
