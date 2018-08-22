c1b46c806293a3c67e2f9b2523426a3b0947bc75
cf19drofxots <38441757+cf19drofxots@users.noreply.github.com>
2018-08-22 10:51:42 -0400

    Moves apps back alongside contracts (#29)
    * Update dependency
    
    * Add example apps back to contracts

---
+++ contracts/apps/PaymentApp.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.4.24;
+pragma experimental "ABIEncoderV2";
+
+import "../lib/Transfer.sol";
+
+
+contract PaymentApp {
+
+  struct AppState {
+    address alice;
+    address bob;
+    uint256 aliceBalance;
+    uint256 bobBalance;
+  }
+
+  function resolve(AppState state, Transfer.Terms terms)
+    public
+    pure
+    returns (Transfer.Details)
+  {
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = state.aliceBalance;
+    amounts[1] = state.bobBalance;
+
+    address[] memory to = new address[](2);
+    to[0] = state.alice;
+    to[1] = state.bob;
+    bytes memory data;
+
+    return Transfer.Details(
+      terms.assetType,
+      terms.token,
+      to,
+      amounts,
+      data
+    );
+  }
+
+}
--- contracts/test/loadContracts.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity 0.4.24;
-
-import "@counterfactual/apps/contracts/CountingApp.sol";
-import "@counterfactual/apps/contracts/CommitRevealApp.sol";
-import "@counterfactual/apps/contracts/PaymentApp.sol";
