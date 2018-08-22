3b742766ea6712c6afa0586a243047231c88965c
cf19drofxots <nima@l4v.io>
2018-08-22 09:05:15 -0400

    Update lib path and gitignore

--- lib/counterfactual/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./stateChannel";
-export * from "./structEncoding";
-export * from "./contract";
-export * from "./multisig";
-export { default as buildArtifacts } from "./buildArtifacts";
--- lib/counterfactual/utils/structEncoding.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function abiEncodingForStruct(structDefinition: string): string;
-export declare function encodeStruct(encoding: string, struct: object): string;
