e9a5c815a4ed43caa9db216d4551df7966c7fe07
Li Xuanji <xuanji@gmail.com>
2018-09-13 16:20:36 -0400

    revert changes to conditionalTransfer.sol

--- contracts/delegateTargets/ConditionalTransfer.sol
+++ contracts/delegateTargets/ConditionalTransfer.sol
@@ -16,7 +16,6 @@ contract ConditionalTransfer is Conditional {
 
   using Transfer for Transfer.Details;
 
-  /// Unused
   function executeSimpleConditionalTransfer(
     Condition condition,
     Transfer.Details memory details
@@ -28,53 +27,28 @@ contract ConditionalTransfer is Conditional {
   }
 
   /// @notice Execute a fund transfer for a state channel in a finalized state
-  /// @param key The key; fixed for a given state channel
-  /// @param expectedR The expected r-nonce in the nonce registry
+  /// @param key The key in the nonce registry
+  /// @param expectedNonce The expected nonce in the nonce registry
   /// @param channelCfAddress Counterfactual address of the state channel contract
   /// @param terms The pre-agreed upon terms of the funds transfer
   function executeStateChannelConditionalTransfer(
     address registry,
     address nonceRegistry,
     bytes32 key,
-    uint256 expectedR,
-    uint8 idx,
+    uint256 expectedNonce,
     bytes32 channelCfAddress,
     Transfer.Terms terms
   )
     public
-    view
   {
     require(
-      NonceRegistry(nonceRegistry).isFinalized120i(key, expectedR, idx),
+      NonceRegistry(nonceRegistry).isFinalized(key, expectedNonce),
       "State Channel nonce is either not finalized or finalized at an incorrect nonce"
     );
 
     address channelAddr = Registry(registry).resolver(channelCfAddress);
     StateChannel channel = StateChannel(channelAddr);
     Transfer.Details memory details = channel.getResolution();
-
-    require(
-      Transfer.meetsTerms(details, terms),
-      "Transfer details do not meet terms"
-    );
-
-    details.executeTransfer();
-  }
-
-  function executeFreeBalanceConditionalTransfer(
-    address nonceRegistry,
-    bytes32 key,
-    uint256 expectedR,
-    Transfer.Terms terms,
-    Transfer.Details details
-  )
-    public
-    view
-  {
-    require(
-      NonceRegistry(nonceRegistry).isFinalized128(key, expectedR),
-      "State Channel nonce is either not finalized or finalized at an incorrect nonce"
-    );
 
     require(
       Transfer.meetsTerms(details, terms),
