6339d5643fff25be35065acdb3cbe4bbab5ed2e9
Li Xuanji <xuanji@gmail.com>
2018-09-24 12:55:47 -0400

    nonceIsSet (#43)
    * nonceIsSet
    
    * rename
    
    * add modifier
    
    * Update NonceRegistry.sol

--- contracts/NonceRegistry.sol
+++ contracts/NonceRegistry.sol
@@ -94,6 +94,10 @@ contract NonceRegistry {
     return true;
   }
 
+  function isNonceSet(bytes32 key) external view returns (bool) {
+    return table[key].finalizesAt > 0;
+  }
+
   /// @notice Set a nonce in the mapping.
   /// Trigger the timeout period to begin if the nonce is set for the first time.
   /// @param salt A salt used to generate the nonce key
