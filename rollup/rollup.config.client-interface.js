3e7cf5d980207c09e1c953d14c645cabbaee1e28
cf19drofxots <38441757+cf19drofxots@users.noreply.github.com>
2018-09-25 14:22:51 -0400

    Fix rollup & editorconfig (#94)

--- rollup/rollup.config.client-interface.js
+++ rollup/rollup.config.client-interface.js
@@ -1,20 +1,25 @@
 import json from "rollup-plugin-json";
 
 export default {
-	input: "dist/cf.js/client-interface.js",
-	output: {
-		file: "build/js/client-interface.min.js",
-		format: "iife",
-		sourcemap: true,
-		name: "ci"
-	},
-	plugins: [
-		json({
-			include: [
-				"dist/contracts/build/contracts/*",
-				"dist/contracts/networks/*"
-			],
-			preferConst: true
-		})
-	]
+  input: "dist/cf.js/client-interface.js",
+  output: {
+    file: "build/js/client-interface.min.js",
+    format: "iife",
+    sourcemap: true,
+    name: "ci",
+    globals: {
+      ethers: "ethers",
+      lodash: "_",
+      testUtils: "test-utils"
+    }
+  },
+  plugins: [
+    json({
+      include: [
+        "dist/contracts/build/contracts/*",
+        "dist/contracts/networks/*"
+      ],
+      preferConst: true
+    })
+  ]
 };
