21a559b0ead70d4bd2965999a0c5c5df44ccef85
cf19drofxots <38441757+cf19drofxots@users.noreply.github.com>
2018-09-25 14:22:51 -0400

    Fix rollup & editorconfig (#94)

--- rollup/rollup.config.wallet.js
+++ rollup/rollup.config.wallet.js
@@ -1,20 +1,25 @@
 import json from "rollup-plugin-json";
 
 export default {
-	input: "dist/test/wallet/wallet.js",
-	output: {
-		file: "build/js/wallet.min.js",
-		format: "iife",
-		sourcemap: true,
-		name: "wa"
-	},
-	plugins: [
-		json({
-			include: [
-				"dist/contracts/build/contracts/*",
-				"dist/contracts/networks/*"
-			],
-			preferConst: true
-		})
-	]
+  input: "dist/test/wallet/wallet.js",
+  output: {
+    file: "build/js/wallet.min.js",
+    format: "iife",
+    sourcemap: true,
+    name: "wa",
+    globals: {
+      ethers: "ethers",
+      testUtils: "test-utils",
+      lodash: "_"
+    }
+  },
+  plugins: [
+    json({
+      include: [
+        "dist/contracts/build/contracts/*",
+        "dist/contracts/networks/*"
+      ],
+      preferConst: true
+    })
+  ]
 };
