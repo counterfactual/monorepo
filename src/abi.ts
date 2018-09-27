8f601dc12edfd95b65040a897ac3dbac79553206
cf19drofxots <38441757+cf19drofxots@users.noreply.github.com>
2018-09-27 16:58:27 -0400

    Bind defaultAbiCoder to functions (#99)

--- src/abi.ts
+++ src/abi.ts
@@ -1,7 +1,13 @@
 import * as ethers from "ethers";
 
-export const encode = ethers.utils.defaultAbiCoder.encode;
+export const encode = ethers.utils.defaultAbiCoder.encode.bind(
+  ethers.utils.defaultAbiCoder
+);
 
-export const decode = ethers.utils.defaultAbiCoder.decode;
+export const decode = ethers.utils.defaultAbiCoder.decode.bind(
+  ethers.utils.defaultAbiCoder
+);
 
-export const encodePacked = ethers.utils.solidityPack;
+export const encodePacked = ethers.utils.solidityPack.bind(
+  ethers.utils.defaultAbiCoder
+);
