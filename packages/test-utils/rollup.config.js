9a6fd2f1e87ec818e8bb36d18982c7b9ca8f568a
cf19drofxots <nima@l4v.io>
2018-10-10 20:42:35 -0700

    Add more packages and start getting some contracts tests passing again

---
+++ packages/test-utils/rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from "rollup-plugin-typescript2";
+
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.main,
+      format: "cjs"
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      })
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: pkg.module,
+      format: "es"
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        typescript: require("typescript")
+      })
+    ]
+  }
+];
