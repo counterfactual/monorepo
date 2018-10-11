0ed3e6639acab9428cdd3a62927267858738a899
cf19drofxots <nima@l4v.io>
2018-10-10 20:42:35 -0700

    Add more packages and start getting some contracts tests passing again

---
+++ README.md
@@ -0,0 +1,9 @@
+# Counterfactual Monorepo
+
+Requirements:
+
+1. `yarn`
+2. Docker
+
+3. `yarn`
+4. `lerna bootstrap --hoist`
--- packages/machine/test/common.ts
+++ packages/machine/test/common.ts
@@ -1,14 +1,14 @@
-import AppInstance from "../contracts/build/contracts/AppInstance.json";
-import ConditionalTransfer from "../contracts/build/contracts/ConditionalTransfer.json";
-import ETHBalanceRefundApp from "../contracts/build/contracts/ETHBalanceRefundApp.json";
-import MinimumViableMultisig from "../contracts/build/contracts/MinimumViableMultisig.json";
-import MultiSend from "../contracts/build/contracts/MultiSend.json";
-import NonceRegistry from "../contracts/build/contracts/NonceRegistry.json";
-import PaymentApp from "../contracts/build/contracts/PaymentApp.json";
-import Registry from "../contracts/build/contracts/Registry.json";
-import Signatures from "../contracts/build/contracts/Signatures.json";
-import StaticCall from "../contracts/build/contracts/StaticCall.json";
-import networkFile from "../contracts/networks/7777777.json";
+import AppInstance from "../../contracts/build/contracts/AppInstance.json";
+import ConditionalTransfer from "../../contracts/build/contracts/ConditionalTransfer.json";
+import ETHBalanceRefundApp from "../../contracts/build/contracts/ETHBalanceRefundApp.json";
+import MinimumViableMultisig from "../../contracts/build/contracts/MinimumViableMultisig.json";
+import MultiSend from "../../contracts/build/contracts/MultiSend.json";
+import NonceRegistry from "../../contracts/build/contracts/NonceRegistry.json";
+import PaymentApp from "../../contracts/build/contracts/PaymentApp.json";
+import Registry from "../../contracts/build/contracts/Registry.json";
+import Signatures from "../../contracts/build/contracts/Signatures.json";
+import StaticCall from "../../contracts/build/contracts/StaticCall.json";
+import networkFile from "../../contracts/networks/7777777.json";
 
 import * as wallet from "@counterfactual/wallet";
 import * as ethers from "ethers";
