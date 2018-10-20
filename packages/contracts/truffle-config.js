73a4232ec874db01e5b125094a4b1e86a097185f
Liam Horne <snario@users.noreply.github.com>
2018-10-20 12:15:17 -0400

    Move tslint and tsconfig to root (#133)
    * Move tslint and tsconfig to root
    
    * Fix typo in typeRoots

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -1,1 +1,26 @@
-module.exports = require("@counterfactual/configs/truffle-config");
+module.exports = {
+  networks: {
+    ganache: {
+      network_id: 7777777,
+      host: "127.0.0.1",
+      port: 9545,
+      gas: "0xfffffffffff",
+      gasPrice: "0x01"
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 21,
+      outputFile: "/dev/null",
+      showTimeSpent: true
+    }
+  }
+};
