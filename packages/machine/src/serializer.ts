e65f85d3ba789b93766ef815a4e273b4d7ea40e8
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-11-07 11:40:44 -0300

    refactored serializer

--- packages/machine/src/serializer.ts
+++ packages/machine/src/serializer.ts
@@ -1,32 +1,73 @@
 import * as cf from "@counterfactual/cf.js";
 import * as ethers from "ethers";
 
+import { DeserializationCase } from "./types";
+
+/**
+ * Condition functions, used to determine the deserialization resolver
+ */
+
+const isObject = data => typeof data === "object" && data !== null;
+const isBigNumber = data => isObject(data) && Object.keys(data).length === 1 && data._hex;
+const isSignature = data =>
+  isObject(data) &&
+  Object.keys(data).length === 3 &&
+  data.v &&
+  data.r &&
+  data.s;
+
+/**
+ * Deserialization functions
+ */
+
+const deserializeArray = data => data.map(value => deserialize(value));
+const deserializeBigNumber = data => ethers.utils.bigNumberify(data._hex);
+const deserializeSignature = data =>
+  new cf.utils.Signature(data.v, data.r, data.s);
+const deserializeObject = data =>
+  Object.keys(data).reduce((deserializedData: object, key: string) => {
+    deserializedData[key] = deserialize(data[key]);
+
+    return deserializedData;
+  }, {});
+
+/**
+ * This array describes how to deal with each deserialization case for
+ * a given JSON payload. The deserialize() function will run through
+ * these cases until it finds a match, then stops and returns the result
+ * of the resolver.
+ */
+
+const deserializeCases: DeserializationCase[] = [
+  {
+    condition: data => Array.isArray(data),
+    resolve: deserializeArray
+  },
+  {
+    condition: isBigNumber,
+    resolve: deserializeBigNumber
+  },
+  {
+    condition: isSignature,
+    resolve: deserializeSignature
+  },
+  {
+    condition: isObject,
+    resolve: deserializeObject
+  }
+];
+
 /**
  * reconstitute a freshly `JSON.parse`d payload, ensuring that objects
  * have the correct class, such as `BigNumber`
  */
 export function deserialize(data: any): any {
-  if (Array.isArray(data)) {
-    return data.map(value => deserialize(value));
+  for (let caseIndex = 0; caseIndex < deserializeCases.length; caseIndex += 1) {
+    const deserializeCase = deserializeCases[caseIndex];
+    if (deserializeCase.condition(data)) {
+      return deserializeCase.resolve(data);
+    }
   }
-  if (typeof data === "object" && data !== null) {
-    const keys = Object.keys(data);
-    if (keys.length === 1 && keys[0] === "_hex") {
-      // bigNumberify
-      return ethers.utils.bigNumberify(data[keys[0]]);
-    }
-    if (
-      keys.length === 3 &&
-      ["v", "r", "s"].every(key => keys.indexOf(key) > -1)
-    ) {
-      // Signatures
-      return new cf.utils.Signature(data.v, data.r, data.s);
-    }
-    return keys.reduce((deserializedData: object, key: string) => {
-      deserializedData[key] = deserialize(data[key]);
 
-      return deserializedData;
-    }, {});
-  }
   return data;
 }
