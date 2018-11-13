712ff6b904180e440e7f17eb7dbf1b014fce3054
Li Xuanji <xuanji@gmail.com>
2018-11-13 18:29:30 +0800

    use STATE_TRANSITION_PROPOSE

--- packages/iframe-wallet-poc/src/iframe/user.ts
+++ packages/iframe-wallet-poc/src/iframe/user.ts
@@ -7,6 +7,8 @@ import { CommitmentStore } from "../commitmentStore";
 import { IframeIoProvider } from "./ioProvider";
 import { IFrameWallet } from "./wallet";
 
+import { instructions } from "@counterfactual/machine";
+
 export let ganacheURL;
 
 try {
@@ -152,7 +154,7 @@ export class User implements machine.mixins.Observable, cf.node.ResponseSink {
   }
 
   public generateObserverNotification(notification: cf.node.Notification): any {
-    return notification.data.results.find(result => result.opCode === 0).value;
+    return notification.data.results.find(result => result.opCode === instructions.Opcode.STATE_TRANSITION_PROPOSE).value;
   }
 
   public addObserver(message: cf.node.ClientActionMessage) {
