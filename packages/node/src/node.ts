6ef6884a15324c7b3da8a59feff37a6a3aa8a468
cf19drofxots <nima@l4v.io>
2018-11-21 14:15:17 -0800

    Add channels to Node

--- packages/node/src/node.ts
+++ packages/node/src/node.ts
@@ -1,1 +1,6 @@
-export default class Node {}
+import * as cf from "@counterfactual/cf.js";
+
+export default class Node {
+  private channels: cf.legacy.channel.StateChannelInfos = {};
+  constructor() {}
+}
---
+++ packages/node/test/integration/machine-protocols.spec.ts
@@ -0,0 +1,13 @@
+import { Node } from "../../src";
+
+describe("Node can use the machine to execute protocols", () => {
+  let node;
+  beforeEach(() => {
+    node = new Node();
+    console.log(node);
+  });
+
+  it("can install an app", () => {});
+  it("can update an app", () => {});
+  it("can uninstall an app", () => {});
+});
