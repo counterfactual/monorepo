39e8ee3add67d9fb8565b78350d41d59ae4305a6
Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
2018-11-21 12:00:34 -0500

    WIP

---
+++ packages/cf.js/src/utils/abi.ts
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+
+export function encode(types: string[], values: any[]) {
+  return ethers.utils.defaultAbiCoder.encode(types, values);
+}
+
+export function decode(types: string[], data: ethers.utils.Arrayish) {
+  return ethers.utils.defaultAbiCoder.decode(types, data);
+}
+
+export function encodePacked(types: string[], values: any[]) {
+  return ethers.utils.solidityPack(types, values);
+}
