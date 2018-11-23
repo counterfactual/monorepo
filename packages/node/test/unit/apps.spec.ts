7bca5e0b8d1fdf277f6a7f27b6455d9d6a7794c9
cf19drofxots <nima@l4v.io>
2018-11-23 13:05:45 -0800

    Update apps tests

--- packages/node/test/unit/apps.spec.ts
+++ packages/node/test/unit/apps.spec.ts
@@ -1,14 +1,17 @@
+import { EventEmitter } from "events";
+
 import { Node } from "../../src/node";
 
-describe("Apps", () => {
+describe("Basic app operations with the Node", () => {
   let node: Node;
   beforeEach(() => {
     node = new Node();
   });
 
-  it("can open a connection with an app", () => {
+  it("can get an EventEmitter for an app", () => {
     const appInstanceEventEmitter = node.openApp("1");
     expect(appInstanceEventEmitter).toBeDefined();
+    expect(appInstanceEventEmitter).toBeInstanceOf(EventEmitter);
   });
 
   it("can get empty list of apps", () => {
