1d7f2b0f0e93d25d21d9cd1ce5bf24f5a0cc6296
Alon Bukai <alonzorz@gmail.com>
2018-11-23 14:58:51 +0200

    Add all dice to app-game and added Roll Dice action

--- packages/dapp-high-roller/src/components/app-game/app-game.tsx
+++ packages/dapp-high-roller/src/components/app-game/app-game.tsx
@@ -1,31 +1,42 @@
 import { Component, State } from "@stencil/core";
 
+const DARK_PATH = "./assets/images/dice/Dark/Dice-Dark-0";
+const LIGHT_PATH = "./assets/images/dice/Light/Dice-Light-0";
+
 @Component({
   tag: "app-game",
   styleUrl: "app-game.scss",
   shadow: true
 })
 export class AppGame {
-  @State() myRoll: number;
-  @State() opponentRoll: number;
+  @State() myRoll: number[];
+  @State() opponentRoll: number[];
 
   constructor() {
-    this.myRoll = 1;
-    this.opponentRoll = 1;
+    this.myRoll = [1, 1];
+    this.opponentRoll = [1, 1];
   }
   handleRoll() {
-    this.myRoll = 1 + Math.floor(Math.random() * Math.floor(6));
-    this.opponentRoll = 1 + Math.floor(Math.random() * Math.floor(6));
+    this.myRoll = [
+      1 + Math.floor(Math.random() * Math.floor(6)),
+      1 + Math.floor(Math.random() * Math.floor(6))
+    ];
+    this.opponentRoll = [
+      1 + Math.floor(Math.random() * Math.floor(6)),
+      1 + Math.floor(Math.random() * Math.floor(6))
+    ];
     console.log(
       `Lets roll! Mine: ${this.myRoll} - Opponent: ${this.opponentRoll}`
     );
+    const totalMyRoll = this.myRoll[0] + this.myRoll[1];
+    const totalOpponentRoll = this.opponentRoll[0] + this.opponentRoll[1];
     const alertText =
-      this.myRoll > this.opponentRoll
+      totalMyRoll > totalOpponentRoll
         ? "You won :)"
-        : this.myRoll < this.opponentRoll
+        : totalMyRoll < totalOpponentRoll
         ? "You lost :("
         : "Its a tie :|";
-    alert(alertText);
+    console.log(alertText);
   }
 
   render() {
@@ -42,8 +53,8 @@ export class AppGame {
             </div>
 
             <div class="player__dice">
-              <img src="./assets/images/dice.svg" alt="" />
-              <img src="./assets/images/dice.svg" alt="" />
+              <img src={`${DARK_PATH}${this.opponentRoll[0]}.svg`} alt="" />
+              <img src={`${DARK_PATH}${this.opponentRoll[1]}.svg`} alt="" />
             </div>
           </div>
 
@@ -61,8 +72,8 @@ export class AppGame {
             </div>
 
             <div class="player__dice">
-              <img src="./assets/images/dice.svg" alt="" />
-              <img src="./assets/images/dice.svg" alt="" />
+              <img src={`${LIGHT_PATH}${this.myRoll[0]}.svg`} alt="" />
+              <img src={`${LIGHT_PATH}${this.myRoll[1]}.svg`} alt="" />
             </div>
           </div>
 
