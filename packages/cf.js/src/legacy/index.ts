0e9d8e6b530b3bff7fc911c028655b9863a1fc77
Li Xuanji <xuanji@gmail.com>
2018-11-26 20:29:10 +0800

    delete unused exports in cf.legacy

--- packages/cf.js/src/legacy/index.ts
+++ packages/cf.js/src/legacy/index.ts
@@ -1,23 +1,15 @@
 import * as app from "./app";
-import { AppInstance } from "./app-instance";
-import { AppInstanceClient } from "./app-instance-client";
 import * as channel from "./channel";
-import { Client } from "./client";
 import { NotificationType } from "./mixins/observable";
 import * as network from "./network";
 import * as node from "./node";
-import * as types from "./types";
 import * as utils from "./utils";
 
 export {
   app,
-  AppInstance,
-  AppInstanceClient,
   channel,
-  Client,
   network,
   node,
   NotificationType,
-  types,
   utils
 };
