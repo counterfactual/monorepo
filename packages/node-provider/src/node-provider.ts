3347b44a247369953d93d56ca6e5e9afc8ed5dfa
Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
2018-11-28 11:54:12 -0500

    Fixy

--- packages/node-provider/src/node-provider.ts
+++ packages/node-provider/src/node-provider.ts
@@ -1,7 +1,7 @@
 import * as cf from "@counterfactual/cf.js";
 
 export default class NodeProvider implements cf.types.INodeProvider {
-  public onMessage(callback: (message: cf.types.NodeMessage) => void) {}
+  public onMessage(callback: (message: cf.types.Node.Message) => void) {}
 
-  public sendMessage(message: cf.types.NodeMessage) {}
+  public sendMessage(message: cf.types.Node.Message) {}
 }
