f740640fa76a4a690bf0e92b6866bed1f17339f7
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-11-28 20:07:41 -0300

    node-provider: added comments about `isConnected`

--- packages/node-provider/src/node-provider.ts
+++ packages/node-provider/src/node-provider.ts
@@ -2,6 +2,13 @@ import * as cf from "@counterfactual/cf.js";
 import EventEmitter from "eventemitter3";
 
 export default class NodeProvider implements cf.types.INodeProvider {
+  /**
+   * This boolean determines if the NodeProvider has received a MessagePort
+   * via the `cf-node-provider:port` message.
+   *
+   * It is used to prevent attempts to send messages without an instance
+   * of MessagePort stored locally.
+   */
   private isConnected: boolean;
   private eventEmitter: EventEmitter;
   private messagePort?: MessagePort;
@@ -17,6 +24,7 @@ export default class NodeProvider implements cf.types.INodeProvider {
 
   public sendMessage(message: cf.types.Node.Message) {
     if (!this.isConnected || !this.messagePort) {
+      // We fail because we do not have a messagePort available.
       throw new Error(
         "It's not possible to use postMessage() before the NodeProvider is connected. Call the connect() method first."
       );
@@ -34,6 +42,8 @@ export default class NodeProvider implements cf.types.INodeProvider {
     return new Promise<NodeProvider>((resolve, reject) => {
       window.addEventListener("message", event => {
         if (event.data === "cf-node-provider:port") {
+          // This message is received from the Playground to connect it
+          // to the dApp so they can exchange messages.
           this.startMessagePort(event);
           this.notifyNodeProviderIsConnected();
           resolve(this);
@@ -47,6 +57,9 @@ export default class NodeProvider implements cf.types.INodeProvider {
   private startMessagePort(event: MessageEvent) {
     this.messagePort = event.ports[0];
     this.messagePort.addEventListener("message", event => {
+      // Every message received by the messagePort will be
+      // relayed to whoever has subscribed to the "message"
+      // event using `onMessage()`.
       this.eventEmitter.emit("message", event.data);
     });
     this.messagePort.start();
