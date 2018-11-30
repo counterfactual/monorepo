f5ae948b7b4615d20f6aa6f620228a94c8fbc72d
cf19drofxots <nima@l4v.io>
2018-11-30 15:28:21 -0500

    Initial structure

--- packages/node/src/node.ts
+++ packages/node/src/node.ts
@@ -1,1 +1,96 @@
-export default class Node {}
+import machine from "@counterfactual/machine";
+import * as ethers from "ethers";
+import EventEmitter from "eventemitter3";
+import firebase from "firebase";
+
+import {
+  Address,
+  AppInstanceInfo,
+  GetAppInstancesResult,
+  MethodName,
+  MethodRequest,
+  MethodResponse
+} from "./node-types";
+
+export default class Node {
+  // @ts-ignore
+  private instructionExecutor: machine.InstructionExecutor = Object.create(
+    null
+  );
+
+  /**
+   * Because the Node receives and sends out messages based on Event type
+   * https://github.com/counterfactual/monorepo/blob/master/packages/cf.js/src/types/node-protocol.ts#L21-L33
+   * the same EventEmitter can't be used since responses messages would get
+   * sent to listeners expecting a request message.
+   **/
+  private incoming: EventEmitter;
+  private outgoing: EventEmitter;
+
+  constructor(
+    // @ts-ignore
+    private readonly privateKey: string,
+    // @ts-ignore
+    private readonly provider: ethers.providers.Web3Provider,
+    // @ts-ignore
+    private readonly firestore: firebase.firestore.Firestore,
+    // @ts-ignore
+    private readonly networkContext: Map<string, Address>
+  ) {
+    this.incoming = new EventEmitter();
+    this.outgoing = new EventEmitter();
+    this.registerListeners();
+  }
+
+  /**
+   * Create a multisig for the channel with the peer specified.
+   * @param peerAddress The peer to create the multisig with.
+   */
+  async createMultisig(peerAddress: Address): Promise<Address> {
+    // TODO:
+    return Promise.resolve(ethers.constants.AddressZero);
+  }
+
+  /**
+   * Delegates setting up a listener to the Node's outgoing EventEmitter.
+   * @param event
+   * @param callback
+   */
+  on(event: string, callback: (res: MethodResponse) => void) {
+    this.outgoing.on(event, callback);
+  }
+
+  /**
+   * Delegates emitting events to the Node's incoming EventEmitter.
+   * @param event
+   * @param req
+   */
+  emit(event: string, req: MethodRequest) {
+    this.incoming.emit(event, req);
+  }
+
+  /**
+   * This sets up all the listeners for the methods the Node is expected to have
+   * as described at https://github.com/counterfactual/monorepo/blob/master/packages/cf.js/API_REFERENCE.md#node-protocol
+   *
+   * The responses to these calls are the events being listened on
+   * https://github.com/counterfactual/monorepo/blob/master/packages/cf.js/API_REFERENCE.md#events
+   */
+  private registerListeners() {
+    this.incoming.on(MethodName.GET_APP_INSTANCES, (req: MethodRequest) => {
+      // TODO: empty for now
+      const res: MethodResponse = {
+        type: req.type,
+        requestId: req.requestId,
+        result: this.getAppInstances()
+      };
+      this.outgoing.emit(req.type, res);
+    });
+  }
+
+  private getAppInstances(): GetAppInstancesResult {
+    return {
+      appInstances: [] as AppInstanceInfo[]
+    };
+  }
+}
--- packages/node/test/unit/node.spec.ts
+++ packages/node/test/unit/node.spec.ts
@@ -1,12 +1,61 @@
-import Node from "../../src/node";
+import * as ethers from "ethers";
+import firebase from "firebase";
+import firebasemock from "firebase-mock";
 
-describe("Node", () => {
+import Node from "../../src/node";
+import {
+  Address,
+  AppInstanceInfo,
+  MethodName,
+  MethodRequest,
+  MethodResponse
+} from "../../src/node-types";
+
+describe("Node operations", () => {
+  const privateKey = process.env.npm_package_config_unlockedAccount0!;
+  const provider = ethers.getDefaultProvider() as ethers.providers.Web3Provider;
+  let firestore: firebase.firestore.Firestore;
+  let networkContext: Map<string, Address>;
+
+  beforeEach(() => {
+    firestore = new firebasemock.MockFirestore();
+    networkContext = new Map<string, Address>();
+    networkContext.set("AppRegistry", ethers.constants.AddressZero);
+  });
+
   it("exists", () => {
     expect(Node).toBeDefined();
   });
 
   it("can be instantiated", () => {
-    const node = new Node();
+    const node = new Node(privateKey, provider, firestore, networkContext);
     expect(node).toBeDefined();
+  });
+
+  it("can create a multisig", async () => {
+    const node = new Node(privateKey, provider, firestore, networkContext);
+    const multisig = await node.createMultisig(ethers.constants.AddressZero);
+    expect(multisig).toBeDefined();
+  });
+
+  it("can accept call to get app instances", done => {
+    const node = new Node(privateKey, provider, firestore, networkContext);
+    const requestId = "1";
+    const req: MethodRequest = {
+      requestId,
+      type: MethodName.GET_APP_INSTANCES,
+      params: {}
+    };
+
+    // Set up listener for the method response
+    node.on(req.type, (res: MethodResponse) => {
+      expect(res.result).toEqual({
+        appInstances: [] as AppInstanceInfo[]
+      });
+      done();
+    });
+
+    // Make the method call
+    node.emit(req.type, req);
   });
 });
