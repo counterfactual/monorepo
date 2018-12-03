5c0b7c8aea2f99bd0dd8fb83e283200fedb58010
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-03 10:29:51 -0300

    playground: moved to port 3334 to side-run with dapp

--- packages/playground/src/utils/app-list.ts
+++ packages/playground/src/utils/app-list.ts
@@ -5,7 +5,7 @@ export default {
   "0x822c045f6F5e7E8090eA820E24A5f327C4E62c96": {
     name: "High Roller",
     slug: "high-roller",
-    url: "/dapps/high-roller.html",
+    url: "http://localhost:3333",
     icon: "assets/icon/high-roller.svg"
   },
   "0xd545e82792b6EF2000908F224275ED0456Cf36FA": {
