8a892a3df71d157cd700772ef00f6a043c9fb486
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-03 10:29:08 -0300

    dapp-high-roller: added cf.js + nodeProvider

--- packages/dapp-high-roller/src/components/app-root/app-root.tsx
+++ packages/dapp-high-roller/src/components/app-root/app-root.tsx
@@ -1,6 +1,9 @@
 import { Component, Prop } from "@stencil/core";
 import { MatchResults } from "@stencil/router";
 
+import { Provider } from "@counterfactual/cf.js";
+import * as NodeProvider from "@counterfactual/node-provider";
+
 @Component({
   tag: "app-root",
   styleUrl: "app-root.scss",
@@ -9,6 +12,15 @@ import { MatchResults } from "@stencil/router";
 export class AppRoot {
   // TODO Tracking this issue: https://github.com/ionic-team/stencil-router/issues/77
   @Prop() match: MatchResults;
+
+  componentDidLoad() {
+    debugger;
+    const nodeProvider = new NodeProvider.NodeProvider();
+    (async () => {
+      await nodeProvider.connect();
+      const client = new Provider(nodeProvider);
+    })();
+  }
 
   render() {
     return (
