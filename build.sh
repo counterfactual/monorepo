c8b856d1a81cbf5fe166cf4ff3039a6a0aceb4f6
cf19drofxots <nima@l4v.io>
2018-12-06 17:05:23 -0500

    Build correctly fails when appropriate

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin
 packages="contracts common-types machine cf.js node node-provider playground dapp-high-roller"
 
+set -e
+
 for package in $packages; do
   echo ">>> Building package: $package"
   cd packages/$package
