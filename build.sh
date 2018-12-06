6d6d887b92f713a87cdcdfcc53a3ce3f035c183d
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-06 17:24:56 -0300

    monorepo: trying classic cd ../..

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin
+#!/usr/bin/bash
 packages="contracts common-types cf.js machine node node-provider playground dapp-high-roller"
 
 for package in $packages; do
   echo ">>> Building package: $package"
   cd packages/$package
   yarn build
-  cd -
+  cd ../..
 done
