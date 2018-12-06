80960a4b517c066f86643ceadf9dce50195c72bf
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-06 16:58:03 -0300

    dapp-high-roller: added jest setup to mock state tunnel

--- packages/dapp-high-roller/stencil.config.ts
+++ packages/dapp-high-roller/stencil.config.ts
@@ -1,12 +1,15 @@
 import { Config } from "@stencil/core";
 import { sass } from "@stencil/sass";
 
+import { resolve } from "path";
+
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
   globalStyle: "src/global/app.scss",
   plugins: [sass({ injectGlobalPaths: ["src/global/app.scss"] })],
   testing: {
+    setupTestFrameworkScriptFile: resolve(__dirname, "jest-setup.js"),
     setupFiles: ["./test-setup.js"]
   }
 };
