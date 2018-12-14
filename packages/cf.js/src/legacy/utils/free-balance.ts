b9e4c92590c99e1263f7ade5f49d3a2fdb454e92
Li Xuanji <xuanji@gmail.com>
2018-12-14 17:29:45 +0800

    import ethers (#342)

--- packages/cf.js/src/legacy/utils/free-balance.ts
+++ packages/cf.js/src/legacy/utils/free-balance.ts
@@ -1,4 +1,4 @@
-import * as ethers from "ethers";
+import { ethers } from "ethers";
 
 import { AppInterface, Terms } from "../app";
 import { NetworkContext } from "../network";
--- packages/cf.js/src/legacy/utils/index.ts
+++ packages/cf.js/src/legacy/utils/index.ts
@@ -1,4 +1,4 @@
-import * as ethers from "ethers";
+import { ethers } from "ethers";
 
 import { FreeBalance } from "./free-balance";
 import { Nonce } from "./nonce";
