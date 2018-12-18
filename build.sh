bc7a13c7b0c3a622cadbac2f0a4ae54095de4dfc
Liam Horne <liam@lihorne.com>
2018-12-18 17:00:09 +0000

    remove bold and normal in build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,10 @@
 
 set -e
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-
 packages="contracts common-types cf.js machine node node-provider playground dapp-high-roller"
 
 for package in $packages; do
-  echo "⚙️  Building package: ${bold}${package}${normal}"
+  echo "⚙️  Building package: ${package}"
   cd packages/${package}
   yarn build
   cd -
