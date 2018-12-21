1f617addcda021fb2a7505c84ff01a1c4fd73012
Liam Horne <liam@lihorne.com>
2018-12-21 21:33:29 +0000

    Add infrastructure to run ganache-cli tests in machine

---
+++ packages/machine/truffle.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require("dotenv-safe").config();
+
+module.exports = {
+  // working_directory: "../contracts",
+  // contracts_build_directory: "../contracts/build/contracts",
+  contracts_directory: "../contracts",
+  migrations_directory: "../contracts/migrations",
+  networks: {
+    machine: {
+      network_id: process.env.DEV_GANACHE_NETWORK_ID,
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.DEV_GANACHE_MNEMONIC,
+          `http://${process.env.DEV_GANACHE_HOST}:${process.env.DEV_GANACHE_PORT}/`
+        );
+      }
+    }
+  }
+};
