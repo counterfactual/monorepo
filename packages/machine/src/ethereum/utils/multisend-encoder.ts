487716e3213023769c4c9ce08c96ee9b93cd3386
Liam Horne <liam@lihorne.com>
2018-12-21 21:58:47 +0000

    Switch arrow notation to function for encodeTransactions

--- packages/machine/src/ethereum/utils/multisend-encoder.ts
+++ packages/machine/src/ethereum/utils/multisend-encoder.ts
@@ -4,9 +4,10 @@ import { MultisigTransaction } from "./types";
 
 const ENCODING = ["uint256", "address", "uint256", "bytes"];
 
-export const encodeTransactions = (txs: MultisigTransaction[]) =>
-  txs
+export function encodeTransactions(txs: MultisigTransaction[]) {
+  return txs
     .map(x =>
       defaultAbiCoder.encode(ENCODING, [x.operation, x.to, x.value, x.data])
     )
     .reduce((acc, v) => acc + v.substr(2), "0x");
+}
