254ec10a213febc555076b3502730aa016d33638
Alon Bukai <alonzorz@gmail.com>
2018-12-22 16:37:09 +0200

    test: tests for highRoller app from Start to Done

--- packages/contracts/test/integration/highRollerApp.spec.ts
+++ packages/contracts/test/integration/highRollerApp.spec.ts
@@ -1,21 +1,18 @@
-import { Contract /* ethers */ } from "ethers";
+import { Contract } from "ethers";
 import { AddressZero } from "ethers/constants";
-import {
-  BigNumber,
-  defaultAbiCoder /* solidityKeccak256 */
-} from "ethers/utils";
+import { BigNumber, defaultAbiCoder, solidityKeccak256 } from "ethers/utils";
 
-import { AbstractContract, buildArtifacts, expect } from "../../utils";
+import {
 import * as Utils from "../../utils/misc";
 
 const web3 = (global as any).web3;
 const { unlockedAccount } = Utils.setupTestEnv(web3);
 
-/* /// Returns the commit hash that can be used to commit to chosenNumber
+/// Returns the commit hash that can be used to commit to chosenNumber
 /// using appSalt
 function computeCommitHash(appSalt: string, chosenNumber: number) {
   return solidityKeccak256(["bytes32", "uint256"], [appSalt, chosenNumber]);
-} */
+}
 
 enum HighRollerStage {
   PRE_GAME,
@@ -56,7 +53,7 @@ function decodeAppState(encodedAppState: string): HighRollerAppState {
 }
 
 // Default value instead of null Bytes32
-const randomBytes32 =
+const nullValueBytes32 =
   "0xdfdaa4d168f0be935a1e1d12b555995bc5ea67bd33fce1bc5be0a1e0a381fc94";
 
 contract("HighRollerApp", (accounts: string[]) => {
@@ -79,8 +76,8 @@ contract("HighRollerApp", (accounts: string[]) => {
       const preState: HighRollerAppState = {
         playerAddrs: [AddressZero, AddressZero],
         stage: HighRollerStage.PRE_GAME,
-        salt: randomBytes32,
-        commitHash: randomBytes32,
+        salt: nullValueBytes32,
+        commitHash: nullValueBytes32,
         playerFirstNumber: 0,
         playerSecondNumber: 0
       };
@@ -88,12 +85,64 @@ contract("HighRollerApp", (accounts: string[]) => {
       const action: Action = {
         actionType: ActionType.START_GAME,
         number: 0,
-        actionHash: randomBytes32
+        actionHash: nullValueBytes32
       };
       const ret = await highRollerApp.functions.applyAction(preState, action);
 
       const state = decodeAppState(ret);
       expect(state.stage).to.be.eql(new BigNumber(1));
+    });
+    it("can commit to hash", async () => {
+      const preState: HighRollerAppState = {
+        playerAddrs: [AddressZero, AddressZero],
+        stage: HighRollerStage.COMMITTING_HASH,
+        salt: nullValueBytes32,
+        commitHash: nullValueBytes32,
+        playerFirstNumber: 0,
+        playerSecondNumber: 0
+      };
+
+      const numberSalt =
+        "0xdfdaa4d168f0be935a1e1d12b555995bc5ea67bd33fce1bc5be0a1e0a381fc90";
+      const playerFirstNumber = 1;
+      const hash = computeCommitHash(numberSalt, playerFirstNumber);
+
+      const action: Action = {
+        actionType: ActionType.COMMIT_TO_HASH,
+        number: 0,
+        actionHash: hash
+      };
+      const ret = await highRollerApp.functions.applyAction(preState, action);
+
+      const state = decodeAppState(ret);
+      expect(state.stage).to.be.eql(new BigNumber(2));
+      expect(state.commitHash).to.be.eql(hash);
+    });
+    it("can commit to num", async () => {
+      const numberSalt =
+        "0xdfdaa4d168f0be935a1e1d12b555995bc5ea67bd33fce1bc5be0a1e0a381fc90";
+      const playerFirstNumber = 1;
+      const hash = computeCommitHash(numberSalt, playerFirstNumber);
+
+      const preState: HighRollerAppState = {
+        playerAddrs: [AddressZero, AddressZero],
+        stage: HighRollerStage.COMMITTING_NUM,
+        salt: nullValueBytes32,
+        commitHash: hash,
+        playerFirstNumber: 0,
+        playerSecondNumber: 0
+      };
+
+      const action: Action = {
+        actionType: ActionType.COMMIT_TO_NUM,
+        number: 2,
+        actionHash: nullValueBytes32
+      };
+      const ret = await highRollerApp.functions.applyAction(preState, action);
+
+      const state = decodeAppState(ret);
+      expect(state.stage).to.be.eql(new BigNumber(3));
+      expect(state.playerSecondNumber).to.be.eql(new BigNumber(2));
     });
   });
 });
