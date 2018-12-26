adfbadf067d3cbaa2a906bbbcd01e9c8e69c5da2
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-26 13:04:48 -0300

    [playground] Implements basic infrastructure to support a ProposeInstall (#326)
    * playground: added symlinks to IIFEs for Node + deps
    
    * playground: added Node instance container
    
    * playground: started node-listener to show Node events' UI
    
    * playground: moved IAppDialogSettings to types' file
    
    * playground: added symlink to ethers
    
    * playground: loaded ethers as global dependency
    
    * playground: removed @stencil/state-tunnel
    
    * monorepo: updated yarn.lock
    
    * playground: fixed imports/exports for <node-listener>
    
    * playground: renamed data provider to .ts
    
    * playground: added basic structure for MessagingService
    
    * playground: exposed DappContainer#postOrQueueMessage()
    
    * playground: managed to import types from other packages
    
    * playground: implemented a type-compatible storeService
    
    * playground: added command for parallel run of pg+dapp
    
    * playground: renamed app-dialog to widget-dialog
    
    * playground: referenced widget-dialog on NodeListener
    
    * playground: implemented Firebase data provider
    
    * playground: integrated Firebase, rendered <node-listener>
    
    * playground: registered UI handlers for MethodNames
    
    * dapp-high-roller: implemented first call to proposeInstall()
    
    * playground: updated Node constructor call
    
    * playground: added necessary IIFE dependencies
    
    * playground: added symlink to cf.js build
    
    * playground: firebase service will cb() only if snapshot.val() is valued
    
    * playground: added non-existing firebase notice
    
    * playground, dapp-high-roller: added some TODOs and remarks
    
    * playground: fixed wrong typers for firebase and $onMessage
    
    * common-types: fixed linting issues
    
    * node: fixed linting issues
    
    * cf.js: fixed linting issues
    
    * playground: fixed linting issues
    
    * playground: added uuid as CDN dependency
    
    * playground: reinstated Firebase service provider
    
    * playground: disabled tests due to GoogleChrome/puppeteer#3683
    
    * playground: disabled E2E due to ionic-team/stencil#1297
    
    * dapp-high-roller: removed unnecessary retval, typed handleChange
    
    * dapp-high-roller: fixed Event type for handleChange

---
+++ packages/playground/src/components/widgets/widget-dialog/widget-dialog.tsx
@@ -0,0 +1,53 @@
+import { Component, Element, Event, EventEmitter, Prop } from "@stencil/core";
+
+@Component({
+  tag: "widget-dialog",
+  shadow: true
+})
+export class WidgetDialog {
+  @Element() el: HTMLStencilElement = {} as HTMLStencilElement;
+  @Prop({ mutable: true }) visible: boolean = false;
+  @Prop() dialogTitle: string = "";
+  @Prop() icon: string = "";
+  @Prop() content: JSX.Element = {} as JSX.Element;
+  @Prop() primaryButtonText: string = "";
+  @Prop() secondaryButtonText: string = "";
+
+  @Event() primaryButtonClicked: EventEmitter = {} as EventEmitter;
+  @Event() secondaryButtonClicked: EventEmitter = {} as EventEmitter;
+
+  primaryButtonClickedHandler() {
+    this.primaryButtonClicked.emit();
+  }
+
+  secondaryButtonClickedHandler() {
+    this.secondaryButtonClicked.emit();
+  }
+
+  render() {
+    return (
+      <dialog open={this.visible}>
+        <header>
+          {this.dialogTitle ? (
+            <h2>${this.dialogTitle}</h2>
+          ) : (
+            <img src={this.icon} />
+          )}
+        </header>
+        <main>{this.content}</main>
+        <footer>
+          <button onClick={() => this.primaryButtonClickedHandler()}>
+            {this.primaryButtonText}
+          </button>
+          {this.secondaryButtonText ? (
+            <button onClick={() => this.secondaryButtonClickedHandler()}>
+              ${this.secondaryButtonText}
+            </button>
+          ) : (
+            {}
+          )}
+        </footer>
+      </dialog>
+    );
+  }
+}
