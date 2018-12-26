e716dd19b40dca1d77d4617d165010ec9faf68c2
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-26 15:12:59 -0300

    playground-server: fixed env-var bug (see netlify/netlify-lambda#59)

--- packages/playground-server/src/api.ts
+++ packages/playground-server/src/api.ts
@@ -11,14 +11,11 @@ const router = new Router();
 
 if (process.env.NODE_ENV !== "development") {
   router.prefix("/.netlify/functions/api");
-  console.log("prefixing with /.netlify/functions/api");
 } else {
   router.prefix("/api");
-  console.log("prefixing with /api");
 }
 
 router.get("/hello", async (ctx, next) => {
-  console.log("entering middleware: hello");
   ctx.body = { hello: ctx.request.query.name };
   ctx.status = 200;
   return next();
