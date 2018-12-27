17d86628ddbb75f08632d4ee9567ccaa4d48a289
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-27 18:51:52 -0300

    playground-server: updated tests

--- packages/playground-server/test/api.spec.ts
+++ packages/playground-server/test/api.spec.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { Server } from "http";
+import jwt from "jsonwebtoken";
 
 import { app as api } from "../src/api";
+import { FirebaseConfigurationResponse, TokenResponse } from "../src/types";
 
 let server: Server;
 const client = axios.create({
@@ -15,13 +17,51 @@ describe("playground-server", () => {
   beforeEach(async () => {
     server = await api.listen(9001);
   });
-  it("exposes /api/hello", async () => {
-    const response = await client.get("/hello", {
-      params: {
-        name: "Alice"
-      }
+  describe("Public API", () => {
+    it("exposes /token", async done => {
+      const response = await client.get<TokenResponse>("/token", {
+        params: {
+          name: "Alice"
+        }
+      });
+      jwt.verify(response.data.token, "foo", (err, token) => {
+        if (!err) {
+          done();
+        }
+      });
     });
-    expect(response.data.hello).toEqual("Alice");
+  });
+  describe("Private API", () => {
+    let token;
+    beforeEach(async () => {
+      token = (await client.get<TokenResponse>("/token", {
+        params: {
+          name: "Alice"
+        }
+      })).data.token;
+    });
+    it("returns 205 when requesting without a token", async () => {
+      const response = await client.get<FirebaseConfigurationResponse>(
+        "/firebase"
+      );
+      expect(response.status).toEqual(205);
+    });
+    it("exposes /firebase", async () => {
+      const response = await client.get<FirebaseConfigurationResponse>(
+        "/firebase",
+        {
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        }
+      );
+      expect(response.data.apiKey).toEqual("a");
+      expect(response.data.authDomain).toEqual("b");
+      expect(response.data.databaseURL).toEqual("c");
+      expect(response.data.projectId).toEqual("d");
+      expect(response.data.storageBucket).toEqual("e");
+      expect(response.data.messagingSenderId).toEqual("f");
+    });
   });
   afterEach(() => {
     server.close();
