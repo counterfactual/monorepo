faa1cd25e543eaca937b61a529bd8333bfa0b5fd
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-27 19:21:14 -0300

    playground-server: fixed terms' order in expiredTokenControl

--- packages/playground-server/src/middlewares/expired-token-control.ts
+++ packages/playground-server/src/middlewares/expired-token-control.ts
@@ -3,7 +3,7 @@ import { Context } from "koa";
 export default function expiredTokenControl() {
   return async function(ctx: Context, next: () => Promise<any>) {
     return next().catch(err => {
-      if (401 === err.status) {
+      if (err.status === 401) {
         ctx.status = 205;
         ctx.body = { error: "TOKEN_EXPIRED" };
       } else {
