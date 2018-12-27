827de6ec644e7c01448cab63c7af841984f7c699
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-27 08:57:04 -0300

    playground-server: replace NODE_ENV with PLAYGROUND_SERVER_ENV

--- packages/playground-server/src/api.ts
+++ packages/playground-server/src/api.ts
@@ -1,15 +1,14 @@
-import serverless from "serverless-http";
-
 import cors from "@koa/cors";
 import Koa from "koa";
 import bodyParser from "koa-body";
 import Router from "koa-router";
+import serverless from "serverless-http";
 
 const app = new Koa();
 
 const router = new Router();
 
-if (process.env.NODE_ENV !== "development") {
+if (process.env.PLAYGROUND_SERVER_ENV !== "development") {
   router.prefix("/.netlify/functions/api");
 } else {
   router.prefix("/api");
