35ad338cca1eaaf1e98ad3a25a1e171f59835d91
Liam Horne <snario@users.noreply.github.com>
2018-12-27 18:38:12 +0000

    [cf.js] Remove all usage of cf.js/legacy (#379)
    * Remove all usage of legacy
    
    * delete generateAbiEncodings

--- packages/cf.js/src/legacy/index.ts
+++
@@ -1,7 +0,0 @@
-import * as app from "./app";
-import * as channel from "./channel";
-import { NotificationType } from "./mixins/observable";
-import * as node from "./node";
-import * as utils from "./utils";
-
-export { app, channel, node, NotificationType, utils };
