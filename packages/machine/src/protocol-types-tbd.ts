48daafd5f19fd31ba14a6c59663d96b9695c1b17
Li Xuanji <xuanji@gmail.com>
2018-12-28 13:30:15 +0800

    fix misses

--- packages/machine/src/protocol-types-tbd.ts
+++ packages/machine/src/protocol-types-tbd.ts
@@ -54,7 +54,7 @@ export type UninstallParams = {
   bobBalanceIncrement: BigNumber;
 };
 
-export type MetaChannelInstallAppData = {
+export type InstallVirtualAppData = {
   /* TODO: @xuanji */
 };
 
@@ -63,4 +63,4 @@ type ProtocolParameters =
   | UpdateData
   | InstallParams
   | UninstallParams
-  | MetaChannelInstallAppData;
+  | InstallVirtualAppParams;
