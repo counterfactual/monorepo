5f30251a407bb05191b2a15716c9113e1f26c0ab
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2018-12-28 17:03:07 -0300

    playground-server: instantiated Node

---
+++ packages/playground-server/src/node.ts
@@ -0,0 +1,37 @@
+import {
+  FirebaseServiceFactory,
+  Node,
+  NodeMessage
+} from "@counterfactual/node";
+import { Node as NodeTypes } from "@counterfactual/types";
+
+const { INSTALL, REJECT_INSTALL } = NodeTypes.EventName;
+const { CREATE_MULTISIG } = NodeTypes.MethodName;
+
+const serviceFactory = new FirebaseServiceFactory({
+  apiKey: "AIzaSyA5fy_WIAw9mqm59mdN61CiaCSKg8yd4uw",
+  authDomain: "foobar-91a31.firebaseapp.com",
+  databaseURL: "https://foobar-91a31.firebaseio.com",
+  projectId: "foobar-91a31",
+  storageBucket: "foobar-91a31.appspot.com",
+  messagingSenderId: "432199632441"
+});
+
+const node = new Node(
+  process.env.NODE_PRIVATE_KEY as string,
+  serviceFactory.createMessagingService("messaging"),
+  serviceFactory.createStoreService("storage"),
+  {
+    STORE_KEY_PREFIX: "store"
+  }
+);
+
+node.on(INSTALL, async (msg: NodeMessage) => {
+  console.log("INSTALL event:", msg);
+});
+
+node.on(REJECT_INSTALL, async (msg: NodeMessage) => {
+  console.log("REJECT_INSTALL event:", msg);
+});
+
+export default node;
