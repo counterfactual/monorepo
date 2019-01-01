fc885739b7c7a314001876795e0835625520eaa9
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-01 17:58:58 -0300

    playground-server: added port as ENV

--- packages/playground-server/src/index.ts
+++ packages/playground-server/src/index.ts
@@ -3,8 +3,9 @@ import node from "./node";
 
 (async () => {
   const api = mountApi(node);
-  await api.listen(9000);
-  console.log("API listening on :9000");
+  const { PLAYGROUND_SERVER_PORT } = process.env;
+  await api.listen(PLAYGROUND_SERVER_PORT);
+  console.log(`API listening on :${PLAYGROUND_SERVER_PORT}`);
   console.log("Node address:", node.address);
 })();
 
