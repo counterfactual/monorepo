a5b2c86c7be2c7eb062f9c8550bbdd77a083a746
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-02 15:00:40 -0300

    playground-server: renamed PLAYGROUND_SERVER_PORT to PORT

--- packages/playground-server/src/index.ts
+++ packages/playground-server/src/index.ts
@@ -3,9 +3,9 @@ import node from "./node";
 
 (async () => {
   const api = mountApi(node);
-  const { PLAYGROUND_SERVER_PORT } = process.env;
-  await api.listen(PLAYGROUND_SERVER_PORT);
-  console.log(`API listening on :${PLAYGROUND_SERVER_PORT}`);
+  const { PORT } = process.env;
+  await api.listen(PORT);
+  console.log(`API listening on :${PORT}`);
   console.log("Node address:", node.address);
 })();
 
