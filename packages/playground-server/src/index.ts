38e3d16e9812234ae21adcbf26e149f00a91573d
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-02 15:03:28 -0300

    playground-server: added default API port

--- packages/playground-server/src/index.ts
+++ packages/playground-server/src/index.ts
@@ -3,9 +3,9 @@ import node from "./node";
 
 (async () => {
   const api = mountApi(node);
-  const { PORT } = process.env;
-  await api.listen(PORT);
-  console.log(`API listening on :${PORT}`);
+  const port = process.env.PORT || 9000;
+  await api.listen(port);
+  console.log(`API listening on :${port}`);
   console.log("Node address:", node.address);
 })();
 
