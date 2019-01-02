240daadadcc38dbd3ad9ee99a8a9796d173bcbb7
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-02 19:07:41 -0300

    playground-server: made "signature" required in CreateAccountRequest

--- packages/playground-server/src/types.ts
+++ packages/playground-server/src/types.ts
@@ -4,7 +4,7 @@ export type CreateAccountRequest = {
   username: string;
   email: string;
   address: Address;
-  signature?: string;
+  signature: string;
 };
 
 export type ErrorResponse = {
@@ -16,6 +16,7 @@ export enum ErrorCode {
   UsernameRequired = "username_required",
   EmailRequired = "email_required",
   AddressRequired = "address_required",
+  SignatureRequired = "signature_required",
   InvalidSignature = "invalid_signature"
 }
 
