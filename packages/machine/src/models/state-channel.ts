7427fbfda859e9a6ce89e5b00a18518e38caf621
Liam Horne <snario@users.noreply.github.com>
2019-01-01 18:29:09 -0800

    Implement root nonce functonality in codebase (#410)
    * Implement root nonce functonality in codebase
    
    * Compute key for NonceRegistry correctly

--- packages/machine/src/models/state-channel.ts
+++ packages/machine/src/models/state-channel.ts
@@ -18,6 +18,8 @@ import { AppInstance } from "./app-instance";
 const HARD_CODED_ASSUMPTIONS = {
   freeBalanceDefaultTimeout: 10,
   freeBalanceInitialStateTimeout: 10,
+  // We assume the Free Balance is installed when the Root Nonce value is 0
+  rootNonceValueAtFreeBalanceInstall: 0,
   // We assume the Free Balance is the first app ever installed
   appSequenceNumberForFreeBalance: 0
 };
@@ -52,6 +54,7 @@ function createETHFreeBalance(
     freeBalanceTerms,
     false,
     HARD_CODED_ASSUMPTIONS.appSequenceNumberForFreeBalance,
+    HARD_CODED_ASSUMPTIONS.rootNonceValueAtFreeBalanceInstall,
     {
       alice: signingKeyForFreeBalanceForPerson1,
       bob: signingKeyForFreeBalanceForPerson2,
@@ -75,7 +78,8 @@ export class StateChannel {
       AssetType,
       string
     > = new Map<AssetType, string>([]),
-    private readonly monotonicNumInstalledApps: number = 0
+    private readonly monotonicNumInstalledApps: number = 0,
+    public readonly rootNonceValue: number = 0
   ) {}
 
   public get numInstalledApps() {
