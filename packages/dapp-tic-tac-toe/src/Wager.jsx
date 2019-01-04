0e8df225419cd7bf8b046320ed770b1eb4a2b0e1
Erik Bryn <erik.bryn@gmail.com>
2019-01-04 09:36:31 -0800

    Add the beginnings of a Tic Tac Toe Dapp example using create-react-app

---
+++ packages/dapp-tic-tac-toe/src/Wager.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ReactComponent as Logo } from './assets/images/logo.svg';
+import { Link } from "react-router-dom";
+
+class Wager extends Component {
+  render() {
+    return (
+      <div className="wager">
+        <div className="message">
+          <Logo />
+          <h1 className="message__title">Lorem ipsum dolor</h1>
+          <p className="message__body">Phasellus nec sem id felis rutrum iaculis non non lorem.</p>
+        </div>
+  
+        <form className="form">
+          <input className="form__input" type="text" placeholder="Your name" />
+          <input className="form__input" type="text" placeholder="3 eth" />
+          <Link to="/game" className="form__button">PLAY!</Link>
+        </form>  
+      </div>
+    );
+  }
+}
+
+export default Wager;
