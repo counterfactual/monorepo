f3db109c1adf43ab0f22df6243b57e1c04285a28
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-07 14:22:15 -0300

    typescript-typings: expanded web3 API definitions as needed

--- packages/typescript-typings/types/web3/index.d.ts
+++ packages/typescript-typings/types/web3/index.d.ts
@@ -1,6 +1,30 @@
 // API matches version 0.20.3 of Web3
 
+declare class BigNumber {
+  constructor(num: string);
+  toNumber(): number;
+}
+
 declare var web3: {
+  fromWei: (amount: number, unit: string) => string;
+  toWei: (amount: BigNumber, unit: string) => string | BigNumber;
+  BigNumber: (num: string) => void;
+  eth: {
+    defaultBlock: number | string;
+    getBalance: (
+      address: string,
+      defaultBlock?: number | string,
+      callback?: (error: Error, balance: BigNumber) => void
+    ) => void;
+    sendTransaction: (
+      transactionObject: {
+        from: string | number;
+        to?: string;
+        value: number | string | BigNumber;
+      },
+      callback: (err: Error, result: any) => void
+    ) => void;
+  };
   personal: {
     sign: (
       dataToSign: string,
