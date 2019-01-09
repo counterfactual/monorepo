414f4ad636a3f1ac85f728fa8010ceddd26f1565
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-09 18:34:22 -0300

    playground-server: injected user into Koa context, fixed header parsing

--- packages/playground-server/src/middleware/authentication.ts
+++ packages/playground-server/src/middleware/authentication.ts
@@ -1,26 +1,34 @@
 import { decode } from "jsonwebtoken";
-import { Context } from "koa";
 
 import { userExists } from "../db";
-import { ErrorCode, PlaygroundUser } from "../types";
+import { AuthenticatedContext, ErrorCode, PlaygroundUser } from "../types";
 
 export default function authentication(...protectedRoutes: string[]) {
-  return async (ctx: Context, next: () => Promise<void>) => {
+  return async (ctx: AuthenticatedContext, next: () => Promise<void>) => {
     if (!protectedRoutes.includes(ctx.path)) {
       return next();
     }
 
-    if (!ctx.headers["Authorization"]) {
+    const authHeader = ctx.headers["authorization"] as string;
+
+    if (!authHeader) {
       throw ErrorCode.TokenRequired;
     }
 
-    const token = ctx.headers["Authorization"] as string;
+    if (!authHeader.startsWith("Bearer ")) {
+      throw ErrorCode.InvalidToken;
+    }
+
+    const [, token] = authHeader.split(" ");
     const user = decode(token) as PlaygroundUser;
+
     const isValidUser = await userExists(user);
 
     if (!isValidUser) {
       throw ErrorCode.InvalidToken;
     }
+
+    ctx.user = user;
 
     return next();
   };
