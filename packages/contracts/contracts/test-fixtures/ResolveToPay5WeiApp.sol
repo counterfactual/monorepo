5f15f118b9a7f8e17a35c088988ab5a35c895825
Liam Horne <liam@lihorne.com>
2019-01-09 13:43:52 -0800

    Make modifications to ResolveToPay5WeiApp test

--- packages/contracts/contracts/test-fixtures/ResolveToPay5WeiApp.sol
+++ packages/contracts/contracts/test-fixtures/ResolveToPay5WeiApp.sol
@@ -2,17 +2,17 @@ pragma solidity 0.5;
 pragma experimental "ABIEncoderV2";
 
 import "../libs/Transfer.sol";
-
+import "../CounterfactualApp.sol";
 
 /// @title ResolveToPay5WeiApp
 /// @notice This contract is a test fixture meant to emulate an AppInstance
-/// contract. An AppInstance has a getResolution() function that returns a
+/// contract. An AppInstance has a resolve() function that returns a
 /// `Transfer.Transaction` object when the channel is closed.
-contract ResolveToPay5WeiApp {
+contract ResolveToPay5WeiApp is CounterfactualApp {
 
-  function getResolution()
+  function resolve(bytes memory encodedState, Transfer.Terms memory terms)
     public
-    view
+    pure
     returns (Transfer.Transaction memory)
   {
     uint256[] memory amounts = new uint256[](1);
@@ -30,5 +30,29 @@ contract ResolveToPay5WeiApp {
       amounts,
       data
     );
+  }
+
+  function isStateTerminal(bytes memory)
+    public
+    pure
+    returns (bool)
+  {
+    revert("Not implemented");
+  }
+
+  function getTurnTaker(bytes memory, address[] memory)
+    public
+    pure
+    returns (address)
+  {
+    revert("Not implemented");
+  }
+
+  function applyAction(bytes memory, bytes memory)
+    public
+    pure
+    returns (bytes memory)
+  {
+    revert("Not implemented");
   }
 }
