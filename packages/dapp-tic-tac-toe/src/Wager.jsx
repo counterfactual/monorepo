9fba00256e914e6de6e908c69893efb287f495f5
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-10 13:39:01 -0300

    [dapp-tic-tac-toe] Implements /api/matchmake call + rewires ProposeInstall flow (#490)
    * dapp-tic-tac-toe: added call to /matchmake (must remove hardcoded token)
    
    * dapp-tic-tac-toe: refactored proposeInstall/install, tied to matchmaking
    
    * dapp-tic-tac-toe: cleaned up Waiting component
    
    * dapp-tic-tac-toe: cleand up Wager component
    
    * dapp-tic-tac-toe: killed promises on fetch with async/await + try/catch
    
    * dapp-tic-tac-toe: removed shouldMatchmake, install()
    
    * dapp-tic-tac-toe: rewired Waiting as state of Wager

--- packages/dapp-tic-tac-toe/src/Wager.jsx
+++ packages/dapp-tic-tac-toe/src/Wager.jsx
@@ -1,22 +1,180 @@
-import React, { Component } from 'react';
-import { ReactComponent as Logo } from './assets/images/logo.svg';
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { ReactComponent as Logo } from "./assets/images/logo.svg";
+import Waiting from "./Waiting";
 
 class Wager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      isWaiting: false,
+      user: {},
+      opponent: {},
+      intermediary: null
+    };
+  }
+
+  async componentDidMount() {
+    this.props.cfProvider.once("install", this.onInstall.bind(this));
+
+    // TODO: This token should be obtained from LocalStorage.
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5OGRhZTNmLWNmYjctNGNmNC05OTZiLWZiNDI5NDI3ZGQ4NSIsInVzZXJuYW1lIjoiam9lbCIsImVtYWlsIjoiZXN0dWRpb0Bqb2VsYWxlamFuZHJvLmNvbSIsImFkZHJlc3MiOiIweDBmNjkzY2M5NTZkZjU5ZGVjMjRiYjFjNjA1YWM5NGNhZGNlNjAxNGQiLCJtdWx0aXNpZ0FkZHJlc3MiOiIweDE0NTczMjUzMTkxRDJDMjUxQTg1Y0JBMTQ1NjY0RWUwYUViNDA4NjgiLCJpYXQiOjE1NDcwODU4MTcsImV4cCI6MTU3ODY0MzQxN30.AQ-ataiWl9emPRWtHVinEXYgyHHZquP9DOXLjmcTKJI";
+
+    try {
+      const response = await fetch(
+        "https://server.playground-staging.counterfactual.com/api/matchmake",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      const result = await response.json();
+
+      this.setState({
+        isLoaded: true,
+        user: result.data.user,
+        opponent: result.data.opponent,
+        intermediary: result.data.intermediary,
+        error: result.error
+      });
+    } catch (error) {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  }
+
+  createAppFactory() {
+    return new window.cf.AppFactory(
+      // TODO: provide valid appId
+      "0x1515151515151515151515151515151515151515",
+      {
+        actionEncoding:
+          "tuple(ActionType actionType, uint256 playX, uint256 playY, WinClaim winClaim)",
+        stateEncoding:
+          "tuple(address[2] players, uint256 turnNum, uint256 winner, uint256[3][3] board)"
+      },
+      this.props.cfProvider
+    );
+  }
+
+  /**
+   * Bob(Proposing) makes a call to CF.js proposeInstall.
+   * Bob(Proposing) waits for Alice(Accepting) to approve -- Add Waiting Room (Waiting for Alice) --
+   */
+  async proposeInstall(user, opponent, intermediary) {
+    const myAddress = user.address;
+    const appFactory = this.createAppFactory();
+
+    appFactory.proposeInstallVirtual({
+      peerAddress: opponent.address,
+      asset: {
+        assetType: 0 /* AssetType.ETH */
+      },
+      peerDeposit: window.ethers.utils.parseEther(
+        this.props.gameInfo.betAmount
+      ),
+      myDeposit: window.ethers.utils.parseEther(this.props.gameInfo.betAmount),
+      timeout: 100,
+      initialState: {
+        address: [myAddress, opponent.address],
+        turnNum: 0,
+        winner: 0,
+        board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+      },
+      intermediaries: [intermediary]
+    });
+  }
+
+  onInstall({ data: { appInstance } }) {
+    this.props.onChangeAppInstance(appInstance);
+    this.props.history.push(`/game?appInstanceId=${appInstance.id}`);
+  }
+
+  onPlayClicked() {
+    const { user, opponent, intermediary } = this.state;
+
+    this.setState({ isWaiting: true });
+
+    this.proposeInstall(user, opponent, intermediary);
+  }
+
   render() {
+    const { error, isLoaded, user, isWaiting } = this.state;
+
+    if (error) {
+      return (
+        <div className="wager">
+          <div className="message">
+            <Logo />
+            <h1 className="message__title">Oops! :/</h1>
+            <p className="message__body">Something went wrong:</p>
+            <textarea
+              rows={10}
+              cols={60}
+              defaultValue={
+                error.errorCode
+                  ? JSON.stringify(error)
+                  : `${error.message}\n${error.stack}`
+              }
+            />
+          </div>
+        </div>
+      );
+    }
+
+    if (!isLoaded) {
+      return (
+        <div className="wager">
+          <div className="message">
+            <Logo />
+            <h1 className="message__title">Getting ready...</h1>
+            <p className="message__body">Just a couple of seconds!</p>
+          </div>
+        </div>
+      );
+    }
+
+    if (isWaiting) {
+      return <Waiting gameInfo={this.props.gameInfo} />;
+    }
+
     return (
       <div className="wager">
         <div className="message">
           <Logo />
-          <h1 className="message__title">Lorem ipsum dolor</h1>
-          <p className="message__body">Phasellus nec sem id felis rutrum iaculis non non lorem.</p>
+          <h1 className="message__title">Welcome!</h1>
+          <p className="message__body">Ready to play?</p>
         </div>
-  
+
         <form className="form">
-          <input className="form__input" type="text" placeholder="Your name" disabled={true} value={this.props.gameInfo.myName} />
-          <input className="form__input" type="text" placeholder="3 eth" disabled={true} value={this.props.gameInfo.betAmount}  />
-          <Link to="/waiting" className="form__button">PLAY!</Link>
-        </form>  
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Your name"
+            disabled={true}
+            value={user.username}
+          />
+          <input
+            className="form__input"
+            type="text"
+            placeholder="3 eth"
+            disabled={true}
+            value={this.props.gameInfo.betAmount}
+          />
+          <button
+            type="button"
+            onClick={this.onPlayClicked.bind(this)}
+            className="form__button"
+          >
+            PLAY!
+          </button>
+        </form>
       </div>
     );
   }
