f3a1d09828b648feed4c9804de6bdf9a5adff08f
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-11 20:07:51 -0300

    playground-server: added middleware logs

--- packages/playground-server/src/middleware/signature.ts
+++ packages/playground-server/src/middleware/signature.ts
@@ -1,6 +1,7 @@
 import { getAddress, verifyMessage } from "ethers/utils";
 import { Context } from "koa";
 import "koa-body"; // Needed for ctx.request.body to be detected by TS, see: https://github.com/dlau/koa-body/issues/109
+import { Log } from "logepi";
 
 import {
   CreateAccountRequest,
@@ -34,10 +35,16 @@ export default function signatureValidator() {
 
     if (!(ctx.path in buildPayloadFor)) {
       // It's not a signed request. Don't do anything with it.
+      Log.debug("Skipping signature validation, it's not a signed request", {
+        tags: { endpoint: ctx.path, middleware: "signatureValidator" }
+      });
       return next();
     }
 
     if (!("signature" in body)) {
+      Log.info("Signature missing, leaving", {
+        tags: { endpoint: ctx.path, middleware: "signatureValidator" }
+      });
       throw ErrorCode.SignatureRequired;
     }
 
@@ -49,8 +56,15 @@ export default function signatureValidator() {
     const expectedAddress = verifyMessage(expectedMessage, providedSignature);
 
     if (providedAddress !== expectedAddress) {
+      Log.info("Signature is not valid, leaving", {
+        tags: { endpoint: ctx.path, middleware: "signatureValidator" }
+      });
       throw ErrorCode.InvalidSignature;
     }
+
+    Log.info("Signature is OK, moving on", {
+      tags: { endpoint: ctx.path, middleware: "signatureValidator" }
+    });
 
     return next();
   };
