b03f4ce813eddd9c3b6b96492263392127974ff5
cf19drofxots <nima@l4v.io>
2019-01-12 22:29:08 -0500

    Lint

--- packages/node/src/methods/state-channel/create/controller.ts
+++ packages/node/src/methods/state-channel/create/controller.ts
@@ -25,7 +25,9 @@ export default async function createMultisigController(
     this.networkContext
   );
 
-  const [respondingAddress] = params.owners.filter(owner => owner !== this.address);
+  const [respondingAddress] = params.owners.filter(
+    owner => owner !== this.address
+  );
 
   const multisigCreatedMsg: NodeMessage = {
     from: this.address,
