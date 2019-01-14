92f0fa879a060668ea69bf129e5eb9ed56a059b7
Alon Bukai <alonzorz@gmail.com>
2019-01-14 20:10:42 +0200

    testing: add global.window mock for stencil tests in high roller

--- packages/dapp-high-roller/test-setup.js
+++ packages/dapp-high-roller/test-setup.js
@@ -1,7 +1,7 @@
 global.XMLHttpRequest = {
   open: () => {},
-  send: () => {},
-}
+  send: () => {}
+};
 global.cuid = require("../../node_modules/cuid/index");
 global.types = require("../types/dist/index-iife");
 global.ethers = require("../../node_modules/ethers/dist/ethers");
@@ -9,3 +9,5 @@ global.EventEmitter3 = require("../node-provider/node_modules/eventemitter3/umd/
 global.EventEmitter = global.EventEmitter3.EventEmitter;
 global.cf = require("../cf.js/dist/index-iife");
 global.NodeProvider = require("../node-provider/dist/index-iife").NodeProvider;
+
+global.window = { location: {} };
