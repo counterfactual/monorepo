6c82915dd5cf4d8b9bf0464f201853588a50dfde
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-15 20:35:45 -0300

    playground-server: injected new controllers

--- packages/playground-server/src/api.ts
+++ packages/playground-server/src/api.ts
@@ -1,41 +1,35 @@
 import cors from "@koa/cors";
 import Koa from "koa";
 import bodyParser from "koa-body";
-import Router from "koa-router";
 import { KoaLoggingMiddleware as logs } from "logepi";
 import path from "path";
 
 // API Controllers
-import createAccount from "./controllers/create-account";
-import getApps from "./controllers/get-apps";
-import getUser from "./controllers/get-user";
-import login from "./controllers/login";
-import matchmake from "./controllers/matchmake";
-// Middlewares
-import authentication from "./middleware/authentication";
-import errorHandler from "./middleware/error";
-import signatureValidator from "./middleware/signature";
+import AppsController from "./controllers/apps";
+import MatchmakingController from "./controllers/matchmaking";
+import SessionController from "./controllers/session";
+import UsersController from "./controllers/users";
+import Router from "./router";
+import { AppsControllerOptions } from "./types";
 
 export default function mountApi() {
   const api = new Koa();
 
   const router = new Router({ prefix: "/api" });
 
-  router.post("/create-account", createAccount());
-  router.post("/login", login());
-
-  router.post("/matchmake", matchmake());
-  router.get("/user", getUser());
-
-  router.get("/apps", getApps(path.resolve(__dirname, "../registry.json")));
-
   api
-    .use(errorHandler(api))
     .use(bodyParser({ json: true }))
     .use(cors({ keepHeadersOnError: false }))
-    .use(signatureValidator())
-    .use(authentication("/api/matchmake", "/api/user"))
-    .use(router.routes())
+    .use(
+      router
+        .inject<AppsControllerOptions>(AppsController, "apps", {
+          registryPath: path.resolve(__dirname, "../registry.json")
+        })
+        .inject(UsersController, "users")
+        .inject(SessionController, "session")
+        .inject(MatchmakingController, "matchmaking")
+        .routes()
+    )
     .use(logs());
 
   return api;
