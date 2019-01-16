294754384baa2afdf2bc0d556901054892305f3a
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-16 12:42:38 -0300

    dapp-high-roller: fixed URL for matchmaking

--- packages/dapp-high-roller/src/components/app-wager/app-wager.tsx
+++ packages/dapp-high-roller/src/components/app-wager/app-wager.tsx
@@ -79,8 +79,7 @@ export class AppWager {
     try {
       const response = await fetch(
         // TODO: This URL must come from an environment variable.
-        // "https://server.playground-staging.counterfactual.com/api/matchmake",
-        "http://localhost:9000/api/matchmaking",
+        "https://server.playground-staging.counterfactual.com/api/matchmaking",
         {
           method: "POST",
           headers: {
