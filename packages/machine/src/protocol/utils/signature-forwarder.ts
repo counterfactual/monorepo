5e4d42d7111833df0c024f5e2cd8488a91b4b19e
Liam Horne <liam@lihorne.com>
2019-01-16 13:26:30 -0800

    Add docstrings for signature forwarder methods

--- packages/machine/src/protocol/utils/signature-forwarder.ts
+++ packages/machine/src/protocol/utils/signature-forwarder.ts
@@ -1,6 +1,15 @@
 import { ProtocolMessage } from "../../protocol-types-tbd";
 import { Context } from "../../types";
 
+/**
+ * @summary Appends a `ProtocolMessage` to the outbox of a `Context
+ * with `seq` hard-coded to 1. This is useful for protocols like
+ * Setup, Install, Update, and Uninstall which are single-round-trip
+ * based and thus A will use this to generate the message to send to B.
+ *
+ * @param message the message A initiated his machine protocol execution with
+ * @param context A's context at this point in the protocol execution
+ */
 export function addSignedCommitmentToOutboxForSeq1(
   message: ProtocolMessage,
   context: Context
@@ -12,6 +21,17 @@ export function addSignedCommitmentToOutboxForSeq1(
   });
 }
 
+/**
+ * @summary Appends a `ProtocolMessage` to the outbox of a `Context
+ * with `seq` hard-coded to 2. This is useful for protocols like
+ * Setup, Install, Update, and Uninstall which are single-round-trip
+ * based. In this case, B uses this to respond back to A. There is no
+ * seq 2 for these kinds of protocols, so this is an alias for "use this
+ * protocol message to finish up your callback on the IO_WAIT opcode"
+ *
+ * @param message the message B initiated his machine protocol execution
+ * @param context B's context at this point in the protocol execution
+ */
 export function addSignedCommitmentInResponseWithSeq2(
   message: ProtocolMessage,
   context: Context
