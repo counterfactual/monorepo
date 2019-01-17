d756ea0807e57bb62b5c59eb2fd24f431b789055
Liam Horne <liam@lihorne.com>
2019-01-17 12:37:02 -0800

    Write docstring

--- packages/node/src/events/protocol-message/controller.ts
+++ packages/node/src/events/protocol-message/controller.ts
@@ -3,6 +3,11 @@ import { StateChannel } from "@counterfactual/machine";
 import { RequestHandler } from "../../request-handler";
 import { NodeMessageWrappedProtocolMessage } from "../../types";
 
+/**
+ * Forwards all received NodeMessages that are for the machine's internal
+ * protocol execution directly to the instructionExecutor's message handler:
+ * `dispatchReceivedMessage`
+ */
 export default async function protocolMessageEventController(
   requestHandler: RequestHandler,
   nodeMsg: NodeMessageWrappedProtocolMessage
