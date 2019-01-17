23a015828385e9419aac94023f5633687e4599b0
Liam Horne <liam@lihorne.com>
2019-01-17 11:46:14 -0800

    Implement protocolMessageEventController with dispatchReceivedMessage

--- packages/node/src/events/protocol-message/controller.ts
+++ packages/node/src/events/protocol-message/controller.ts
@@ -1,3 +1,5 @@
+import { StateChannel } from "@counterfactual/machine";
+
 import { RequestHandler } from "../../request-handler";
 import { NodeMessageWrappedProtocolMessage } from "../../types";
 
@@ -8,4 +10,11 @@ import { NodeMessageWrappedProtocolMessage } from "../../types";
 export default async function protocolMessageEventController(
   requestHandler: RequestHandler,
   nodeMsg: NodeMessageWrappedProtocolMessage
-) {}
+) {
+  await requestHandler.instructionExecutor.dispatchReceivedMessage(
+    nodeMsg.data,
+    new Map<string, StateChannel>(
+      Object.entries(await requestHandler.store.getAllChannels())
+    )
+  );
+}
