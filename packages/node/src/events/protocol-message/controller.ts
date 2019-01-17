2c5647374c25241a10c5ad7f7a3cafb26cda55bc
Liam Horne <liam@lihorne.com>
2019-01-17 12:18:16 -0800

    Remove docstring

--- packages/node/src/events/protocol-message/controller.ts
+++ packages/node/src/events/protocol-message/controller.ts
@@ -3,10 +3,6 @@ import { StateChannel } from "@counterfactual/machine";
 import { RequestHandler } from "../../request-handler";
 import { NodeMessageWrappedProtocolMessage } from "../../types";
 
-/**
- * This function responds to a installation proposal approval from a peer Node
- * by counter installing the AppInstance this Node proposed earlier.
- */
 export default async function protocolMessageEventController(
   requestHandler: RequestHandler,
   nodeMsg: NodeMessageWrappedProtocolMessage
