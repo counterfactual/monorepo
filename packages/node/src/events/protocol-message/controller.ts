588407a6a16b4dc736969ce3686fe51eebf7386e
Liam Horne <snario@users.noreply.github.com>
2019-01-18 12:38:49 -0800

    Initial implementation of IO_SEND (#552)

--- packages/node/src/events/protocol-message/controller.ts
+++ packages/node/src/events/protocol-message/controller.ts
@@ -12,6 +12,17 @@ export default async function protocolMessageEventController(
   requestHandler: RequestHandler,
   nodeMsg: NodeMessageWrappedProtocolMessage
 ) {
+  // FIXME: Take into account the protocols sequence length. More
+  //        generally this if statement is trying to capture the notion:
+  //        "do not restart a protocol if a message is received from another
+  //         client mid-protocol execution (assume IO_WAIT is listening)"
+  //
+  //        An alternative might be that IO_WAIT is hooked onto toms event
+  //        listeniner inside the instructionExecutor that emits noise
+  //        when dispatchReceivedMessage receives a message for an in-progress
+  //        protocol execution.
+  if (nodeMsg.data.seq === 2) return;
+
   await requestHandler.instructionExecutor.dispatchReceivedMessage(
     nodeMsg.data,
     new Map<string, StateChannel>(
