e98e3df589009b73232ca49d86f15917e007adc3
Liam Horne <liam@lihorne.com>
2019-01-23 09:17:22 -0800

    Adds eventemitter3 to cf.js dist

--- packages/cf.js/rollup.config.js
+++ packages/cf.js/rollup.config.js
@@ -1,10 +1,12 @@
 import typescript from "rollup-plugin-typescript2";
 import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 import pkg from "./package.json";
 
 const bundledDependencies = new Set([
   "@counterfactual/types",
+  "eventemitter3",
 ]);
 
 export default {
@@ -29,6 +31,9 @@ export default {
   plugins: [
     nodeResolve({
       only: [...bundledDependencies]
+    }),
+    commonjs({
+      include: 'node_modules/eventemitter3/index.js',
     }),
     typescript(),
   ]
