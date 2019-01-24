15979f1b5f635795ff81ef3eb47010b8c5fdfc35
cf19drofxots <nima@l4v.io>
2019-01-24 15:33:07 -0500

    Implement getAppInstanceDetails

---
+++ packages/node/src/methods/app-instance/get-app-instance/controller.ts
@@ -0,0 +1,23 @@
+import { Node } from "@counterfactual/types";
+
+import { RequestHandler } from "../../../request-handler";
+import { ERRORS } from "../../errors";
+
+/**
+ * Handles the retrieval of an AppInstance.
+ * @param this
+ * @param params
+ */
+export default async function getAppInstanceController(
+  requestHandler: RequestHandler,
+  params: Node.GetAppInstanceDetailsParams
+): Promise<Node.GetAppInstanceDetailsResult> {
+  const { appInstanceId } = params;
+  if (!appInstanceId) {
+    Promise.reject(ERRORS.NO_APP_INSTANCE_ID_FOR_GET_STATE);
+  }
+
+  return {
+    appInstance: await requestHandler.store.getAppInstanceInfo(appInstanceId)
+  };
+}
