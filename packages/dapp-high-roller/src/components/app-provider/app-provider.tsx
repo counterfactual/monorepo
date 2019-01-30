a985f97368f52fc751cb77ff7cad6d563f2e4f86
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-01-30 17:20:43 -0300

    dapp-high-roller: added appInstance wait + contract address

--- packages/dapp-high-roller/src/components/app-provider/app-provider.tsx
+++ packages/dapp-high-roller/src/components/app-provider/app-provider.tsx
@@ -25,14 +25,38 @@ export class AppProvider {
 
   @State() appInstance: AppInstance = {} as AppInstance;
 
-  componentWillLoad() {
+  async componentWillLoad() {
     const params = new URLSearchParams(window.location.search);
 
     // TODO use async/await
-    this.nodeProvider = params.get("standalone")
-      ? new MockNodeProvider()
-      : new NodeProvider();
-    return this.nodeProvider.connect().then(this.setupCfProvider.bind(this));
+    this.nodeProvider = !params.get("standalone")
+      ? new NodeProvider()
+      : new MockNodeProvider();
+
+    await this.nodeProvider.connect();
+
+    this.setupCfProvider();
+    this.waitForCounterpartyAppInstance();
+  }
+
+  waitForCounterpartyAppInstance() {
+    window.addEventListener("message", event => {
+      if (
+        typeof event.data === "string" &&
+        event.data.startsWith("playground:appInstance")
+      ) {
+        const [, data] = event.data.split("|");
+        console.log("Received counterparty app instance", event.data);
+
+        if (data) {
+          const { appInstance } = JSON.parse(data);
+          this.updateAppInstance(appInstance);
+          this.history.push({
+            pathname: "/game"
+          });
+        }
+      }
+    });
   }
 
   setupCfProvider() {
@@ -43,7 +67,7 @@ export class AppProvider {
 
     this.appFactory = new cf.AppFactory(
       // TODO: This probably should be in a configuration, somewhere.
-      "0x1515151515151515151515151515151515151515",
+      "0x6296F3ACf03b6D787BD1068B4DB8093c54d5d915",
       { actionEncoding: "uint256", stateEncoding: "uint256" },
       this.cfProvider
     );
