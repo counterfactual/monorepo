21a1d2cad7315a8401bfd022050a02ac9a0a7e26
cf19drofxots <38441757+cf19drofxots@users.noreply.github.com>
2019-01-31 14:32:08 -0500

    [node] Implements uninstalling a Virtual AppInstance (#638)
    * Implement uninstalling an AppInstance
    
    * Implement uninstall event handler
    
    * Fix event handling & add integration test
    
    * lint
    
    * Fix naming

---
+++ packages/node/src/methods/app-instance/uninstall/controller.ts
@@ -0,0 +1,47 @@
+import { Node } from "@counterfactual/types";
+
+import { RequestHandler } from "../../../request-handler";
+import { NODE_EVENTS, UninstallMessage } from "../../../types";
+import { getCounterpartyAddress } from "../../../utils";
+import { ERRORS } from "../../errors";
+
+import { uninstallAppInstanceFromChannel } from "./operation";
+
+export default async function uninstallController(
+  requestHandler: RequestHandler,
+  params: Node.UninstallParams
+): Promise<Node.UninstallResult> {
+  const { appInstanceId } = params;
+  if (!appInstanceId) {
+    return Promise.reject(ERRORS.NO_APP_INSTANCE_ID_TO_UNINSTALL);
+  }
+
+  const appInstanceInfo = await requestHandler.store.getAppInstanceInfo(
+    appInstanceId
+  );
+  const updatedChannel = await uninstallAppInstanceFromChannel(
+    requestHandler.store,
+    appInstanceId
+  );
+
+  const uninstallMsg: UninstallMessage = {
+    from: requestHandler.address,
+    type: NODE_EVENTS.UNINSTALL,
+    data: {
+      appInstance: appInstanceInfo
+    }
+  };
+
+  // TODO: the next two statements should be synchronized so that the other party
+  // receives an uninstall message iff the uninstall initiator can successfully
+  // uninstall the AppInstance
+  await requestHandler.store.saveStateChannel(updatedChannel);
+
+  const to = getCounterpartyAddress(requestHandler.address, [
+    appInstanceInfo.initiatingAddress,
+    appInstanceInfo.respondingAddress
+  ]);
+
+  await requestHandler.messagingService.send(to, uninstallMsg);
+  return {};
+}
