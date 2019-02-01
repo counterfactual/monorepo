41e8aaa2d348d12354b9d96c582a68f06caedf84
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-02-01 13:03:21 -0300

    typescript-typings: added Ethers typings

--- packages/typescript-typings/types/ethers/index.d.ts
+++ packages/typescript-typings/types/ethers/index.d.ts
@@ -1,8 +1,24 @@
 // This file should be removed in favour of the real "ethers" package typings.
 
+type Signer = {
+  getAddress(): Promise<string>;
+  signMessage(message): Promise<string>;
+  sendTransaction(tx): Promise<any>;
+  getBalance(): Promise<any>;
+};
+
+declare class Web3Provider {
+  constructor(provider);
+  getSigner(): Signer;
+}
+
 declare var ethers = {
   utils: {
     formatEther: (value: string) => string,
-    parseEther: (value: string) => BigNumber
+    parseEther: (value: string) => BigNumber,
+    bigNumberify: (value: any) => BigNumber
+  },
+  providers: {
+    Web3Provider: Web3Provider
   }
 };
