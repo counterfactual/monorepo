215cb100ebadd2cbde9e56ccbd8c68c163d39508
Liam Horne <liam@lihorne.com>
2019-02-07 12:05:39 -0800

    Fix rebase mistakes

--- packages/node/src/methods/app-instance/install-virtual/operation.ts
+++ packages/node/src/methods/app-instance/install-virtual/operation.ts
@@ -1,18 +1,6 @@
-import {
-  AppInstance,
-  InstructionExecutor,
-  StateChannel,
-  xkeysToSortedKthAddresses
-} from "@counterfactual/machine";
-import {
-  AppInstanceInfo,
-  AppInterface,
-  Node,
-  Terms
-} from "@counterfactual/types";
-import { AddressZero } from "ethers/constants";
+import { InstructionExecutor } from "@counterfactual/machine";
+import { AppInstanceInfo, Node } from "@counterfactual/types";
 
-import { ProposedAppInstanceInfo } from "../../../models";
 import { Store } from "../../../store";
 import { ERRORS } from "../../errors";
 
@@ -38,18 +26,10 @@ export async function installVirtual(
   const updatedStateChannelsMap = await instructionExecutor.runInstallVirtualAppProtocol(
     new Map(Object.entries(await store.getAllChannels())),
     {
-      // TODO: Explain why appInstanceInfo.{initiating...,respomding...} are
-      //       incorrect to use at this point since it is non obvious.
-      initiatingAddress,
-      respondingAddress,
-      // TODO: Perhaps remove the below fields
-      multisig1Address: stateChannel.multisigAddress,
-      multisig2Address: "",
+      initiatingAddress: proposedToIdentifier,
+      respondingAddress: proposedByIdentifier,
       intermediaryAddress: appInstanceInfo.intermediaries![0],
-      signingKeys: [
-        appInstanceInfo.initiatingAddress,
-        appInstanceInfo.respondingAddress
-      ],
+      signingKeys: stateChannel.getSigningKeysFor(1337),
       defaultTimeout: appInstanceInfo.timeout.toNumber(),
       appInterface: {
         addr: appInstanceInfo.appId,
@@ -61,7 +41,6 @@ export async function installVirtual(
     }
   );
 
-  // TODO: Replace with `runInstallVirtualAppProtocol`
   await store.saveStateChannel(
     updatedStateChannelsMap.get(stateChannel.multisigAddress)!
   );
@@ -69,44 +48,4 @@ export async function installVirtual(
   await store.saveRealizedProposedAppInstance(appInstanceInfo);
 
   return appInstanceInfo;
-}
-
-function createAppInstanceFromAppInstanceInfo(
-  proposedAppInstanceInfo: ProposedAppInstanceInfo,
-  channel: StateChannel
-): AppInstance {
-  const appInterface: AppInterface = {
-    ...proposedAppInstanceInfo.abiEncodings,
-    addr: proposedAppInstanceInfo.appId
-  };
-
-  // TODO: throw if asset type is ETH and token is also set
-  const terms: Terms = {
-    assetType: proposedAppInstanceInfo.asset.assetType,
-    limit: proposedAppInstanceInfo.myDeposit.add(
-      proposedAppInstanceInfo.peerDeposit
-    ),
-    token: proposedAppInstanceInfo.asset.token || AddressZero
-  };
-
-  return new AppInstance(
-    AddressZero,
-    // FIXME: Incorrect for virtual app atm
-    xkeysToSortedKthAddresses(
-      [
-        proposedAppInstanceInfo.proposedByIdentifier,
-        proposedAppInstanceInfo.proposedToIdentifier
-      ],
-      0
-    ),
-    proposedAppInstanceInfo.timeout.toNumber(),
-    appInterface,
-    terms,
-    true,
-    channel.numInstalledApps, // FIXME: Incorrect for virtual app atm
-    channel.rootNonceValue,
-    proposedAppInstanceInfo.initialState,
-    0,
-    proposedAppInstanceInfo.timeout.toNumber()
-  );
 }
