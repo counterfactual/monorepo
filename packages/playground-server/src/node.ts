3740c0498b62249cbe3e30aa33fa0278b3dc6b37
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-02-08 11:49:20 -0300

    playground-server: refactored Node singleton with a NodeWrapper class

--- packages/playground-server/src/node.ts
+++ packages/playground-server/src/node.ts
@@ -1,14 +1,8 @@
-import {
-  FirebaseServiceFactory,
-  Node,
-  NodeMessage
-} from "@counterfactual/node";
+import { FirebaseServiceFactory, Node } from "@counterfactual/node";
 import { NetworkContext, Node as NodeTypes } from "@counterfactual/types";
 import { ethers } from "ethers";
 import { BaseProvider } from "ethers/providers";
 import { v4 as generateUUID } from "uuid";
-
-const { INSTALL, REJECT_INSTALL } = NodeTypes.EventName;
 
 const serviceFactory = new FirebaseServiceFactory({
   apiKey: "AIzaSyA5fy_WIAw9mqm59mdN61CiaCSKg8yd4uw",
@@ -19,74 +13,97 @@ const serviceFactory = new FirebaseServiceFactory({
   messagingSenderId: "432199632441"
 });
 
-let node: Node;
-export async function createNodeSingleton(
-  network: string,
-  networkContext?: NetworkContext,
-  provider?: BaseProvider,
-  mnemonic?: string
-): Promise<Node> {
-  return (
-    node || (await createNode(network, networkContext, provider, mnemonic))
-  );
-}
+const store = serviceFactory.createStoreService("pg-server-store");
 
-export async function createNode(
-  network: string,
-  networkContext?: NetworkContext,
-  provider?: BaseProvider,
-  mnemonic?: string
-): Promise<Node> {
-  const store = serviceFactory.createStoreService("pg-server-store");
+export default class NodeWrapper {
+  private static node: Node;
 
-  if (mnemonic) {
-    await store.set([{ key: "MNEMONIC", value: mnemonic }]);
-  }
-
-  const node = await Node.create(
-    serviceFactory.createMessagingService("messaging"),
-    store,
-    {
-      STORE_KEY_PREFIX: "store"
-    },
-    provider || ethers.getDefaultProvider(network),
-    network,
-    networkContext
-  );
-
-  node.on(INSTALL, async (msg: NodeMessage) => {
-    console.log("INSTALL event:", msg);
-  });
-
-  node.on(REJECT_INSTALL, async (msg: NodeMessage) => {
-    console.log("REJECT_INSTALL event:", msg);
-  });
-
-  return node;
-}
-
-export function getNodeAddress(): string {
-  return node.publicIdentifier;
-}
-
-export async function createStateChannelFor(
-  userAddress: string
-): Promise<NodeTypes.CreateChannelResult> {
-  if (!node) {
-    console.log("creating node...");
-    node = await createNodeSingleton(process.env.ETHEREUM_NETWORK || "ropsten");
-  }
-
-  const multisigResponse = await node.call(
-    NodeTypes.MethodName.CREATE_CHANNEL,
-    {
-      params: {
-        owners: [node.publicIdentifier, userAddress]
-      } as NodeTypes.CreateChannelParams,
-      type: NodeTypes.MethodName.CREATE_CHANNEL,
-      requestId: generateUUID()
+  public static getInstance() {
+    if (!NodeWrapper.node) {
+      throw new Error(
+        "Node hasn't been instantiated yet. Call NodeWrapper.createNode() first."
+      );
     }
-  );
 
-  return multisigResponse.result as NodeTypes.CreateChannelResult;
+    return NodeWrapper.node;
+  }
+
+  public static getNodeAddress() {
+    if (!NodeWrapper.node) {
+      throw new Error(
+        "Node hasn't been instantiated yet. Call NodeWrapper.createNode() first."
+      );
+    }
+
+    return NodeWrapper.node.publicIdentifier;
+  }
+
+  public static async createNodeSingleton(
+    network: string,
+    networkContext?: NetworkContext,
+    provider?: BaseProvider,
+    mnemonic?: string
+  ): Promise<Node> {
+    if (NodeWrapper.node) {
+      return NodeWrapper.node;
+    }
+
+    NodeWrapper.node = await NodeWrapper.createNode(
+      network,
+      networkContext,
+      provider,
+      mnemonic
+    );
+
+    return NodeWrapper.node;
+  }
+
+  public static async createNode(
+    network: string,
+    networkContext?: NetworkContext,
+    provider?: BaseProvider,
+    mnemonic?: string
+  ): Promise<Node> {
+    if (mnemonic) {
+      await store.set([{ key: "MNEMONIC", value: mnemonic }]);
+    }
+
+    const node = await Node.create(
+      serviceFactory.createMessagingService("messaging"),
+      store,
+      {
+        STORE_KEY_PREFIX: "store"
+      },
+      provider || ethers.getDefaultProvider(network),
+      network,
+      networkContext
+    );
+
+    return node;
+  }
+
+  public static async createStateChannelFor(
+    userAddress: string
+  ): Promise<NodeTypes.CreateChannelResult> {
+    if (!NodeWrapper.node) {
+      throw new Error(
+        "Node hasn't been instantiated yet. Call NodeWrapper.createNode() first."
+      );
+    }
+
+    const { node } = NodeWrapper;
+
+    const multisigResponse = await node.call(
+      NodeTypes.MethodName.CREATE_CHANNEL,
+      {
+        params: {
+          owners: [node.publicIdentifier, userAddress]
+        } as NodeTypes.CreateChannelParams,
+        type: NodeTypes.MethodName.CREATE_CHANNEL,
+        requestId: generateUUID()
+      }
+    );
+
+    return multisigResponse.result as NodeTypes.CreateChannelResult;
+  }
 }
