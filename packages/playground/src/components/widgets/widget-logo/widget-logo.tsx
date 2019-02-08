21368afd1067793ffb5f069994257b9489da52f3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-02-08 18:34:00 -0300

    playground: finishing CSS touches for dialog logo

--- packages/playground/src/components/widgets/widget-logo/widget-logo.tsx
+++ packages/playground/src/components/widgets/widget-logo/widget-logo.tsx
@@ -10,7 +10,7 @@ export class WidgetLogo {
 
   render() {
     return (
-      <h1 class="logo">
+      <h1 class={`logo ${this.caption ? "logo--icon-only" : ""}`}>
         <stencil-route-link url="/">
           <img src="/assets/icon/logo.svg" alt="Counterfactual" />
           {!this.caption ? <span>Playground</span> : this.caption}
