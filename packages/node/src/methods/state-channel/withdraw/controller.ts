c3d67f9f88f3a5fc19f50c27fe4b18732eba7b41
Liam Horne <liam@lihorne.com>
2019-02-17 15:27:23 -0700

    Clean code

--- packages/node/src/methods/state-channel/withdraw/controller.ts
+++ packages/node/src/methods/state-channel/withdraw/controller.ts
@@ -27,7 +27,7 @@ export default async function withdrawController(
   const commitment = await store.getWithdrawalCommitment(multisigAddress);
 
   const tx = {
-    ...(await store.getWithdrawalCommitment(multisigAddress)),
+    ...commitment,
     gasPrice: await provider.getGasPrice(),
     gasLimit: await provider.estimateGas(commitment)
   };
