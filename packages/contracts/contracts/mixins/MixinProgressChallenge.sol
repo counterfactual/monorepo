6bab5d4ddfcc5578cf541894f6ff480ce48a2cd9
Li Xuanji <xuanji@gmail.com>
2019-02-21 13:48:29 +0800

    uncomment

--- packages/contracts/contracts/mixins/MixinProgressChallenge.sol
+++ packages/contracts/contracts/mixins/MixinProgressChallenge.sol
@@ -25,7 +25,7 @@ contract MixinProgressChallenge is
   /// @param claimFinal If set, the caller claims that the action progresses the state
   /// to a terminal / finalized state
   /// @dev This function is only callable when the state channel is in a DISPUTE state
-  function progressDispute(
+  function progressChallenge(
     AppIdentity memory appIdentity,
     bytes memory appState,
     bytes memory action,
@@ -65,76 +65,26 @@ contract MixinProgressChallenge is
       "Action must have been signed by correct turn taker"
     );
 
-    // bytes memory newAppState = executeAppApplyAction(app, appState, action);
-
-    // state.appStateHash = keccak256(newAppState);
-    // state.disputeNonce += 1;
-    // state.latestSubmitter = msg.sender;
-
-    // if (claimFinal) {
-    //   require(
-    //     isAppStateTerminal(app, newAppState),
-    //     "Attempted to claimFinal on a non-terminal state"
-    //   );
-    //   state.finalizesAt = block.number;
-    //   state.status = Status.OFF;
-
-    //   emit DisputeFinalized(msg.sender, newAppState);
-    // } else {
-    //   state.status = Status.DISPUTE;
-    //   state.finalizesAt = block.number + defaultTimeout;
-
-    //   emit DisputeProgressed(
-    //     msg.sender,
-    //     appState,
-    //     action,
-    //     newAppState,
-    //     state.disputeNonce,
-    //     block.number + defaultTimeout
-    //   );
-    // }
-  }
-
-  /// @notice Unanimously agree to cancel a dispute
-  /// @param appIdentity an AppIdentity object pointing to the app being cancelled
-  /// @param signatures Signatures by all signing keys of the currently latest disputed
-  /// state; an indication of agreement of this state and valid to cancel a dispute
-  /// @dev Note this function is only callable when the state channel is in a DISPUTE state
-  function cancelChallenge(
-    AppIdentity memory appIdentity,
-    bytes memory signatures
-  )
-    // TODO: Uncomment when ABIEncoderV2 supports `external`
-    //       ref: https://github.com/ethereum/solidity/issues/3199
-    // external
-    public
-  {
-    bytes32 identityHash = appIdentityToHash(appIdentity);
-
-    AppChallenge storage challenge = appStates[identityHash];
-
-    require(
-      challenge.status == AppStatus.DISPUTE && challenge.finalizesAt >= block.number,
-      "cancelChallenge called on app not in DISPUTE state"
+    bytes memory newAppState = applyAction(
+      appIdentity.appDefinitionAddress,
+      appState,
+      action
     );
 
-    bytes32 stateHash = computeStateHash(
-      identityHash,
-      challenge.appStateHash,
-      challenge.nonce,
-      appIdentity.defaultTimeout
-    );
-
-    if (msg.sender != appIdentity.owner) {
-      require(
-        verifySignatures(signatures, stateHash, appIdentity.signingKeys),
-        "Invalid signatures"
-      );
-    }
-
-    challenge.disputeNonce = 0;
-    challenge.finalizesAt = 0;
-    challenge.status = AppStatus.ON;
+    challenge.appStateHash = keccak256(newAppState);
+    challenge.disputeNonce += 1;
     challenge.latestSubmitter = msg.sender;
+
+    if (claimFinal) {
+      require(
+        isStateTerminal(appIdentity.appDefinitionAddress, newAppState),
+        "Attempted to claimFinal on a non-terminal state"
+      );
+      challenge.finalizesAt = block.number;
+      challenge.status = AppStatus.OFF;
+    } else {
+      challenge.status = AppStatus.DISPUTE;
+      challenge.finalizesAt = block.number + appIdentity.defaultTimeout;
+    }
   }
 }
