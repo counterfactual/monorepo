67632179a763fa83be765abf5871b3ab0e89e365
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-02-26 17:05:45 -0300

    dapp-high-roller: replaced type

--- packages/dapp-high-roller/src/components/app-wager/app-wager.tsx
+++ packages/dapp-high-roller/src/components/app-wager/app-wager.tsx
@@ -5,7 +5,7 @@ import { RouterHistory } from "@stencil/router";
 
 import CounterfactualTunnel from "../../data/counterfactual";
 import { HighRollerAppState, HighRollerStage } from "../../data/game-types";
-import { Address, AppInstanceID, cf } from "../../data/types";
+import { Address, AppInstanceInfo, cf } from "../../data/types";
 
 const { HashZero } = ethers.constants;
 
@@ -44,9 +44,7 @@ export class AppWager {
   @Prop() account: any;
   @Prop() standalone: boolean = false;
 
-  @Prop() updateAppInstance: (
-    appInstance: { id: AppInstanceID }
-  ) => void = () => {};
+  @Prop() updateAppInstance: (appInstance: AppInstanceInfo) => void = () => {};
   @Prop() updateOpponent: (opponent: any) => void = () => {};
 
   async componentWillLoad() {
