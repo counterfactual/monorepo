c8e15b1e740554a93e9a2e86bd298e207408c9ad
Liam Horne <liam@lihorne.com>
2019-02-28 14:24:43 -0500

    Attempt to fetch remote node values

--- packages/playground-server/scripts/postinstall.sh
+++ packages/playground-server/scripts/postinstall.sh
@@ -4,5 +4,7 @@ set -e
 
 if [ ! -z "${IS_HEROKU_ENV}" ]; then
     echo "📟  Detected Heroku. Transpiling Typescript into JS for Node.JS to run on server."
+    yarn workspace @counterfactual/playground-server add @counterfactual/node@latest
+    yarn workspace @counterfactual/playground-server add @counterfactual/types@latest
     tsc -p tsconfig.heroku.json
 fi
