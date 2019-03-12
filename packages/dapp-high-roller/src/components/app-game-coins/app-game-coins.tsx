4ad0780cd080859a70084b6b29986623ae1c1750
Patience Tema Baron <patience.tema.baron@gmail.com>
2019-03-12 15:14:52 -0700

    [dapp-high-roller] preload high roller coin images (#1012)
    * preload high roller coin images
    
    * check if user data is received again
    
    * ping

--- packages/dapp-high-roller/src/components/app-game-coins/app-game-coins.tsx
+++ packages/dapp-high-roller/src/components/app-game-coins/app-game-coins.tsx
@@ -1,4 +1,4 @@
-import { Component } from "@stencil/core";
+import { Component, State } from "@stencil/core";
 
 // coin audio attribution:
 // https://freesound.org/people/Robinhood76/sounds/51671/
@@ -9,17 +9,53 @@ import { Component } from "@stencil/core";
   shadow: true
 })
 export class AppGameCoins {
+  @State() coins: any;
+
+  constructor() {
+    this.preloadCoins();
+  }
+
+  async preloadCoins() {
+    this.coins = await Promise.all(
+      [
+        ["02", "11", "12", "06"],
+        ["10", "07", "04", "07"],
+        ["03", "09", "08", "02"]
+      ].map(set => {
+        return Promise.all(
+          set.map(number => {
+            return new Promise(resolve => {
+              const img = new Image();
+              const c = document.createElement("canvas");
+              const ctx = c.getContext("2d") || new CanvasRenderingContext2D();
+
+              img.onload = function() {
+                c.width = img.naturalWidth;
+                c.height = img.naturalHeight;
+                ctx.drawImage(img, 0, 0);
+
+                resolve(c.toDataURL("image/png"));
+              };
+              img.src = `./assets/images/coins/${number}.png`;
+            });
+          })
+        );
+      })
+    );
+  }
+
   render() {
     const length = Math.min(
       50,
       Math.max(20, (screen.width * screen.height) / 10000)
     );
 
-    return (
+    return this.coins ? (
       <div class="coins">
         {Array.from({ length }).map(() => (
           <div class="coins__coin">
             <app-game-coin
+              coins={this.coins}
               delay={Math.random() * 3}
               speed={1.5 + Math.random() * 1}
               x={-20 + Math.random() * 120}
@@ -27,6 +63,8 @@ export class AppGameCoins {
           </div>
         ))}
       </div>
+    ) : (
+      undefined
     );
   }
 }
