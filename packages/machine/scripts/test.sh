98b39e53e44673fe3ebeca1497cec21dca52734b
Liam Horne <liam@lihorne.com>
2019-03-15 14:38:24 -0400

    Undo comment

--- packages/machine/scripts/test.sh
+++ packages/machine/scripts/test.sh
@@ -61,7 +61,7 @@ PID_FOR_GANACHE_CLI=$!
 echo "⚙️ Running migrations with build artifacts from @counterfactual/contracts"
 # TODO: For some reason this re-compiles all of the contracts unnecessarily
 #       and there isn't a --no-compile option on the command :(
-yarn run truffle migrate --network machine --reset #> /dev/null
+yarn run truffle migrate --network machine --reset > /dev/null
 
 echo "🎬 Starting jest test suites"
 jest \
