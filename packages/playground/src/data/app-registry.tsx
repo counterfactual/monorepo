2184fc7481bd95aa4869f06c60cb71c62a30c2e1
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-03-15 12:44:13 -0700

    playground: added schemaVersion/maintenanceMode states

--- packages/playground/src/data/app-registry.tsx
+++ packages/playground/src/data/app-registry.tsx
@@ -6,10 +6,12 @@ export interface AppRegistryState {
   apps: AppDefinition[];
   canUseApps: boolean;
   updateAppRegistry?(data: Partial<AppRegistryState>): Promise<void>;
+  schemaVersion: string;
+  maintenanceMode: boolean;
 }
 
 export default createProviderConsumer<AppRegistryState>(
-  { apps: [], canUseApps: false },
+  { apps: [], canUseApps: false, maintenanceMode: false, schemaVersion: "0" },
   (subscribe, child) => (
     <context-consumer subscribe={subscribe} renderer={child} />
   )
