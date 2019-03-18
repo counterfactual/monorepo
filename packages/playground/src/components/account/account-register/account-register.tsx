f1a1b52af1b065b76d8ba58c47f5263608ad95c9
Liam Horne <liam@lihorne.com>
2019-03-18 10:19:36 -0400

    Email optional

--- packages/playground/src/components/account/account-register/account-register.tsx
+++ packages/playground/src/components/account/account-register/account-register.tsx
@@ -250,7 +250,7 @@ export class AccountRegister {
           />
           <form-input
             disabled={inputIsDisabled}
-            label="Twitter handle (optional)"
+            label="Email (optional)"
             value={this.changeset.email}
             error={this.errors.email}
             onChange={e => this.change("email", e)}
