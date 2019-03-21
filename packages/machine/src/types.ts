fc2a22747a98f25946e7174c625500d66207eecb
Li Xuanji <xuanji@gmail.com>
2019-03-21 14:32:34 +0800

    [machine,node] remove duplicate fromXpub in machine message

--- packages/machine/src/types.ts
+++ packages/machine/src/types.ts
@@ -34,7 +34,6 @@ export interface Context {
 export type ProtocolMessage = {
   protocol: Protocol;
   params: ProtocolParameters;
-  fromXpub: string;
   toXpub: string;
   seq: number;
   signature?: Signature;
