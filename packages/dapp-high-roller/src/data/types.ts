e0b0b3bbf3ff47e83fba8f009baf1a136d36c49e
Alon Bukai <alonzorz@gmail.com>
2019-04-01 20:43:23 +0300

    fix: revert updated types

--- packages/dapp-high-roller/src/data/types.ts
+++ packages/dapp-high-roller/src/data/types.ts
@@ -367,8 +367,8 @@ export type HighRollerUIMutableState = {
 export type HighRollerUIState = HighRollerUIMutableState & {
   updateUIState: (state: HighRollerUIMutableState) => void;
   highRoller: (
-    num1: any,
-    num2: any
-  ) => Promise<{ playerFirstRoll: number[]; playerSecondRoll: number[] }>;
+    num1: number,
+    num2: number
+  ) => Promise<{ myRoll: number[]; opponentRoll: number[] }>;
   generateRandomRoll: () => number[];
 };
