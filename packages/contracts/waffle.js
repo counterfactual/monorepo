a75e7454b5874f9df1409381ec1a0e8ac3b3a474
Liam Horne <snario@users.noreply.github.com>
2019-04-16 08:29:14 +1000

    [apps, contracts] Clean up waffle js (#1305)
    * Clean up waffle js
    
    * Remove remote solcVersion since resolutions in package.json works

--- packages/contracts/waffle.js
+++ packages/contracts/waffle.js
@@ -14,12 +14,8 @@ var selectSolc = () => {
   if (process.env.CI || process.env.NATIVE_SOLC == "true") {
     // use native solc binary for fast compilation in CI
     waffleConfig.compiler = "native";
-
-  } else {
-    const solcjsVersion =
-    'v'+/([0-9a-z\.\+]+)\.Emscripten\.clang/g.exec(require("solc").version()).splice(1)[0];
-    waffleConfig.solcVersion = solcjsVersion;
   }
+
   return waffleConfig;
 }
 
