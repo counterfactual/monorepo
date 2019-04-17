712e142ed0e00871117385aa586992ec968ce985
Li Xuanji <xuanji@gmail.com>
2019-04-17 17:46:28 +0800

    [apps] delete CommitRevealApp

--- packages/apps/migrations/2_deploy_contracts.js
+++ packages/apps/migrations/2_deploy_contracts.js
@@ -1,12 +1,10 @@
 const tdr = require("truffle-deploy-registry");
 
-const CommitReveal = artifacts.require("CommitRevealApp");
 const HighRollerApp = artifacts.require("HighRollerApp");
 const NimApp = artifacts.require("NimApp");
 const TicTacToeApp = artifacts.require("TicTacToeApp");
 
 const ARTIFACTS = [
-  CommitReveal,
   HighRollerApp,
   NimApp,
   TicTacToeApp
