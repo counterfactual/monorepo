a874d543ef310b2cde6027c32e184cc17e0761a6
Liam Horne <snario@users.noreply.github.com>
2019-05-03 02:08:48 +0100

    [repo] Bump solidity code to 0.5.8 (#1389)
    * Bump solidity code to 0.5.8
    
    * Fix lock file
    
    * Fix yarn lock
    
    * Temporarily skip solc native binary usage

--- packages/contracts/contracts/MultiSend.sol
+++ packages/contracts/contracts/MultiSend.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.7;
+pragma solidity 0.5.8;
 
 
 /// @title Multi Send - Allows to batch multiple transactions into one.
--- packages/contracts/contracts/libs/LibSignature.sol
+++ packages/contracts/contracts/libs/LibSignature.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.7;
+pragma solidity 0.5.8;
 
 
 /// @title LibSignature - A library wrapper around signature verification
