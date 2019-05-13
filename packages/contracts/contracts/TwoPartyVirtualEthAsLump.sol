91b84c27d6cc0480570a5cbc8f74a60d1839842e
Li Xuanji <xuanji@gmail.com>
2019-05-13 13:29:27 -0400

    add comment

--- packages/contracts/contracts/TwoPartyVirtualEthAsLump.sol
+++ packages/contracts/contracts/TwoPartyVirtualEthAsLump.sol
@@ -11,6 +11,10 @@ import "./NonceRegistry.sol";
 /// committed in a ledger channel.
 /// The `target` contract must return via resolution the outcome of the app,
 /// and the resolve function must be TwoPartyOutcome type.
+/// We do not use the interpreter mechanism in StateChannelTransaction.sol
+/// because the delegateTarget function must be committed to in at least two
+/// places (in general, the same number of places as the channel length) with
+/// different parameters.
 contract TwoPartyVirtualEthAsLump {
 
   // todo(xuanji): is it possible to make address(registry) a constant specified
