164432f2b2a48b0b3c7360f64fee9f35c4a2699c
cf19drofxots <nima@l4v.io>
2019-05-17 14:14:15 -0400

    Refactor to DRY code

--- packages/contracts/test/check-deployed-contracts.spec.ts
+++ packages/contracts/test/check-deployed-contracts.spec.ts
@@ -11,33 +11,17 @@ const NETWORK_NAME_TO_ID = {
 };
 
 describe("Checks that all the needed contracts have been deployed on each test net", () => {
-  it("checks deployments on Kovan", () => {
-    const networkName = "kovan";
-    const kovanDeployments = extractContractNamesFromDeployments(networkName);
-    networkContextProps.forEach((contractName: string) => {
-      if (!kovanDeployments.has(contractName)) {
-        throw Error(contractNotDeployed(contractName, networkName));
-      }
-    });
-  });
-
-  it("checks deployments on Ropsten", () => {
-    const networkName = "ropsten";
-    const ropstenDeployments = extractContractNamesFromDeployments(networkName);
-    networkContextProps.forEach((contractName: string) => {
-      if (!ropstenDeployments.has(contractName)) {
-        throw Error(contractNotDeployed(contractName, networkName));
-      }
-    });
-  });
-
-  it("checks deployments on Rinkeby", () => {
-    const networkName = "rinkeby";
-    const rinkebyDeployments = extractContractNamesFromDeployments(networkName);
-    networkContextProps.forEach((contractName: string) => {
-      if (!rinkebyDeployments.has(contractName)) {
-        throw Error(contractNotDeployed(contractName, networkName));
-      }
+  const networks = ["kovan", "ropsten", "rinkeby"];
+  networks.forEach((networkName: string) => {
+    it(`checks deployments on ${networkName}`, () => {
+      const networkDeployments = extractContractNamesFromDeployments(
+        networkName
+      );
+      networkContextProps.forEach((contractName: string) => {
+        if (!networkDeployments.has(contractName)) {
+          throw Error(contractNotDeployed(contractName, networkName));
+        }
+      });
     });
   });
 });
@@ -51,7 +35,7 @@ function extractContractNamesFromDeployments(networkName: string): Set<string> {
     Array.from<ContractMigration>(
       JSON.parse(
         readFileSync(
-          path.join(NETWORKS_DIR, `${NETWORK_NAME_TO_ID["kovan"]}.json`),
+          path.join(NETWORKS_DIR, `${NETWORK_NAME_TO_ID[networkName]}.json`),
           "utf8"
         )
       )
