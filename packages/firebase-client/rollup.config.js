0be23fdec1546c04ca1b3514f991281c3e52e24e
cf19drofxots <nima@l4v.io>
2019-05-23 18:24:33 -0400

    More cleanup

--- packages/firebase-client/rollup.config.js
+++ packages/firebase-client/rollup.config.js
@@ -1,39 +1,7 @@
-import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
-import nodeResolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
-
-const globals = {
-  "ethers": "ethers",
-  "ethers/constants": "ethers.constants",
-  "ethers/errors": "ethers.errors",
-  "ethers/providers": "ethers.providers",
-  "ethers/utils": "ethers.utils",
-  "ethers/utils/hdnode": "ethers.utils.HDNode",
-  "ethers/wallet": "ethers.wallet",
-  firebase: "firebase",
-  "firebase/app": "firebase.app",
-  events: "EventEmitter",
-  loglevel: "log",
-  uuid: "uuid"
-};
-
-const bundledDependencies = new Set([
-  "@counterfactual/types",
-  "typescript-memoize",
-  "p-queue"
-]);
-
-const external = [
-  ...Object.keys(pkg.dependencies || {}).filter(dependency => {
-    return !bundledDependencies.has(dependency);
-  }),
-  ...Object.keys(pkg.peerDependencies || {}).filter(dependency => {
-    return !bundledDependencies.has(dependency);
-  })
-];
 
 export default [
   {
@@ -42,31 +10,11 @@ export default [
       {
         file: pkg.main,
         sourcemap: true,
-        format: "cjs",
-        globals: globals
+        format: "cjs"
       },
-      {
-        file: pkg.iife,
-        sourcemap: true,
-        name: "window",
-        format: "iife",
-        extend: true,
-        globals: globals
-      }
     ],
-    external: external,
     plugins: [
-      commonjs({
-        namedExports: {
-          "../../node_modules/typescript-memoize/dist/memoize-decorator.js": [
-            "Memoize"
-          ]
-        }
-      }),
       json({ compact: true }),
-      nodeResolve({
-        only: [...bundledDependencies]
-      }),
       typescript(),
     ]
   }
