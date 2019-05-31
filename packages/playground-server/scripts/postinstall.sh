cc4ed39a21cf3cb97a80ff7fe62ab3e25f0c2fe8
Liam Horne <liam@lihorne.com>
2019-05-31 12:30:38 -0400

    Attempt to delete a rambunctious file

--- packages/playground-server/scripts/postinstall.sh
+++ packages/playground-server/scripts/postinstall.sh
@@ -5,4 +5,5 @@ set -e
 if [ ! -z "${IS_HEROKU_ENV}" ]; then
     echo "📟  Detected Heroku. Transpiling Typescript into JS for Node.JS to run on server."
     tsc -p tsconfig.heroku.json &> /dev/null || :
+    rm -rf ./node_modules/websocket/.git
 fi
