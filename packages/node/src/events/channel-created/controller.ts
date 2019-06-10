b3caafb15d22276e5acb2f621bc19381e4d64e9f
Liam Horne <snario@users.noreply.github.com>
2019-06-10 22:14:06 +0200

    Remove deprecated event handler being misfired (#1667)
    * Remove unused event handler
    
    * Remove unused import

--- packages/node/src/events/channel-created/controller.ts
+++ packages/node/src/events/channel-created/controller.ts
@@ -1,4 +1,3 @@
-import { StateChannel } from "../../models";
 import { RequestHandler } from "../../request-handler";
 import { CreateChannelMessage } from "../../types";
 
@@ -9,14 +8,4 @@ import { CreateChannelMessage } from "../../types";
 export default async function addMultisigController(
   requestHandler: RequestHandler,
   nodeMsg: CreateChannelMessage
-) {
-  const multisigAddress = nodeMsg.data.multisigAddress;
-  const multisigOwners = nodeMsg.data.owners;
-  await requestHandler.store.saveStateChannel(
-    StateChannel.setupChannel(
-      requestHandler.networkContext.ETHBucket,
-      multisigAddress,
-      multisigOwners
-    )
-  );
-}
+) {}
