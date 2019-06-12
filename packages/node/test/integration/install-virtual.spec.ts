5bcaaf2d670910f74cd7287beb76892fb0ad97c0
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-06-12 12:34:15 -0300

    why do I need to repeat the getProposedAppInstances call? T_T

--- packages/node/test/integration/install-virtual.spec.ts
+++ packages/node/test/integration/install-virtual.spec.ts
@@ -43,7 +43,7 @@ describe("Node method follows spec - proposeInstallVirtual", () => {
   });
   describe(
     "Node A makes a proposal through an intermediary Node B to install a " +
-      "Virtual AppInstance with Node C. All Nodes confirm receipt of proposal",
+    "Virtual AppInstance with Node C. All Nodes confirm receipt of proposal",
     () => {
       it("sends proposal with non-null initial state", async done => {
         const multisigAddressAB = await createChannel(nodeA, nodeB);
@@ -91,6 +91,11 @@ describe("Node method follows spec - proposeInstallVirtual", () => {
             const { appInstanceId } = msg.data;
             const { intermediaries } = msg.data.params;
             const [proposedAppNodeA] = await getProposedAppInstances(nodeA);
+
+            // TODO: For some odd reason I need to run this twice.
+            // It shouldn't be that way. This feels like an async issue.
+            await getProposedAppInstances(nodeC);
+
             const [proposedAppNodeC] = await getProposedAppInstances(nodeC);
 
             confirmProposedVirtualAppInstance(proposalParams, proposedAppNodeA);
