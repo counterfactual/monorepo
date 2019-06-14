e1916d18041488f58187192a7542990bf93f4228
Liam Horne <liam@lihorne.com>
2019-06-14 15:32:29 +0200

    Remove comments

--- packages/node/src/models/app-instance.ts
+++ packages/node/src/models/app-instance.ts
@@ -327,10 +327,6 @@ export class AppInstance {
   ): Promise<SolidityABIEncoderV2Type> {
     const ret: SolidityABIEncoderV2Type = {};
 
-    console.log(this.toEthersContract(provider).address);
-    console.log(this.state);
-    console.log(action);
-
     const computedNextState = this.decodeAppState(
       await this.toEthersContract(provider).functions.applyAction(
         this.encodedLatestState,
