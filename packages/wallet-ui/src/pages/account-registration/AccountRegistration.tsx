3940cf0a9c9f81080bb65805f5f594e84ef6fc76
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-06-18 18:21:48 -0300

    added login link to Account Registration screen

--- packages/wallet-ui/src/pages/account-registration/AccountRegistration.tsx
+++ packages/wallet-ui/src/pages/account-registration/AccountRegistration.tsx
@@ -4,29 +4,38 @@ import { WidgetScreen } from "../../components/widget";
 import { FormButton, FormInput } from "../../components/form";
 
 import "./AccountRegistration.scss";
+import { Link } from "react-router-dom";
 
-export type AccountRegistrationProps = {
+type AccountRegistrationProps = {
   ethAddress: string;
 };
 
+const AlreadyHaveAnAccount: React.FC = () => (
+  <React.Fragment>
+    Already have an account? <Link to="/login">Login here</Link>
+  </React.Fragment>
+);
+
 const AccountRegistration: React.FC<AccountRegistrationProps> = ({
   ethAddress = "0xd30E537Bc4BDb191FF2450f5949c16CFc957abE8"
-}) => {
-  return (
-    <WidgetScreen header={"Create a Counterfactual Account"} exitable={false}>
-      <form onSubmit={async e => console.log(e)}>
-        <FormInput label="Username" type="text" />
-        <FormInput label="E-mail (optional)" type="email" />
-        <div className="smallprint">
-          <b>Account will be linked to your Ethereum address: </b>
-          {ethAddress}
-        </div>
-        <FormButton type="submit" className="button">
-          Create an account
-        </FormButton>
-      </form>
-    </WidgetScreen>
-  );
-};
+}) => (
+  <WidgetScreen
+    header={"Create a Counterfactual Account"}
+    post={<AlreadyHaveAnAccount />}
+    exitable={false}
+  >
+    <form onSubmit={async e => console.log(e)}>
+      <FormInput label="Username" type="text" />
+      <FormInput label="E-mail (optional)" type="email" />
+      <div className="smallprint">
+        <b>Account will be linked to your Ethereum address: </b>
+        {ethAddress}
+      </div>
+      <FormButton type="submit" className="button">
+        Create account
+      </FormButton>
+    </form>
+  </WidgetScreen>
+);
 
 export { AccountRegistration };
