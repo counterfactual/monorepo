070f7ddb26a3d4e9eb643de9a04a553708d53d7e
Liam Horne <snario@users.noreply.github.com>
2019-06-19 11:11:51 +0200

    Move DolphinCoin test to contracts (#1716)

--- packages/contracts/test/check-deployed-contracts.spec.ts
+++ packages/contracts/test/check-deployed-contracts.spec.ts
@@ -10,7 +10,7 @@ const NETWORK_NAME_TO_ID = {
   rinkeby: "4"
 };
 
-const CONTRACTS_TO_SKIP = new Set(["DolphinCoin"]);
+const CONTRACTS_TO_SKIP = new Set([""]);
 
 describe("Checks that all the needed contracts have been deployed on each test net", () => {
   const networks = ["kovan", "ropsten", "rinkeby"];
