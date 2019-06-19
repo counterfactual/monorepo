326d7a55b71a64bb63692c7a691efd5e7bc71b84
Liam Horne <snario@users.noreply.github.com>
2019-06-19 19:14:06 +0200

    Add GetStateDepositHolderAddress method on Node (#1719)

---
+++ packages/node/test/integration/get-state-deposit-holder-address.spec.ts
@@ -0,0 +1,32 @@
+import { Node as NodeTypes } from "@counterfactual/types";
+
+import { jsonRpcDeserialize, JsonRpcResponse, Node } from "../../src";
+
+import { setup, SetupContext } from "./setup";
+
+describe("Node method follows spec - getStateDepositHolderAddress", () => {
+  let nodeA: Node;
+  let nodeB: Node;
+
+  beforeAll(async () => {
+    const context: SetupContext = await setup(global);
+    nodeA = context["A"].node;
+    nodeB = context["B"].node;
+  });
+
+  it("can accept a valid call and return correctly formatted address", async () => {
+    const owners = [nodeA.publicIdentifier, nodeB.publicIdentifier];
+
+    const { address } = ((await nodeA.router.dispatch(
+      jsonRpcDeserialize({
+        id: Date.now(),
+        method: NodeTypes.RpcMethodName.GET_STATE_DEPOSIT_HOLDER_ADDRESS,
+        params: { owners },
+        jsonrpc: "2.0"
+      })
+    )) as JsonRpcResponse)
+      .result as NodeTypes.GetStateDepositHolderAddressResult;
+
+    expect(address.length).toBe(42);
+  });
+});
