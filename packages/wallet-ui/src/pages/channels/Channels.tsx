7537af08ab60baf60840d0f31a8d37a3c3e9a8f3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-06-19 17:13:26 -0300

    added "Discover State Channel apps" button

--- packages/wallet-ui/src/pages/channels/Channels.tsx
+++ packages/wallet-ui/src/pages/channels/Channels.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { ChannelTree, ChannelNode } from "../../components/channel";
 
+import "./Channels.scss";
+
 export class Channels extends React.Component {
   render() {
     return (
       <section className="section fill">
         <h2>State Channels</h2>
-        <ChannelTree>
-          <ChannelNode type="hub" ethAddress="0x0" name="Playground Server">
-            <ChannelNode type="user" ethAddress="0x1" name="High Roller Bot">
-              <ChannelNode type="app" ethAddress="0x2" name="High Roller" />
+        <div className="channel-trees">
+          <ChannelTree>
+            <ChannelNode type="hub" ethAddress="0x0" name="Playground Server">
+              <ChannelNode type="user" ethAddress="0x1" name="High Roller Bot">
+                <ChannelNode type="app" ethAddress="0x2" name="High Roller" />
+              </ChannelNode>
             </ChannelNode>
-          </ChannelNode>
-        </ChannelTree>
-        <ChannelTree>
-          <ChannelNode type="hub" ethAddress="0x0" name="Connext Server" />
-        </ChannelTree>
+          </ChannelTree>
+          <ChannelTree>
+            <ChannelNode type="hub" ethAddress="0x0" name="Connext Server" />
+          </ChannelTree>
+        </div>
+        <button className="btn btn-secondary button-discover-apps">
+          Discover State Channel apps
+        </button>
       </section>
     );
   }
