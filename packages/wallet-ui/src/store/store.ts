4de2a4ffc960ccdd0f27fcb1759e6a0e9e41d17a
Santiago Persico <santiagopersico@gmail.com>
2019-06-20 17:42:30 -0300

    add devtools integration

--- packages/wallet-ui/src/store/store.ts
+++ packages/wallet-ui/src/store/store.ts
@@ -1,14 +1,16 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 
 import { reducers as User } from "./user";
 import { reducers as Wallet } from "./wallet";
 import { ApplicationState, StoreAction } from "./types";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   combineReducers<ApplicationState, StoreAction<any>>({
     User,
     Wallet
   }),
-  applyMiddleware(ReduxThunk)
+  composeEnhancer(applyMiddleware(ReduxThunk))
 );
