e4878d72ddb10d465113c672e6d6d76246dc6793
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-06-24 15:00:53 -0300

    registration flow completed! :D

--- packages/wallet-ui/src/utils/counterfactual.ts
+++ packages/wallet-ui/src/utils/counterfactual.ts
@@ -3,15 +3,7 @@ import { CounterfactualMethod, CounterfactualEvent } from "../types";
 import log from "./log";
 
 export async function getNodeAddress(): Promise<string> {
-  const nodeAddress = localStorage.getItem("counterfactual:node:address");
-
-  if (nodeAddress) {
-    return nodeAddress;
-  }
-
   const data = await window.ethereum.send(CounterfactualMethod.GetNodeAddress);
-
-  localStorage.setItem("counterfactual:node:address", data.result);
 
   return data.result;
 }
