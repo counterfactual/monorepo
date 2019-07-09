53c946328ce22da56f02a5ab988fe89ea497c0ee
Liam Horne <snario@users.noreply.github.com>
2019-07-09 16:28:58 +0300

    Deploy new contrcts (#1831)

--- packages/contracts/migrations/2_deploy_contracts.js
+++ packages/contracts/migrations/2_deploy_contracts.js
@@ -1,16 +1,16 @@
 const tdr = require("truffle-deploy-registry");
 
 const ARTIFACTS = [
-  artifacts.require("ChallengeRegistry"),
+  // artifacts.require("ChallengeRegistry"),
   artifacts.require("ConditionalTransactionDelegateTarget"),
   artifacts.require("CoinBalanceRefundApp"),
   artifacts.require("CoinTransferETHInterpreter"),
-  artifacts.require("FreeBalanceApp"),
-  artifacts.require("IdentityApp"),
-  artifacts.require("MinimumViableMultisig"),
-  artifacts.require("ProxyFactory"),
-  artifacts.require("TwoPartyFixedOutcomeETHInterpreter"),
-  artifacts.require("TwoPartyFixedOutcomeFromVirtualAppETHInterpreter")
+  // artifacts.require("FreeBalanceApp"),
+  // artifacts.require("IdentityApp"),
+  // artifacts.require("MinimumViableMultisig"),
+  // artifacts.require("ProxyFactory"),
+  // artifacts.require("TwoPartyFixedOutcomeETHInterpreter"),
+  // artifacts.require("TwoPartyFixedOutcomeFromVirtualAppETHInterpreter")
 ];
 
 module.exports = (deployer, network) => {
