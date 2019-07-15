678c53205d1d7ed5f5e77ed174b1f041c8fc7aef
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-07-15 13:45:32 -0300

    added reducer tests

--- packages/wallet-ui/src/store/wallet.spec.ts
+++ packages/wallet-ui/src/store/wallet.spec.ts
@@ -2,15 +2,21 @@ import { One } from "ethers/constants";
 import { parseEther } from "ethers/utils";
 import { createMemoryHistory } from "history";
 import { RoutePath } from "../types";
-import callAction from "../utils/call-action";
+import callAction, { ActionResult } from "../utils/call-action";
 import EthereumMock, {
   enableEthereumMockBehavior,
   ETHEREUM_MOCK_ADDRESS,
   MULTISIG_MOCK_ADDRESS,
   NODE_MOCK_ADDRESS
 } from "./ethereum.mock";
-import { ActionType, StoreAction, WalletState } from "./types";
-import { connectToWallet, deposit, WalletDepositTransition } from "./wallet";
+import { ActionType, WalletState } from "./types";
+import {
+  connectToWallet,
+  deposit,
+  initialState,
+  reducers,
+  WalletDepositTransition
+} from "./wallet";
 import Web3ProviderMock from "./web3provider.mock";
 
 // TODO: Test store states after implementing reducers in CallAction.
@@ -25,21 +31,41 @@ describe("Store > Wallet", () => {
       enableEthereumMockBehavior("failOnEnable");
 
       try {
-        await callAction(connectToWallet);
+        await callAction(connectToWallet, { initialState, reducers });
         fail("Should have thrown error message");
       } catch (e) {
-        const [{ data }] = e as StoreAction<WalletState>[];
+        const {
+          dispatchedActions,
+          reducedStates
+        }: ActionResult<WalletState> = e;
+        const [{ data }] = dispatchedActions;
+        const [state] = reducedStates;
+
+        expect(dispatchedActions.length).toBe(1);
+        expect(reducedStates.length).toBe(1);
+
         expect(data.error.message).toBe(
           "You must allow Counterfactual to connect with Metamask in order to use it."
         );
+
+        expect(state.error).toEqual(data.error);
       }
     });
     it("should dispatch a WALLET_SET_ADDRESS action with the expected information when user allows access to the wallet", async () => {
-      const [{ data, type }] = await callAction(connectToWallet);
+      const { dispatchedActions, reducedStates } = await callAction<
+        WalletState
+      >(connectToWallet, { initialState, reducers });
+      const [{ data, type }] = dispatchedActions;
 
       expect(type).toBe(ActionType.WalletSetAddress);
       expect(data.error).toBe(undefined);
       expect(data.ethAddress).toBe(ETHEREUM_MOCK_ADDRESS);
+
+      const [state] = reducedStates;
+
+      expect(state.ethAddress).toBe(data.ethAddress);
+      expect(state.status).toBe(type);
+      expect(state.error).toEqual({});
     });
   });
   describe("deposit()", () => {
@@ -55,9 +81,9 @@ describe("Store > Wallet", () => {
       enableEthereumMockBehavior("rejectDeposit");
 
       try {
-        await callAction(
-          deposit,
-          [
+        await callAction<WalletState>(deposit, {
+          reducers,
+          actionParameters: [
             {
               amount: parseEther("1.0"),
               ethAddress: window.ethereum.selectedAddress,
@@ -66,18 +92,34 @@ describe("Store > Wallet", () => {
             },
             web3Provider
           ],
-          ActionType.WalletSetBalance
-        );
+          finalActionType: ActionType.WalletSetBalance
+        });
         fail("Deposit should have never proceeded.");
       } catch (e) {
-        const [{ data }] = e as StoreAction<WalletState>[];
+        const {
+          dispatchedActions,
+          reducedStates
+        }: ActionResult<WalletState> = e;
+        expect(dispatchedActions.length).toBe(1);
+        expect(reducedStates.length).toBe(2);
+
+        const [{ data }] = dispatchedActions;
+        const [transitionState, errorState] = reducedStates;
+
         expect(data.error.message).toContain("because of");
+        expect(transitionState.status).toEqual(
+          WalletDepositTransition.CheckWallet
+        );
+        expect(errorState.status).toEqual(ActionType.WalletError);
+        expect(errorState.error).toEqual(data.error);
       }
     });
     it("should dispatch WALLET_DEPOSIT_CHECK_WALLET, WALLET_DEPOSIT_WAIT_FOR_FUNDS, WALLET_SET_BALANCE", async () => {
-      const dispatchedActions = await callAction(
-        deposit,
-        [
+      const { dispatchedActions, reducedStates } = await callAction<
+        WalletState
+      >(deposit, {
+        reducers,
+        actionParameters: [
           {
             amount: parseEther("1.0"),
             ethAddress: window.ethereum.selectedAddress,
@@ -86,8 +128,8 @@ describe("Store > Wallet", () => {
           },
           web3Provider
         ],
-        ActionType.WalletSetBalance
-      );
+        finalActionType: ActionType.WalletSetBalance
+      });
 
       expect(dispatchedActions).toEqual([
         { type: WalletDepositTransition.CheckWallet },
@@ -100,6 +142,16 @@ describe("Store > Wallet", () => {
           type: ActionType.WalletSetBalance
         }
       ]);
+
+      expect(reducedStates).toEqual([
+        { status: WalletDepositTransition.CheckWallet },
+        { status: WalletDepositTransition.WaitForFunds },
+        {
+          status: ActionType.WalletSetBalance,
+          ethereumBalance: One,
+          counterfactualBalance: parseEther("1.0")
+        }
+      ]);
     });
     it("should should redirect to /channels if the History object is provided", async () => {
       const history = createMemoryHistory();
@@ -107,9 +159,9 @@ describe("Store > Wallet", () => {
       expect(history.length).toBe(1);
       expect(history.location.pathname).toBe(RoutePath.Root);
 
-      await callAction(
-        deposit,
-        [
+      await callAction<WalletState>(deposit, {
+        reducers,
+        actionParameters: [
           {
             amount: parseEther("1.0"),
             ethAddress: window.ethereum.selectedAddress,
@@ -119,8 +171,8 @@ describe("Store > Wallet", () => {
           web3Provider,
           history
         ],
-        ActionType.WalletSetBalance
-      );
+        finalActionType: ActionType.WalletSetBalance
+      });
 
       expect(history.length).toBe(2);
       expect(history.location.pathname).toBe(RoutePath.Channels);
--- packages/wallet-ui/src/utils/call-action.ts
+++ packages/wallet-ui/src/utils/call-action.ts
@@ -2,37 +2,67 @@ import { Action } from "redux";
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { ActionType, ApplicationState, StoreAction } from "../store/types";
 
-const callAction = async (
-  actionCreator: (
-    ...args: any[]
-  ) => ThunkAction<void, ApplicationState, null, Action<ActionType>>,
-  actionParameters: any[] = [],
-  finalActionType?: ActionType
-  // TODO: Inject reducers here!
-): Promise<StoreAction<any>[]> => {
-  return new Promise((resolve, reject) => {
-    const dispatchedActions: StoreAction<any>[] = [];
+export type ActionCreator = (
+  ...args: any[]
+) => ThunkAction<void, ApplicationState, null, Action<ActionType>>;
 
-    const dispatch = (action: StoreAction<any>) => {
+export type ActionSettings<T> = {
+  actionParameters?: any[];
+  reducers?: (state: T, action: StoreAction<T>) => T;
+  initialState?: any;
+  finalActionType?: ActionType;
+};
+
+export type ActionResult<T> = {
+  dispatchedActions: StoreAction<T>[];
+  reducedStates: T[];
+};
+
+const callAction = async <T>(
+  actionCreator: ActionCreator,
+  {
+    actionParameters = [],
+    reducers = () => ({} as T),
+    initialState = {},
+    finalActionType = undefined
+  }: ActionSettings<T> = {} as ActionSettings<T>
+): Promise<ActionResult<T>> => {
+  return new Promise((resolve, reject) => {
+    const dispatchedActions: StoreAction<T>[] = [];
+    const reducedStates: T[] = [];
+
+    const dispatch = (action: StoreAction<T>) => {
+      const previousAction = dispatchedActions[dispatchedActions.length - 1];
+      reducedStates.push(
+        reducers(
+          previousAction
+            ? { ...previousAction.data, status: previousAction.type }
+            : initialState,
+          action
+        )
+      );
+
+      dispatchedActions.push(action);
+
       if (action.data && action.data["error"]) {
-        reject([action]);
-      } else {
-        dispatchedActions.push(action);
-        // TODO: Inject reducers here!
-        if (
-          !finalActionType ||
-          (finalActionType && action.type === finalActionType)
-        ) {
-          resolve(dispatchedActions);
-        }
+        reject({ reducedStates, dispatchedActions: [action] });
+      } else if (
+        !finalActionType ||
+        (finalActionType && action.type === finalActionType)
+      ) {
+        resolve({ dispatchedActions, reducedStates });
       }
     };
 
-    actionCreator(...actionParameters)(
-      dispatch as ThunkDispatch<ApplicationState, null, Action<ActionType>>,
-      () => ({} as ApplicationState),
-      null
-    );
+    try {
+      actionCreator(...actionParameters)(
+        dispatch as ThunkDispatch<ApplicationState, null, Action<ActionType>>,
+        () => ({} as ApplicationState),
+        null
+      );
+    } catch {
+      reject({ dispatchedActions, reducedStates });
+    }
   });
 };
 
