6f70c4ad21b76d0edf58f3c322eaea4e400114b6
Liam Horne <snario@users.noreply.github.com>
2019-07-23 17:14:30 -0400

    Better testing of CoinTransferInterpreter (#1977)

--- packages/contracts/contracts/interpreters/CoinTransferInterpreter.sol
+++ packages/contracts/contracts/interpreters/CoinTransferInterpreter.sol
@@ -14,50 +14,52 @@ contract CoinTransferInterpreter is Interpreter {
 
   address constant CONVENTION_FOR_ETH_TOKEN_ADDRESS = address(0x0);
 
-  using LibOutcome for LibOutcome.CoinTransfer;
-
   struct Param {
     uint256[] limit;
     address[] tokenAddresses;
   }
 
+  function () external payable { }
+
   function interpretOutcomeAndExecuteEffect(
-    bytes calldata input,
+    bytes calldata encodedOutput,
     bytes calldata encodedParams
   )
     external
   {
 
     Param memory params = abi.decode(encodedParams, (Param));
-    uint256[] memory limitsRemaining = params.limit;
-    address[] memory tokenAddresses = params.tokenAddresses;
 
     LibOutcome.CoinTransfer[][] memory assetTransfers =
-      abi.decode(input, (LibOutcome.CoinTransfer[][]));
+      abi.decode(
+        encodedOutput,
+        (LibOutcome.CoinTransfer[][])
+      );
 
     for (uint256 i = 0; i < assetTransfers.length; i++) {
-      address tokenAddress = tokenAddresses[i];
-      uint256 limitRemaining = limitsRemaining[i];
+      address tokenAddress = params.tokenAddresses[i];
+      uint256 limitRemaining = params.limit[i];
       LibOutcome.CoinTransfer[] memory transfers = assetTransfers[i];
 
       for (uint256 transferIndex = 0; transferIndex < transfers.length; transferIndex++) {
         LibOutcome.CoinTransfer memory transfer = transfers[transferIndex];
-        address payable to = address(uint160(transfer.to));
-        uint256 amount = transfer.amount;
 
-        if (amount == 0) {
+        address payable to = address(uint160(transfer.to));
+
+        if (transfer.amount == 0) {
           continue;
         }
 
-        require(amount <= limitRemaining, "Hit the transfer limit.");
-        limitRemaining -= amount;
+        require(transfer.amount <= limitRemaining, "Hit the transfer limit.");
+
+        limitRemaining -= transfer.amount;
 
         if (tokenAddress == CONVENTION_FOR_ETH_TOKEN_ADDRESS) {
           // note: send() is deliberately used instead of transfer() here
           // so that a revert does not stop the rest of the sends
-          to.send(amount);
+          to.send(transfer.amount);
         } else {
-          ERC20(tokenAddress).transfer(to, amount);
+          ERC20(tokenAddress).transfer(to, transfer.amount);
         }
       }
     }
