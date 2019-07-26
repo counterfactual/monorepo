9333c49b7f137dadd94accbebbac1fa09682925e
Liam Horne <liam@lihorne.com>
2019-07-25 22:38:51 -0400

    Roll up jest.config.js files into default monorepo one

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "bail": true,
+  "coverageDirectory": "jest-coverage",
+  "coveragePathIgnorePatterns": [
+    "test"
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": 90
+    }
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "json"
+  ],
+  "rootDir": ".",
+  "roots": [
+    "test"
+  ],
+  "testPathIgnorePatterns": [
+    "node_modules",
+    "dist"
+  ],
+  "testRegex": "\\.spec.(jsx?|tsx?)$",
+  "testURL": "http://localhost/",
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "verbose": false,
+  "extraGlobals": ["Math"]
+}
