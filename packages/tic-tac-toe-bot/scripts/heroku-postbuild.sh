0455d3504de04eb27898ef8fb512381bb774c521
Liam Horne <snario@users.noreply.github.com>
2019-07-28 11:31:10 -0400

    Use lerna build and remove build.sh (#2048)
    * Use lerna build and remove build.sh
    
    * Add dependencies to force topological order

--- packages/tic-tac-toe-bot/scripts/heroku-postbuild.sh
+++ packages/tic-tac-toe-bot/scripts/heroku-postbuild.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ ! -z "${IS_HEROKU_ENV}" ]; then
     echo "📟  Detected Heroku. Transpiling Typescript into JS for Node.JS to run on server."
-    tsc -p tsconfig.heroku.json
+    tsc -b tsconfig.heroku.json
 fi
