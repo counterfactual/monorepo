88d64ae05035d204bfb7d3410dbdddce7e15c8d6
Liam Horne <liam@lihorne.com>
2019-07-31 12:54:34 -0400

    Fix typo

--- packages/cf-adjudicator-contracts/test/app-instance-adjudicator.spec.ts
+++ packages/cf-adjudicator-contracts/test/app-instance-adjudicator.spec.ts
@@ -7,23 +7,23 @@ import { defaultAbiCoder } from "ethers/utils";
 import AppInstanceAdjudicator from "../build/AppInstanceAdjudicator.json";
 import AppWithAction from "../build/AppWithAction.json";
 
-const ALICE =
-  // 0xaeF082d339D227646DB914f0cA9fF02c8544F30b
-  new Wallet(
-    "0x3570f77380e22f8dc2274d8fd33e7830cc2d29cf76804e8c21f4f7a6cc571d27"
-  );
-
-const BOB =
-  // 0xb37e49bFC97A948617bF3B63BC6942BB15285715
-  new Wallet(
-    "0x4ccac8b1e81fb18a98bbaf29b9bfe307885561f71b76bd4680d7aec9d0ddfcfd"
-  );
-
-describe("AppInstanceAdjudicator", () => {
+describe("Force Move Game Rules", () => {
   let provider: Web3Provider;
   let wallet: Wallet;
   let adjudicator: Contract;
   let app: Contract;
+
+  const ALICE =
+    // 0xaeF082d339D227646DB914f0cA9fF02c8544F30b
+    new Wallet(
+      "0x3570f77380e22f8dc2274d8fd33e7830cc2d29cf76804e8c21f4f7a6cc571d27"
+    );
+
+  const BOB =
+    // 0xb37e49bFC97A948617bF3B63BC6942BB15285715
+    new Wallet(
+      "0x4ccac8b1e81fb18a98bbaf29b9bfe307885561f71b76bd4680d7aec9d0ddfcfd"
+    );
 
   before(async () => {
     provider = waffle.createMockProvider();
@@ -36,12 +36,7 @@ describe("AppInstanceAdjudicator", () => {
       []
     );
 
-    app = await waffle.deployContract(
-      wallet,
-      AppWithAction,
-      []
-    );
-
+    app = await waffle.deployContract(wallet, AppWithAction, []);
   });
 
   it("test", async () => {
@@ -53,7 +48,7 @@ describe("AppInstanceAdjudicator", () => {
       challengeTimeout: 100,
       nonce: 0,
       versionNum: 0,
-      stateType: 0,
+      stateType: 0
     };
 
     await adjudicator.functions.challenge(
@@ -66,8 +61,7 @@ describe("AppInstanceAdjudicator", () => {
         ...appInstanceState,
         versionNum: 1,
         actionTaken: defaultAbiCoder.encode([""])
-      },
-      bytes[] memory signatures
-    )
+      }
+    );
   });
 });
