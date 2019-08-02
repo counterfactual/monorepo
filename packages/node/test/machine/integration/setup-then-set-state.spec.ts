07da45bdb87c629acd0e3323907977b03bc99ed0
Liam Horne <liam@lihorne.com>
2019-08-02 19:28:27 -0400

    Remove unused import

--- packages/node/test/machine/integration/setup-then-set-state.spec.ts
+++ packages/node/test/machine/integration/setup-then-set-state.spec.ts
@@ -1,14 +1,11 @@
 import ChallengeRegistry from "@counterfactual/cf-adjudicator-contracts/build/ChallengeRegistry.json";
 import MinimumViableMultisig from "@counterfactual/cf-funding-protocol-contracts/build/MinimumViableMultisig.json";
 import ProxyFactory from "@counterfactual/cf-funding-protocol-contracts/build/ProxyFactory.json";
-import {
-  multiAssetMultiPartyCoinTransferInterpreterParamsEncoding,
-  NetworkContext
-} from "@counterfactual/types";
+import { NetworkContext } from "@counterfactual/types";
 import { Contract, Wallet } from "ethers";
 import { WeiPerEther, Zero } from "ethers/constants";
 import { JsonRpcProvider } from "ethers/providers";
-import { defaultAbiCoder, Interface, keccak256 } from "ethers/utils";
+import { Interface, keccak256 } from "ethers/utils";
 
 import { CONVENTION_FOR_ETH_TOKEN_ADDRESS } from "../../../src/constants";
 import { SetStateCommitment, SetupCommitment } from "../../../src/ethereum";
