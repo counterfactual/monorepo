39c82daf23bb236449516f61f79b2f7da6209de9
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-08-08 15:40:22 -0300

    added Ethereum global mock

--- packages/node-provider/src/types.ts
+++ packages/node-provider/src/types.ts
@@ -1,12 +1,11 @@
 import { Node } from "@counterfactual/types";
 import { IpcProvider, JsonRPCResponse } from "web3/providers";
 
+export type NodeEthereumResponse = JsonRPCResponse & { result: Node.Message };
+
 export type EthereumGlobal = Omit<IpcProvider, "send"> & {
   enable: () => Promise<void>;
   selectedAddress: string;
   networkVersion: string;
-  send: (
-    eventOrMethod: string,
-    data?: any[]
-  ) => Promise<JsonRPCResponse & { result: Node.Message }>;
+  send: (eventOrMethod: string, data?: any[]) => Promise<NodeEthereumResponse>;
 };
