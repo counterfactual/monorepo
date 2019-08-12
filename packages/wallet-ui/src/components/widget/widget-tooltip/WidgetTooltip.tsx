ae72231517ac57595dda0b72655a5b363c9bcc51
Santiago Persico <santiagopersico@gmail.com>
2019-08-12 17:43:38 -0300

    add new behaviour tooltip (go toLeft)

--- packages/wallet-ui/src/components/widget/widget-tooltip/WidgetTooltip.tsx
+++ packages/wallet-ui/src/components/widget/widget-tooltip/WidgetTooltip.tsx
@@ -4,17 +4,21 @@ import "./WidgetTooltip.scss";
 export type WidgetTooltipProps = {
   children?: React.ReactNode;
   message?: string;
+  toLeft?: boolean;
 };
 
 const WidgetTooltip: React.FC<WidgetTooltipProps> = ({
   children,
-  message = ""
+  message = "",
+  toLeft = false
 }: WidgetTooltipProps) => {
   return (
     <div className="widget-tooltip">
       {children}
       {message ? (
-        <div className="widget-tooltip-message">{message}</div>
+        <div className={`widget-tooltip-message${toLeft ? " toLeft" : ""}`}>
+          {message}
+        </div>
       ) : (
         undefined
       )}
