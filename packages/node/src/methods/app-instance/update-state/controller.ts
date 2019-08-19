a684a6b5c3dc232ca1af4d19dabde4d67ca67585
Liam Horne <snario@users.noreply.github.com>
2019-08-19 16:33:13 +0200

    Rename util file to be clearer (#2215)
    * Rename util file to be clearer
    
    * Fix linting issues

--- packages/node/src/methods/app-instance/update-state/controller.ts
+++ packages/node/src/methods/app-instance/update-state/controller.ts
@@ -7,7 +7,10 @@ import { InstructionExecutor, Protocol } from "../../../machine";
 import { StateChannel } from "../../../models";
 import { RequestHandler } from "../../../request-handler";
 import { Store } from "../../../store";
-import { getCounterpartyAddress, prettyPrintObject } from "../../../utils";
+import {
+  getFirstElementInListNotEqualTo,
+  prettyPrintObject
+} from "../../../utils";
 import { NodeController } from "../../controller";
 import {
   IMPROPERLY_FORMATTED_STRUCT,
@@ -67,7 +70,7 @@ export default class UpdateStateController extends NodeController {
 
     const sc = await store.getChannelFromAppInstanceID(appInstanceId);
 
-    const responderXpub = getCounterpartyAddress(
+    const responderXpub = getFirstElementInListNotEqualTo(
       publicIdentifier,
       sc.userNeuteredExtendedKeys
     );
