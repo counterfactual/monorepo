1373c8e35a4715565f9fb78f410000213028d3ac
cf19drofxots <nima@l4v.io>
2019-08-19 12:11:07 -0700

    Revert contract deployment mod

--- packages/cf-funding-protocol-contracts/migrations/2_deploy_contracts.js
+++ packages/cf-funding-protocol-contracts/migrations/2_deploy_contracts.js
@@ -1,7 +1,18 @@
 const tdr = require("truffle-deploy-registry");
 
 const ARTIFACTS = [
-  artifacts.require("ProxyFactory")
+  artifacts.require("ChallengeRegistry"),
+  artifacts.require("CoinBalanceRefundApp"),
+  artifacts.require("ConditionalTransactionDelegateTarget"),
+  artifacts.require("IdentityApp"),
+  artifacts.require("MinimumViableMultisig"),
+  artifacts.require("MultiAssetMultiPartyCoinTransferInterpreter"),
+  artifacts.require("ProxyFactory"),
+  artifacts.require("SingleAssetTwoPartyCoinTransferFromVirtualAppInterpreter"),
+  artifacts.require("SingleAssetTwoPartyCoinTransferInterpreter"),
+  artifacts.require("TimeLockedPassThrough"),
+  artifacts.require("TwoPartyFixedOutcomeFromVirtualAppInterpreter"),
+  artifacts.require("TwoPartyFixedOutcomeInterpreter")
 ];
 
 module.exports = (deployer, network) => {
