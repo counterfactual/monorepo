e500047151689cd7c9ca74d8280bbfb344075bee
Li Xuanji <xuanji@gmail.com>
2019-08-19 16:56:11 -0400

    [contracts] make Proxy build reproducible across machines

--- packages/cf-funding-protocol-contracts/waffle.js
+++ packages/cf-funding-protocol-contracts/waffle.js
@@ -6,17 +6,27 @@ var waffleConfig = {
   "npmPath": "../../node_modules",
   "legacyOutput": true,
   "compilerOptions": {
-    "evmVersion": "constantinople"
+    "optimizer": {
+      "enabled": true,
+      "runs": 200
+    },
+    "evmVersion": "petersburg",
+    "metadata": {
+      "useLiteralContent": true
+    },
+    "outputSelection": {
+      "*": {
+        "*": [
+          "metadata", "evm.bytecode", "evm.bytecode.sourceMap"
+        ],
+      },
+    }
+
   }
 };
 
 var selectSolc = () => {
-  // TODO: which should select "native" in CI, but the solc binary in the CI
-  // environment is currently too old
-  if (process.env.NATIVE_SOLC == "true") {
-    waffleConfig.compiler = "native";
-  }
-
+  waffleConfig.compiler = "solcjs";
   return waffleConfig;
 }
 
