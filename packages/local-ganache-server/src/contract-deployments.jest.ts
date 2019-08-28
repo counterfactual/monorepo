57c7e6a80dcec46964ed4a6caeb4d8df8f2e81f8
LayneHaber <layne.haber@gmail.com>
2019-08-27 20:41:13 -0700

    deploy both transfer apps into network context

--- packages/local-ganache-server/src/contract-deployments.jest.ts
+++ packages/local-ganache-server/src/contract-deployments.jest.ts
@@ -1,4 +1,6 @@
 import TicTacToeApp from "@counterfactual/apps/expected-build-artifacts/TicTacToeApp.json";
+import UnidirectionalLinkedTransferApp from "@counterfactual/apps/expected-build-artifacts/UnidirectionalLinkedTransferApp.json";
+import UnidirectionalTransferApp from "@counterfactual/apps/expected-build-artifacts/UnidirectionalTransferApp.json";
 import ChallengeRegistry from "@counterfactual/cf-adjudicator-contracts/expected-build-artifacts/ChallengeRegistry.json";
 import CoinBalanceRefundApp from "@counterfactual/cf-funding-protocol-contracts/expected-build-artifacts/CoinBalanceRefundApp.json";
 import ConditionalTransactionDelegateTarget from "@counterfactual/cf-funding-protocol-contracts/expected-build-artifacts/ConditionalTransactionDelegateTarget.json";
@@ -17,6 +19,8 @@ import { ContractFactory, Wallet } from "ethers";
 export type NetworkContextForTestSuite = NetworkContext & {
   TicTacToeApp: string;
   DolphinCoin: string;
+  UnidirectionalTransferApp: string;
+  UnidirectionalLinkedTransferApp: string;
 };
 
 export async function deployTestArtifactsToChain(wallet: Wallet) {
@@ -86,6 +90,18 @@ export async function deployTestArtifactsToChain(wallet: Wallet) {
     wallet
   ).deploy();
 
+  const transferContract = await new ContractFactory(
+    UnidirectionalTransferApp.abi,
+    UnidirectionalTransferApp.bytecode,
+    wallet
+  ).deploy();
+
+  const linkContract = await new ContractFactory(
+    UnidirectionalLinkedTransferApp.abi,
+    UnidirectionalLinkedTransferApp.bytecode,
+    wallet
+  ).deploy();
+
   const timeLockedPassThrough = await new ContractFactory(
     TimeLockedPassThrough.abi,
     TimeLockedPassThrough.evm.bytecode,
@@ -115,6 +131,8 @@ export async function deployTestArtifactsToChain(wallet: Wallet) {
     TimeLockedPassThrough: timeLockedPassThrough.address,
     TwoPartyFixedOutcomeInterpreter: twoPartyFixedOutcomeInterpreter.address,
     TwoPartyFixedOutcomeFromVirtualAppInterpreter:
-      twoPartyFixedOutcomeFromVirtualAppETHInterpreter.address
+      twoPartyFixedOutcomeFromVirtualAppETHInterpreter.address,
+    UnidirectionalLinkedTransferApp: linkContract.address,
+    UnidirectionalTransferApp: transferContract.address
   } as NetworkContextForTestSuite;
 }
