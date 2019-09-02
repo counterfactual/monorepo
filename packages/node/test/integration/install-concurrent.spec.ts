06347c36e9058de266b0ff2585a46947048821c6
LayneHaber <layne.haber@gmail.com>
2019-09-01 16:54:26 -1000

    concurrency test passing, prettier

--- packages/node/test/integration/install-concurrent.spec.ts
+++ packages/node/test/integration/install-concurrent.spec.ts
@@ -1,5 +1,6 @@
 import { NetworkContextForTestSuite } from "@counterfactual/local-ganache-server";
 import { One } from "ethers/constants";
+import { bigNumberify } from "ethers/utils";
 
 import { Node } from "../../src";
 import { CONVENTION_FOR_ETH_TOKEN_ADDRESS } from "../../src/constants";
@@ -10,11 +11,16 @@ import { setup, SetupContext } from "./setup";
 import {
   collateralizeChannel,
   createChannel,
+  getInstalledAppInstances,
   makeInstallCall,
-  makeProposeCall,
+  makeProposeCall
 } from "./utils";
 
 expect.extend({ toBeLt });
+
+const delay = async (ms: number) => {
+  await new Promise(resolve => setTimeout(resolve, ms));
+};
 
 describe("Node method follows spec - install", () => {
   let multisigAddress: string;
@@ -33,32 +39,48 @@ describe("Node method follows spec - install", () => {
         multisigAddress = await createChannel(nodeA, nodeB);
       });
 
+      jest.setTimeout(7000);
+
       it("install apps with ETH concurrently", async done => {
-        await collateralizeChannel(nodeA, nodeB, multisigAddress);
+        await collateralizeChannel(
+          nodeA,
+          nodeB,
+          multisigAddress,
+          bigNumberify(3)
+        );
 
         nodeB.on(NODE_EVENTS.PROPOSE_INSTALL, async (msg: ProposeMessage) => {
-          await new Promise(resolve => setTimeout(resolve, 1000));
           makeInstallCall(nodeB, msg.data.appInstanceId);
-          await new Promise(resolve => setTimeout(resolve, 1000));
-
-          expect(done);
         });
 
-        const proposeRpc = () => makeProposeCall(
-          nodeB,
-          (global["networkContext"] as NetworkContextForTestSuite).TicTacToeApp,
-          /* initialState */ undefined,
-          One,
-          CONVENTION_FOR_ETH_TOKEN_ADDRESS,
-          One,
-          CONVENTION_FOR_ETH_TOKEN_ADDRESS
-        );
+        const proposeRpc = () =>
+          makeProposeCall(
+            nodeB,
+            (global["networkContext"] as NetworkContextForTestSuite)
+              .TicTacToeApp,
+            /* initialState */ undefined,
+            One,
+            CONVENTION_FOR_ETH_TOKEN_ADDRESS,
+            One,
+            CONVENTION_FOR_ETH_TOKEN_ADDRESS
+          );
         await Promise.all([
           nodeA.rpcRouter.dispatch(proposeRpc()),
           nodeA.rpcRouter.dispatch(proposeRpc())
-        ])
+        ]);
+
+        const pollApps = async () => {
+          while ((await getInstalledAppInstances(nodeA)).length < 2) {
+            await delay(100);
+          }
+        };
+
+        await pollApps();
+        const aApps = await getInstalledAppInstances(nodeA);
+        const bApps = await getInstalledAppInstances(nodeB);
+        expect(aApps).toEqual(bApps);
+        done();
       });
     }
   );
 });
-
