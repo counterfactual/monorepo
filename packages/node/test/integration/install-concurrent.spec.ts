d457170205467abd7f29fa86fce4c45e27ef9564
LayneHaber <layne.haber@gmail.com>
2019-09-01 22:56:02 -1000

    add virtual concurrency test

--- packages/node/test/integration/install-concurrent.spec.ts
+++ packages/node/test/integration/install-concurrent.spec.ts
@@ -4,7 +4,11 @@ import { bigNumberify } from "ethers/utils";
 
 import { Node } from "../../src";
 import { CONVENTION_FOR_ETH_TOKEN_ADDRESS } from "../../src/constants";
-import { NODE_EVENTS, ProposeMessage } from "../../src/types";
+import {
+  NODE_EVENTS,
+  ProposeMessage,
+  ProposeVirtualMessage
+} from "../../src/types";
 import { toBeLt } from "../machine/integration/bignumber-jest-matcher";
 
 import { setup, SetupContext } from "./setup";
@@ -12,8 +16,10 @@ import {
   collateralizeChannel,
   createChannel,
   getInstalledAppInstances,
+  installTTTVirtual,
   makeInstallCall,
-  makeProposeCall
+  makeProposeCall,
+  makeVirtualProposeCall
 } from "./utils";
 
 expect.extend({ toBeLt });
@@ -23,9 +29,11 @@ const delay = async (ms: number) => {
 };
 
 describe("Node method follows spec - install", () => {
-  let multisigAddress: string;
+  let multisigAddressAB: string;
+  let multisigAddressBC: string;
   let nodeA: Node;
   let nodeB: Node;
+  let nodeC: Node;
 
   describe(
     "Node A gets app install proposal, sends to node B, B approves it, installs it, " +
@@ -36,7 +44,7 @@ describe("Node method follows spec - install", () => {
         nodeA = context["A"].node;
         nodeB = context["B"].node;
 
-        multisigAddress = await createChannel(nodeA, nodeB);
+        multisigAddressAB = await createChannel(nodeA, nodeB);
       });
 
       jest.setTimeout(7000);
@@ -45,7 +53,7 @@ describe("Node method follows spec - install", () => {
         await collateralizeChannel(
           nodeA,
           nodeB,
-          multisigAddress,
+          multisigAddressAB,
           bigNumberify(3)
         );
 
@@ -68,6 +76,77 @@ describe("Node method follows spec - install", () => {
           nodeA.rpcRouter.dispatch(proposeRpc()),
           nodeA.rpcRouter.dispatch(proposeRpc())
         ]);
+
+        const pollApps = async () => {
+          while ((await getInstalledAppInstances(nodeA)).length < 2) {
+            await delay(100);
+          }
+        };
+
+        await pollApps();
+        const aApps = await getInstalledAppInstances(nodeA);
+        const bApps = await getInstalledAppInstances(nodeB);
+        expect(aApps).toEqual(bApps);
+        done();
+      });
+    }
+  );
+
+  describe(
+    "Node A gets app install virtual proposal, sends to node C, C approves it, installs it, " +
+      "sends acks back to A, A installs it, both nodes have the same virtual app instance",
+    () => {
+      beforeEach(async () => {
+        const context: SetupContext = await setup(global, true);
+        nodeA = context["A"].node;
+        nodeB = context["B"].node;
+        nodeC = context["C"].node;
+
+        multisigAddressAB = await createChannel(nodeA, nodeB);
+        multisigAddressBC = await createChannel(nodeB, nodeC);
+      });
+
+      jest.setTimeout(7000);
+
+      it("install virtual apps with ETH concurrently", async done => {
+        // collateralize all nodes in both channels
+        await collateralizeChannel(
+          nodeA,
+          nodeB,
+          multisigAddressAB,
+          bigNumberify(3)
+        );
+
+        await collateralizeChannel(
+          nodeB,
+          nodeC,
+          multisigAddressBC,
+          bigNumberify(3)
+        );
+
+        expect(multisigAddressAB).toBeDefined();
+        expect(multisigAddressBC).toBeDefined();
+
+        nodeC.on(
+          NODE_EVENTS.PROPOSE_INSTALL_VIRTUAL,
+          async (msg: ProposeVirtualMessage) => {
+            await installTTTVirtual(
+              nodeC,
+              msg.data.appInstanceId,
+              nodeB.publicIdentifier
+            );
+          }
+        );
+
+        const proposeVirtualRpc = () =>
+          makeVirtualProposeCall(
+            nodeA,
+            nodeC,
+            nodeB,
+            (global["networkContext"] as NetworkContextForTestSuite)
+              .TicTacToeApp
+          );
+        await Promise.all([proposeVirtualRpc(), proposeVirtualRpc()]);
 
         const pollApps = async () => {
           while ((await getInstalledAppInstances(nodeA)).length < 2) {
