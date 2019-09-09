a9106bc29be874ea5c1791c80add1179f4f0e873
LayneHaber <layne.haber@gmail.com>
2019-09-09 14:11:55 -0700

    add test cases for uninstall regular, install virtual

--- packages/node/test/integration/uninstall-install-concurrent-mixed.spec.ts
+++ packages/node/test/integration/uninstall-install-concurrent-mixed.spec.ts
@@ -4,12 +4,13 @@ import { parseEther } from "ethers/utils";
 
 import { Node } from "../../src";
 import { CONVENTION_FOR_ETH_TOKEN_ADDRESS } from "../../src/constants";
-import { NODE_EVENTS, ProposeMessage } from "../../src/types";
+import { InstallMessage, NODE_EVENTS, ProposeMessage } from "../../src/types";
 import { toBeLt } from "../machine/integration/bignumber-jest-matcher";
 
 import { setup, SetupContext } from "./setup";
 import {
   collateralizeChannel,
+  constructUninstallRpc,
   constructUninstallVirtualRpc,
   createChannel,
   installVirtualApp,
@@ -21,7 +22,7 @@ expect.extend({ toBeLt });
 
 jest.setTimeout(7500);
 
-describe("Node method follows spec when happening concurrently - install / uninstall", () => {
+describe("Node method follows spec when happening concurrently - install / uninstall virtual", () => {
   let multisigAddressAB: string;
   let multisigAddressBC: string;
   let nodeA: Node;
@@ -30,7 +31,7 @@ describe("Node method follows spec when happening concurrently - install / unins
   let installedAppInstanceId: string;
   let installCall;
 
-  describe("NodeA can uninstall virtual and install regular an app with nodeB concurrently", () => {
+  describe("NodeA can uninstall virtual app with NodeC and install regular app with nodeB concurrently", () => {
     beforeEach(async () => {
       const context: SetupContext = await setup(global, true);
       nodeA = context["A"].node;
@@ -103,16 +104,6 @@ describe("Node method follows spec when happening concurrently - install / unins
         }
       });
 
-      const installCall = makeProposeCall(
-        nodeB,
-        (global["networkContext"] as NetworkContextForTestSuite).TicTacToeApp,
-        /* initialState */ undefined,
-        One,
-        CONVENTION_FOR_ETH_TOKEN_ADDRESS,
-        One,
-        CONVENTION_FOR_ETH_TOKEN_ADDRESS
-      );
-
       nodeA.rpcRouter.dispatch(installCall);
       nodeA.rpcRouter.dispatch(
         constructUninstallVirtualRpc(
@@ -122,7 +113,7 @@ describe("Node method follows spec when happening concurrently - install / unins
       );
     });
 
-    it("install app with ETH then uninstall and install apps simultaneously from separate nodes", async done => {
+    it("install app with ETH then uninstall virtual and install apps simultaneously from separate nodes", async done => {
       let completedActions = 0;
 
       nodeB.once(NODE_EVENTS.PROPOSE_INSTALL, (msg: ProposeMessage) => {
@@ -143,22 +134,126 @@ describe("Node method follows spec when happening concurrently - install / unins
         }
       });
 
-      const installCall = makeProposeCall(
-        nodeB,
-        (global["networkContext"] as NetworkContextForTestSuite).TicTacToeApp,
-        /* initialState */ undefined,
-        One,
-        CONVENTION_FOR_ETH_TOKEN_ADDRESS,
-        One,
-        CONVENTION_FOR_ETH_TOKEN_ADDRESS
-      );
-
       nodeA.rpcRouter.dispatch(installCall);
       nodeC.rpcRouter.dispatch(
         constructUninstallVirtualRpc(
           installedAppInstanceId,
           nodeB.publicIdentifier
         )
+      );
+    });
+  });
+});
+
+describe("Node method follows spec when happening concurrently - install virtual / uninstall", () => {
+  let multisigAddressAB: string;
+  let multisigAddressBC: string;
+  let nodeA: Node;
+  let nodeB: Node;
+  let nodeC: Node;
+  let installedAppInstanceId: string;
+
+  describe("NodeA can uninstall regular app with NodeB and install virtual app with nodeC concurrently", () => {
+    beforeEach(async () => {
+      const context: SetupContext = await setup(global, true);
+      nodeA = context["A"].node;
+      nodeB = context["B"].node;
+      nodeC = context["C"].node;
+
+      multisigAddressAB = await createChannel(nodeA, nodeB);
+      multisigAddressBC = await createChannel(nodeB, nodeC);
+
+      // collateralize both channels
+      await collateralizeChannel(
+        multisigAddressAB,
+        nodeA,
+        nodeB,
+        parseEther("2")
+      );
+
+      await collateralizeChannel(
+        multisigAddressBC,
+        nodeB,
+        nodeC,
+        parseEther("2")
+      );
+
+      const appDef = (global["networkContext"] as NetworkContextForTestSuite)
+        .TicTacToeApp;
+
+      // install regular app
+      installedAppInstanceId = await new Promise(async resolve => {
+        nodeB.once(NODE_EVENTS.PROPOSE_INSTALL, (msg: ProposeMessage) => {
+          makeInstallCall(nodeB, msg.data.appInstanceId);
+        });
+
+        nodeA.once(NODE_EVENTS.INSTALL, (msg: InstallMessage) => {
+          resolve(msg.data.params.appInstanceId);
+        });
+
+        await nodeA.rpcRouter.dispatch(
+          makeProposeCall(
+            nodeB,
+            appDef,
+            undefined,
+            One,
+            CONVENTION_FOR_ETH_TOKEN_ADDRESS,
+            One,
+            CONVENTION_FOR_ETH_TOKEN_ADDRESS
+          )
+        );
+      });
+    });
+
+    it("nodeA calls uninstall and install virtual", async done => {
+      let completedActions = 0;
+
+      nodeB.once(NODE_EVENTS.UNINSTALL, () => {
+        completedActions += 1;
+        if (completedActions === 2) {
+          done();
+        }
+      });
+
+      nodeA.once(NODE_EVENTS.INSTALL_VIRTUAL, () => {
+        completedActions += 1;
+        if (completedActions === 2) {
+          done();
+        }
+      });
+
+      nodeA.rpcRouter.dispatch(constructUninstallRpc(installedAppInstanceId));
+      installVirtualApp(
+        nodeA,
+        nodeB,
+        nodeC,
+        (global["networkContext"] as NetworkContextForTestSuite).TicTacToeApp
+      );
+    });
+
+    it("separate nodes make the uninstall and install virtual calls", async done => {
+      let completedActions = 0;
+
+      nodeA.once(NODE_EVENTS.UNINSTALL, () => {
+        completedActions += 1;
+        if (completedActions === 2) {
+          done();
+        }
+      });
+
+      nodeA.once(NODE_EVENTS.INSTALL_VIRTUAL, () => {
+        completedActions += 1;
+        if (completedActions === 2) {
+          done();
+        }
+      });
+
+      nodeB.rpcRouter.dispatch(constructUninstallRpc(installedAppInstanceId));
+      installVirtualApp(
+        nodeA,
+        nodeB,
+        nodeC,
+        (global["networkContext"] as NetworkContextForTestSuite).TicTacToeApp
       );
     });
   });
