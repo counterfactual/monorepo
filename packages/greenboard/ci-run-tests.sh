021ed85e161416f0d1df0c6a2418581abc806bea
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-09 16:09:56 -0300

    using --unsafe-perm, --build-from-source

--- packages/greenboard/ci-run-tests.sh
+++ packages/greenboard/ci-run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-alias yarn_install_command="yarn --ignore-engines"
-alias yarn_ci_command="yarn --frozen-lockfile --ignore-engines"
+alias yarn_install_command="yarn --ignore-engines --unsafe-perm --build-from-source"
+alias yarn_ci_command="yarn --frozen-lockfile --ignore-engines --unsafe-perm --build-from-source"
 alias yarn_build_command="yarn --ignore-engines build"
 alias yarn_dist_command="yarn --ignore-engines dist"
 alias yarn_rm_command="yarn --ignore-engines remove"
