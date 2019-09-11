5ac5147b010292a0db5db0f08c486bbc60fed951
Liam Horne <liam@lihorne.com>
2019-09-11 14:42:53 -0400

    Add an ILockService concept to ProcessQueue

--- packages/node/src/process-queue.ts
+++ packages/node/src/process-queue.ts
@@ -1,9 +1,38 @@
 import Queue, { Task } from "p-queue";
 
+import MemoryLockService from "./lock/memory-lock-service";
+import { ILockInterface } from "./lock/types";
 import { addToManyQueues } from "./methods/queued-execution";
 
+class QueueWithLockingServiceConnection extends Queue {
+  constructor(
+    private readonly queueName,
+    private readonly lockingService: ILockInterface,
+    ...args: any[]
+  ) {
+    super(...args);
+  }
+
+  async add(task: Task<any>) {
+    return super.add(() =>
+      this.lockingService.acquireLock(this.queueName, task, 30_000)
+    );
+  }
+}
+
 export default class ProcessQueue {
-  private readonly queues: Map<string, Queue> = new Map<string, Queue>();
+  private readonly queues: Map<
+    string,
+    QueueWithLockingServiceConnection
+  > = new Map<string, QueueWithLockingServiceConnection>();
+
+  constructor(
+    // NOTE: This hard-codes a LOCAL single lock service in any ProcessQueue,
+    // this means it is not acting as a centralized lock service right now; a
+    // further implementation should extend this primitive and pass in a global
+    // service into this constructor that also implements the interface.
+    private readonly lockingService: ILockInterface = new MemoryLockService()
+  ) {}
 
   addTask(queueKeys: string[], task: Task<any>) {
     return addToManyQueues(
@@ -12,9 +41,16 @@ export default class ProcessQueue {
     );
   }
 
-  private getOrCreateQueue(queueKey: string): Queue {
+  private getOrCreateQueue(
+    queueKey: string
+  ): QueueWithLockingServiceConnection {
     if (!this.queues.has(queueKey)) {
-      this.queues.set(queueKey, new Queue({ concurrency: 1 }));
+      this.queues.set(
+        queueKey,
+        new QueueWithLockingServiceConnection(queueKey, this.lockingService, {
+          concurrency: 1
+        })
+      );
     }
     return this.queues.get(queueKey)!;
   }
