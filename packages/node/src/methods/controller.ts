6a7a2fe71ba8344ab4c460997a54b29466538062
Liam Horne <liam@lihorne.com>
2019-09-11 16:41:10 -0400

    wip

--- packages/node/src/methods/controller.ts
+++ packages/node/src/methods/controller.ts
@@ -17,6 +17,8 @@ export abstract class NodeController extends Controller {
       params
     );
 
+    console.log(`❓ ${requestHandler.publicIdentifier.substr(4, 8)} reuqesting locks ${queueNames.map(x => x.substr(0, 4))}`)
+
     const createExecutionPromise = () =>
       this.executeMethodImplementation(requestHandler, params);
 
