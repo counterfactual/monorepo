96acedc98198b3c985782d70c198bff7262d61a7
Liam Horne <liam@lihorne.com>
2019-09-11 21:26:28 -0400

    Rename variables in take action protocol

--- packages/node/src/protocol/take-action.ts
+++ packages/node/src/protocol/take-action.ts
@@ -49,10 +49,14 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
       appInstance.timeout
     );
 
-    const mySig = yield [OP_SIGN, setStateCommitment, appInstance.appSeqNo];
+    const initiatorSignature = yield [
+      OP_SIGN,
+      setStateCommitment,
+      appInstance.appSeqNo
+    ];
 
     const {
-      customData: { signature }
+      customData: { signature: responderSignature }
     } = yield [
       IO_SEND_AND_WAIT,
       {
@@ -62,7 +66,7 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
         seq: 1,
         toXpub: responderXpub,
         customData: {
-          signature: mySig
+          signature: initiatorSignature
         }
       } as ProtocolMessage
     ];
@@ -70,7 +74,7 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
     assertIsValidSignature(
       xkeyKthAddress(responderXpub, appInstance.appSeqNo),
       setStateCommitment,
-      signature
+      responderSignature
     );
 
     context.stateChannelsMap.set(
@@ -85,7 +89,7 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
     const {
       processID,
       params,
-      customData: { signature }
+      customData: { signature: initiatorSignature }
     } = message;
 
     const {
@@ -119,10 +123,14 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
     assertIsValidSignature(
       xkeyKthAddress(initiatorXpub, appInstance.appSeqNo),
       setStateCommitment,
-      signature
+      initiatorSignature
     );
 
-    const mySig = yield [OP_SIGN, setStateCommitment, appInstance.appSeqNo];
+    const responderSignature = yield [
+      OP_SIGN,
+      setStateCommitment,
+      appInstance.appSeqNo
+    ];
 
     yield [
       IO_SEND,
@@ -132,7 +140,7 @@ export const TAKE_ACTION_PROTOCOL: ProtocolExecutionFlow = {
         toXpub: initiatorXpub,
         seq: -1,
         customData: {
-          signature: mySig
+          signature: responderSignature
         }
       } as ProtocolMessage
     ];
