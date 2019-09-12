ca1a210a0f25372c2c107aeae10fa4e6fe706de9
Liam Horne <snario@users.noreply.github.com>
2019-09-12 12:39:40 -0400

    Add an ILockService concept to ProcessQueue (#2386)
    * Add an ILockService concept to ProcessQueue
    
    * Add missing await
    
    * Use async await syntax for MemoryLockService
    
    * Simplify check for whether or not lock is available
    
    * Add isAcquired method
    
    * Add an ILockService concept to ProcessQueue
    
    * Add missing await
    
    * Use async await syntax for MemoryLockService
    
    * Simplify check for whether or not lock is available
    
    * Add isAcquired method
    
    * Make lockService optional
    
    * Move types
    
    * Remove extra type
    
    * Proper deferred
    
    * Rename ILockInterface to ILockService

--- packages/node/src/process-queue.ts
+++ packages/node/src/process-queue.ts
@@ -1,9 +1,37 @@
+import { Node } from "@counterfactual/types";
 import Queue, { Task } from "p-queue";
 
 import { addToManyQueues } from "./methods/queued-execution";
 
+class QueueWithLockingServiceConnection extends Queue {
+  constructor(
+    private readonly queueName,
+    private readonly lockingService: Node.ILockService,
+    ...args: any[]
+  ) {
+    super(...args);
+  }
+
+  async add(task: Task<any>) {
+    return super.add(() =>
+      this.lockingService.acquireLock(this.queueName, task, 30_000)
+    );
+  }
+}
+
 export default class ProcessQueue {
-  private readonly queues: Map<string, Queue> = new Map<string, Queue>();
+  private readonly queues: Map<
+    string,
+    QueueWithLockingServiceConnection | Queue
+  > = new Map<string, QueueWithLockingServiceConnection | Queue>();
+
+  constructor(
+    // NOTE: This hard-codes a LOCAL single lock service in any ProcessQueue,
+    // this means it is not acting as a centralized lock service right now; a
+    // further implementation should extend this primitive and pass in a global
+    // service into this constructor that also implements the interface.
+    private readonly lockingService?: Node.ILockService
+  ) {}
 
   addTask(queueKeys: string[], task: Task<any>) {
     return addToManyQueues(
@@ -12,9 +40,22 @@ export default class ProcessQueue {
     );
   }
 
-  private getOrCreateQueue(queueKey: string): Queue {
+  private getOrCreateQueue(
+    queueKey: string
+  ): QueueWithLockingServiceConnection | Queue {
     if (!this.queues.has(queueKey)) {
-      this.queues.set(queueKey, new Queue({ concurrency: 1 }));
+      this.queues.set(
+        queueKey,
+        this.lockingService
+          ? new QueueWithLockingServiceConnection(
+              queueKey,
+              this.lockingService,
+              {
+                concurrency: 1
+              }
+            )
+          : new Queue({ concurrency: 1 })
+      );
     }
     return this.queues.get(queueKey)!;
   }
