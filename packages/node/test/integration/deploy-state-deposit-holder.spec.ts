8d3041b03db248ee3a4379359bba4cd508bb51a2
Rahul <rksethuram9@gmail.com>
2019-09-27 10:13:00 +0200

    Add test for blocked withdrawals

--- packages/node/test/integration/deploy-state-deposit-holder.spec.ts
+++ packages/node/test/integration/deploy-state-deposit-holder.spec.ts
@@ -1,11 +1,19 @@
-import { HashZero } from "ethers/constants";
+import { HashZero, One, Zero } from "ethers/constants";
+import { JsonRpcProvider } from "ethers/providers";
+import { getAddress, hexlify, randomBytes } from "ethers/utils";
 import log from "loglevel";
 
 import { Node, NODE_EVENTS } from "../../src";
+import { CONVENTION_FOR_ETH_TOKEN_ADDRESS } from "../../src/constants";
 import { toBeEq } from "../machine/integration/bignumber-jest-matcher";
 
 import { setup, SetupContext } from "./setup";
-import { createChannel, deployStateDepositHolder } from "./utils";
+import {
+  constructWithdrawRpc,
+  createChannel,
+  deployStateDepositHolder,
+  deposit
+} from "./utils";
 
 expect.extend({ toBeEq });
 
@@ -14,10 +22,12 @@ log.setLevel(log.levels.SILENT);
 describe("Node method follows spec - deploy state desposit holder", () => {
   let nodeA: Node;
   let nodeB: Node;
+  let provider: JsonRpcProvider;
   let multisigAddress: string;
 
   beforeEach(async () => {
     const context: SetupContext = await setup(global);
+    provider = new JsonRpcProvider(global["ganacheURL"]);
     nodeA = context["A"].node;
     nodeB = context["B"].node;
 
@@ -34,10 +44,27 @@ describe("Node method follows spec - deploy state desposit holder", () => {
     expect(deployTxHash !== HashZero).toBeTruthy();
   });
 
-  it("cannot deploy multisig when", async () => {
-    const deployTxHash = await deployStateDepositHolder(nodeA, multisigAddress);
+  it("cannot withdraw when multisig has not been deployed", async () => {
+    const startingMultisigBalance = await provider.getBalance(multisigAddress);
+    await deposit(nodeA, multisigAddress, One);
 
-    expect(deployTxHash).toBeDefined();
-    expect(deployTxHash !== HashZero).toBeTruthy();
+    const postDepositMultisigBalance = await provider.getBalance(
+      multisigAddress
+    );
+
+    expect(postDepositMultisigBalance).toBeEq(startingMultisigBalance.add(One));
+
+    const recipient = getAddress(hexlify(randomBytes(20)));
+
+    expect(await provider.getBalance(recipient)).toBeEq(Zero);
+
+    const withdrawReq = constructWithdrawRpc(
+      multisigAddress,
+      One,
+      CONVENTION_FOR_ETH_TOKEN_ADDRESS,
+      recipient
+    );
+
+    expect(nodeA.rpcRouter.dispatch(withdrawReq)).rejects.toBeDefined();
   });
 });
