c204cbcff68f46ddc99d609f7cea22c2c4015a74
Liam Horne <liam@lihorne.com>
2019-10-28 10:55:02 -0400

    Handle all PRs since migration

--- packages/node/src/migrations/patch-1.ts
+++ packages/node/src/migrations/patch-1.ts
@@ -1,11 +1,13 @@
 import { Node } from "@counterfactual/types";
-import { bigNumberify } from "ethers/utils";
+import { bigNumberify, BigNumberish } from "ethers/utils";
 
 import { StateChannelJSON } from "../models/state-channel";
 
 const DB_NAMESPACE_CHANNEL = "channel";
 const DB_NAMESPACE_WITHDRAWALS = "multisigAddressToWithdrawalCommitment";
 const DB_NAMESPACE_ALL_COMMITMENTS = "allCommitments";
+
+const bigNumberIshToString = (x: BigNumberish) => bigNumberify(x).toHexString();
 
 export const migrateToPatch1 = async (
   storeService: Node.IStoreService,
@@ -17,23 +19,90 @@ export const migrateToPatch1 = async (
     oldData[DB_NAMESPACE_CHANNEL];
 
   for (const multisigAddress in stateChannelsMap) {
+    /**
+     * Update proposal:
+     * https://github.com/counterfactual/monorepo/pull/2542/files
+     */
+
+    const proposals = stateChannelsMap[multisigAddress].proposedAppInstances;
+
+    for (let i = 0; i < proposals.length; i++) {
+      const proposal = proposals[i][1];
+      stateChannelsMap[multisigAddress].proposedAppInstances[i][1] = {
+        ...proposal,
+        initiatorDeposit: bigNumberIshToString(proposal.initiatorDeposit),
+        responderDeposit: bigNumberIshToString(proposal.responderDeposit),
+        timeout: bigNumberIshToString(proposal.timeout)
+      };
+    }
+
+    /**
+     * Update interpreter params
+     * https://github.com/counterfactual/monorepo/pull/2544/files#diff-95c2150ee50c44be12700a72fd6fbf73R32
+     */
+
+    const apps = stateChannelsMap[multisigAddress].appInstances;
+
+    for (let i = 0; i < apps.length; i++) {
+      const app = apps[i][1];
+
+      if (app.twoPartyOutcomeInterpreterParams) {
+        stateChannelsMap[multisigAddress].appInstances[
+          i
+        ][1].twoPartyOutcomeInterpreterParams = {
+          ...app.twoPartyOutcomeInterpreterParams!,
+          amount: bigNumberIshToString(
+            app.twoPartyOutcomeInterpreterParams!.amount
+          )
+        };
+      }
+
+      if (app.singleAssetTwoPartyCoinTransferInterpreterParams) {
+        stateChannelsMap[multisigAddress].appInstances[
+          i
+        ][1].singleAssetTwoPartyCoinTransferInterpreterParams = {
+          ...app.singleAssetTwoPartyCoinTransferInterpreterParams!,
+          limit: bigNumberIshToString(
+            app.singleAssetTwoPartyCoinTransferInterpreterParams!.limit
+          )
+        };
+      }
+
+      if (app.multiAssetMultiPartyCoinTransferInterpreterParams) {
+        stateChannelsMap[multisigAddress].appInstances[
+          i
+        ][1].multiAssetMultiPartyCoinTransferInterpreterParams = {
+          ...app.multiAssetMultiPartyCoinTransferInterpreterParams!,
+          limit: app.multiAssetMultiPartyCoinTransferInterpreterParams.limit.map(
+            bigNumberIshToString!
+          )
+        };
+      }
+    }
+
+    /**
+     * Delete createdAt
+     * https://github.com/counterfactual/monorepo/pull/2541/files
+     */
+    delete stateChannelsMap[multisigAddress]["createdAt"];
+
+    /**
+     * https://github.com/counterfactual/monorepo/pull/2538/files
+     */
+
     const agreements =
       stateChannelsMap[multisigAddress]
         .singleAssetTwoPartyIntermediaryAgreements;
 
     for (let i = 0; i < agreements.length; i++) {
+      const agreement = agreements[i][1];
       stateChannelsMap[
         multisigAddress
       ].singleAssetTwoPartyIntermediaryAgreements[i][1] = {
         ...agreements[i][1],
-        capitalProvided: bigNumberify(
-          agreements[i][1].capitalProvided
-        ).toHexString()
+        capitalProvided: bigNumberIshToString(agreement.capitalProvided)
       };
     }
-
-    // TODO: Repeat this { _hex: string } to string conversion
-    // for all other places where BigNumber was replaced by string
   }
 
   const withdrawals = oldData[DB_NAMESPACE_WITHDRAWALS];
