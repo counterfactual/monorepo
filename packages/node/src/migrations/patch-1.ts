268426a38f48e46be07993e50ed1eaad35d617a9
Liam Horne <liam@lihorne.com>
2019-10-28 12:45:50 -0400

    Update script to use finer DB queries

--- packages/node/src/migrations/patch-1.ts
+++ packages/node/src/migrations/patch-1.ts
@@ -13,10 +13,9 @@ export const migrateToPatch1 = async (
   storeService: Node.IStoreService,
   storeKeyPrefix: string
 ) => {
-  const oldData = await storeService.get(`${storeKeyPrefix}`);
-
-  const stateChannelsMap: { [multisigAddress: string]: StateChannelJSON } =
-    oldData[DB_NAMESPACE_CHANNEL];
+  const stateChannelsMap: {
+    [multisigAddress: string]: StateChannelJSON;
+  } = await storeService.get(`${storeKeyPrefix}/${DB_NAMESPACE_CHANNEL}`);
 
   for (const multisigAddress in stateChannelsMap) {
     /**
@@ -105,9 +104,13 @@ export const migrateToPatch1 = async (
     }
   }
 
-  const withdrawals = oldData[DB_NAMESPACE_WITHDRAWALS];
+  const withdrawals = await storeService.get(
+    `${storeKeyPrefix}/${DB_NAMESPACE_WITHDRAWALS}`
+  );
 
-  const commitments = oldData[DB_NAMESPACE_ALL_COMMITMENTS];
+  const commitments = await storeService.get(
+    `${storeKeyPrefix}/${DB_NAMESPACE_ALL_COMMITMENTS}`
+  );
 
   const sharedData = {
     commitments,
