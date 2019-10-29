0f68c3754e5c41f959d6ddca2d2dd2493426ac24
Liam Horne <liam@lihorne.com>
2019-10-29 07:50:15 -0400

    Export migrate script

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -4,3 +4,4 @@ export * from "./types";
 export * from "./methods/errors";
 export * from "rpc-server";
 export { getNetworkEnum, EthereumNetworkName } from "./network-configuration";
+export { migrateToPatch1 } from "./migrations/patch-1";
